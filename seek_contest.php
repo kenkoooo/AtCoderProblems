1d9c096d36a0717db7a33a4669948346cfcb8c16
kenkoooo <kenkou.n@gmail.com>
2015-03-12 02:46:28 +0900

    Contest Seeker

--- seek_contest.php
+++ seek_contest.php
@@ -3,5 +3,13 @@ require_once 'simple_html_dom.php';
 require_once 'sql.php';
 
 $sql = new SQLConnect ();
-$query = "SELECT * FROM contests";
-$sql->exectute ( $query );
+$html = file_get_html ( 'http://atcoder.jp/' );
+
+// a要素のhref属性の抽出
+foreach ( $html->find ( a ) as $element ) {
+	if (preg_match ( '/contest\\.atcoder\\.jp/i', $element->href )) {
+		$url = $element->href;
+		$name = preg_replace ( '/http:\\/\\/([a-z0-9_\\-]*)\\.contest\\.atcoder\\.jp.*/i', '$1', $url );
+		$sql->insertContest ( $name );
+	}
+}
--- sql.php
+++ sql.php
@@ -1,6 +1,6 @@
 <?php
 class SQLConnect {
-	public function exectute($sql) {
+	private function exectute($sql) {
 		$dsn = 'mysql:dbname=LAA0348733-atcoder;host=mysql022.phy.lolipop.lan';
 		$user = 'LAA0348733';
 		$password = 'escherichia';
@@ -8,21 +8,21 @@ class SQLConnect {
 		try {
 			$dbh = new PDO ( $dsn, $user, $password );
 			
-			print ('接続に成功しました。<br>') ;
-			
 			$dbh->query ( 'SET NAMES sjis' );
+			$stmt = $dbh->query ( $sql );
 			
-			echo $sql;
-			
-			foreach ( $dbh->query ( $sql ) as $row ) {
-				print ($row ['id']) ;
-				print ($row ['name'] . '<br>') ;
-			}
+			return $stmt;
 		} catch ( PDOException $e ) {
 			print ('Error:' . $e->getMessage ()) ;
 			die ();
 		}
-		
-		$dbh = null;
+	}
+	public function insertContest($name) {
+		$query = "SELECT * FROM contests where name='$name'";
+		$data = $this->exectute ( $query );
+		if (! $data->fetch ( PDO::FETCH_ASSOC )) {
+			$query = "INSERT INTO contests (name) VALUES ('$name')";
+			$this->exectute ( $query );
+		}
 	}
 }
