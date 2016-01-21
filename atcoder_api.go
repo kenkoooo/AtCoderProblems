458eb1f9d77ae6eea2a183c412b42a45c7e230d4
kenkoooo <kenkou.n@gmail.com>
2016-01-21 14:06:12 +0900

    Ranking API

--- atcoder_api.go
+++ atcoder_api.go
@@ -20,9 +20,9 @@ type Problem struct {
 	Id       string `json:"-"`
 	Contest  string `json:"contest"`
 	Name     string `json:"name"`
-	Shortest string `json:"shortest"`
-	Fastest  string `json:"fastest"`
-	First    string `json:"first"`
+	Shortest int    `json:"shortest"`
+	Fastest  int    `json:"fastest"`
+	First    int    `json:"first"`
 
 	ShortestUser string `json:"shortest_user"`
 	FastestUser  string `json:"fastest_user"`
@@ -52,6 +52,39 @@ type Contest struct {
 	Name  string `json:"name"`
 	Start string `json:"start"`
 	End   string `json:"end"`
+}
+
+type Ranking struct {
+	Rank  int    `json:"rank"`
+	User  string `json:"user"`
+	Count int    `json:"count"`
+}
+
+func GetRanking(db *sql.DB, kind string) []Ranking {
+	ret := []Ranking{}
+	s := sq.Select()
+	if kind == "short" {
+		s = s.Column("COUNT(submissions.id) AS c").Column("user_name").From("problems").LeftJoin("submissions ON submissions.id=problems.shortest_submission_id").GroupBy("user_name").OrderBy("c DESC")
+	} else if kind == "exec" {
+		s = s.Column("COUNT(submissions.id) AS c").Column("user_name").From("problems").LeftJoin("submissions ON submissions.id=problems.fastest_submission_id").GroupBy("user_name").OrderBy("c DESC")
+	} else if kind == "fa" {
+		s = s.Column("COUNT(submissions.id) AS c").Column("user_name").From("problems").LeftJoin("submissions ON submissions.id=problems.first_submission_id").GroupBy("user_name").OrderBy("c DESC")
+	} else {
+		s = s.Column("COUNT(DISTINCT(problem_id)) AS c").Column("user_name").From("submissions").Where(sq.Eq{"status": "AC"}).GroupBy("user_name").OrderBy("c DESC")
+	}
+
+	rows, _ := s.RunWith(db).Query()
+	now := 0
+	for rows.Next() {
+		r := Ranking{Rank: 0, User: "", Count: 0}
+		rows.Scan(&r.Count, &r.User)
+		if len(ret) == 0 || r.Count != ret[now-1].Count {
+			now = len(ret) + 1
+		}
+		r.Rank = now
+		ret = append(ret, r)
+	}
+	return ret
 }
 
 func GetProblemList(db *sql.DB, user, rivals string) map[string]Problem {
@@ -164,7 +197,7 @@ func main() {
 		{
 			_, ok := req.Form["user"]
 			if ok {
-				urep := regexp.MustCompile(`^[0-9_a-zA-Z]*$`)
+				urep := regexp.MustCompile(`^[0-9_a-zA-Z\-]*$`)
 				if urep.Match([]byte(req.Form["user"][0])) {
 					user = req.Form["user"][0]
 				}
@@ -174,10 +207,20 @@ func main() {
 		{
 			_, ok := req.Form["rivals"]
 			if ok {
-				rrep := regexp.MustCompile(`^[0-9_a-zA-Z,]*$`)
+				rrep := regexp.MustCompile(`^[0-9_a-zA-Z,\-]*$`)
 				req.Form["rivals"][0] = strings.Replace(req.Form["rivals"][0], "%2C", ",", -1)
 				if rrep.Match([]byte(req.Form["rivals"][0])) {
 					rivals = req.Form["rivals"][0]
+				}
+			}
+		}
+		kind := ""
+		{
+			_, ok := req.Form["kind"]
+			if ok {
+				rrep := regexp.MustCompile(`^[a-z]*$`)
+				if rrep.Match([]byte(req.Form["kind"][0])) {
+					kind = req.Form["kind"][0]
 				}
 			}
 		}
@@ -196,6 +239,11 @@ func main() {
 				contests[c.Id] = c
 			}
 			b, _ := json.MarshalIndent(contests, "", "\t")
+			res.Header().Set("Content-Type", "application/json")
+			fmt.Fprint(res, string(b))
+		} else if tool == "ranking" {
+			ranking := GetRanking(db, kind)
+			b, _ := json.MarshalIndent(ranking, "", "\t")
 			res.Header().Set("Content-Type", "application/json")
 			fmt.Fprint(res, string(b))
 		}
