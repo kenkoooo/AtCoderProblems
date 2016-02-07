966a26207b9886f5c5f5677c3a3004dc9166fa8a
kenkoooo <kenkou.n@gmail.com>
2016-02-07 20:36:45 +0900

    Add Line Chart with D3.js

--- web/atcoder.js
+++ web/atcoder.js
@@ -70,164 +70,280 @@ $(document).ready(function() {
         });
 
     } else if (params['kind'] === 'list') {
-        // List Mode
-        $('input[name=list]').val(['1']);
-        $.when($.getJSON("../atcoder-api/contests"), $.getJSON("../atcoder-api/problems", {
-            "user": user,
-            "rivals": rivals,
-        })).done(function(data_c, data_p) {
-            contests_json = data_c[0];
-            problems_json = data_p[0];
-            contestList = [];
-            for (contest in contests_json) {
-                link = "<a target='_blank' href='http://" + contest + ".contest.atcoder.jp/'>" + contests_json[contest]["name"] + "</a>";
-                contestList[contest] = link;
-            }
-            rows = [];
-            for (problem in problems_json) {
-                p = problems_json[problem];
-                contest = p["contest"];
-                rival_num = 0;
-                if (p["status"] === 'AC') {
-                    s = "<div class='text-center'><span class='label label-success'>AC</span></div>";
-                } else if (Object.keys(p["rivals"]).length > 0) {
-                    s = "";
-                    for (rival in p["rivals"]) {
-                        s += "<div class='text-center'><span class='label label-danger'>" + rival + "</span></div>";
-                        rival_num++;
-                    }
-                } else if (p["status"] !== '') {
-                    s = "<div class='text-center'><span class='label label-warning'>" + p["status"] + "</span></div>";
-                } else {
-                    s = "";
-                }
-                var e = "";
-                var l = "";
-                var f = "";
-                if (p["source_length"] > 0) {
-                    e = "<a target='_blank' href='" + p["fastest_url"] + "'>" + p["exec_time"] + " ms (" + p["fastest_user"] + ")</a>";
-                    l = "<a target='_blank' href='" + p["shortest_url"] + "'>" + p["source_length"] + " Bytes (" + p["shortest_user"] + ")</a>";
-                    f = "<a target='_blank' href='" + p["first_url"] + "'>" + p["first_user"] + "</a>";
-                }
-
-                rows.push({
-                    problem_name: "<a target='_blank' href='http://" + contest + ".contest.atcoder.jp/tasks/" + problem + "'>" + p["name"] + "</a>",
-                    contest_name: contestList[contest],
-                    status: s,
-                    solvers: p["solvers"],
-                    exec: e,
-                    length: l,
-                    date: contests_json[contest]["start"].replace(/ .*$/g, ''),
-                    first: f,
-                    raw_status: p["status"],
-                    raw_rivals: rival_num,
-                });
-            }
-            var $table = $('#all-problems');
-            $table.bootstrapTable('append', rows);
-        }).fail(function() {
-            console.log('error');
-        });
+        showList(user, rivals);
     } else if (params['kind'] === 'ranking') {
-        $("#problem-search-block").remove();
-        $("#lead-text").remove();
-        var k = "";
-        if (params["ranking"] == 1) {
-            k = "";
-            $("#header-title").text('AtCoder AC 数ランキング');
-        }
-        if (params["ranking"] == 2) {
-            k = "short";
-            $("#header-title").text('AtCoder コードゴルフランキング');
-        }
-        if (params["ranking"] == 3) {
-            k = "fast";
-            $("#header-title").text('AtCoder 実行速度ランキング');
-        }
-        if (params["ranking"] == 4) {
-            k = "fa";
-            $("#header-title").text('AtCoder 最速提出ランキング');
-        }
-        $.when($.getJSON("../atcoder-api/ranking", {
-            "kind": k,
-        })).done(function(ranking_json) {
-            rows = [];
-            for (var i = 0; i < Math.min(ranking_json.length, 500) && ranking_json[i]['count'] > 0; i++) {
-                rows.push({
-                    rank: ranking_json[i]['rank'],
-                    user_name: "<a href='./?kind=list&name=" + ranking_json[i]['user'] + "'>" + ranking_json[i]['user'] + "</a>",
-                    count: ranking_json[i]['count'],
-                });
-            }
-            var $table = $('#all-ranking');
-            $table.bootstrapTable('append', rows);
-        }).fail(function() {
-            console.log('error');
-        });
+        // Ranking Mode
+        showRanking(params);
     } else if (params['kind'] === 'user') {
-        // User Page
-        $("#header-user").text(params["name"]);
-        $("#problem-container").remove();
-
-        $.when($.getJSON("../atcoder-api/user", {
-            "user": user
-        })).done(function(json) {
-            if (json["ac_rank"] == 0) {
-                $("#user-contents").remove();
-                return;
-            }
-            $("#ac-num").text(json["ac_num"] + " 問");
-            $("#short-num").text(json["short_num"] + " 問");
-            $("#fast-num").text(json["fast_num"] + " 問");
-            $("#first-num").text(json["first_num"] + " 問");
-
-            if (json["ac_rank"] > 0) $("#ac-rank").text(json["ac_rank"] + " 位");
-            if (json["short_rank"] > 0) $("#short-rank").text(json["short_rank"] + " 位");
-            if (json["fast_rank"] > 0) $("#fast-rank").text(json["fast_rank"] + " 位");
-            if (json["first_rank"] > 0) $("#first-rank").text(json["first_rank"] + " 位");
-
-            var abcs = [
-                "abc_a", "abc_b", "abc_c", "abc_d",
-            ];
-            for (var i = 0; i < abcs.length; i++) {
-                var doughnutData = [{
-                    value: json[abcs[i]],
-                    color: "#32CD32",
-                    label: 'Accepted',
-                }, {
-                    value: json["abc_num"] - json[abcs[i]],
-                    color: "#58616A",
-                }];
-                new Chart(document.getElementById(abcs[i] + "_donuts").getContext("2d")).Doughnut(doughnutData);
-                $("#" + abcs[i] + "_num").text(json[abcs[i]] + "問 / " + json["abc_num"] + " 問");
-            }
-            var arcs = [
-                "arc_a", "arc_b", "arc_c", "arc_d",
-            ];
-            for (var i = 0; i < arcs.length; i++) {
-                var doughnutData = [{
-                    value: json[arcs[i]],
-                    color: "#32CD32",
-                    label: 'Accepted',
-                }, {
-                    value: json["arc_num"] - json[arcs[i]],
-                    color: "#58616A",
-                }];
-                new Chart(document.getElementById(arcs[i] + "_donuts").getContext("2d")).Doughnut(doughnutData);
-                $("#" + arcs[i] + "_num").text(json[arcs[i]] + "問 / " + json["arc_num"] + " 問");
-            }
-
-        }).fail(function() {
-            console.log('error');
-        });
+        showUserPage(user)
     }
 
     var user_href = $("#user-page-link").attr("href");
     $("#user-page-link").attr("href", user_href + '&name=' + params["name"]);
     var header_href = $("#header-link").attr("href");
     $("#header-link").attr("href", header_href + '&name=' + params["name"]);
-
 });
+
+function showList(user, rivals) {
+    // List Mode
+    $('input[name=list]').val(['1']);
+    $.when($.getJSON("../atcoder-api/contests"), $.getJSON("../atcoder-api/problems", {
+        "user": user,
+        "rivals": rivals,
+    })).done(function(data_c, data_p) {
+        contests_json = data_c[0];
+        problems_json = data_p[0];
+        contestList = [];
+        for (contest in contests_json) {
+            link = "<a target='_blank' href='http://" + contest + ".contest.atcoder.jp/'>" + contests_json[contest]["name"] + "</a>";
+            contestList[contest] = link;
+        }
+        rows = [];
+        for (problem in problems_json) {
+            p = problems_json[problem];
+            contest = p["contest"];
+            rival_num = 0;
+            if (p["status"] === 'AC') {
+                s = "<div class='text-center'><span class='label label-success'>AC</span></div>";
+            } else if (Object.keys(p["rivals"]).length > 0) {
+                s = "";
+                for (rival in p["rivals"]) {
+                    s += "<div class='text-center'><span class='label label-danger'>" + rival + "</span></div>";
+                    rival_num++;
+                }
+            } else if (p["status"] !== '') {
+                s = "<div class='text-center'><span class='label label-warning'>" + p["status"] + "</span></div>";
+            } else {
+                s = "";
+            }
+            var e = "";
+            var l = "";
+            var f = "";
+            if (p["source_length"] > 0) {
+                e = "<a target='_blank' href='" + p["fastest_url"] + "'>" + p["exec_time"] + " ms (" + p["fastest_user"] + ")</a>";
+                l = "<a target='_blank' href='" + p["shortest_url"] + "'>" + p["source_length"] + " Bytes (" + p["shortest_user"] + ")</a>";
+                f = "<a target='_blank' href='" + p["first_url"] + "'>" + p["first_user"] + "</a>";
+            }
+
+            rows.push({
+                problem_name: "<a target='_blank' href='http://" + contest + ".contest.atcoder.jp/tasks/" + problem + "'>" + p["name"] + "</a>",
+                contest_name: contestList[contest],
+                status: s,
+                solvers: p["solvers"],
+                exec: e,
+                length: l,
+                date: contests_json[contest]["start"].replace(/ .*$/g, ''),
+                first: f,
+                raw_status: p["status"],
+                raw_rivals: rival_num,
+            });
+        }
+        var $table = $('#all-problems');
+        $table.bootstrapTable('append', rows);
+    }).fail(function() {
+        console.log('error');
+    });
+
+}
+
+function showUserPage(user) {
+    // User Page
+    $("#header-user").text(user);
+    $("#problem-container").remove();
+
+    $.when($.getJSON("../atcoder-api/user", {
+        "user": user
+    }), $.getJSON("../atcoder-api/problems", {
+        "user": user
+    })).done(function(json_u, json_p) {
+        user_json = json_u[0];
+        problems_json = json_p[0];
+        if (user_json["ac_rank"] == 0) {
+            $("#user-contents").remove();
+            return;
+        }
+        $("#ac-num").text(user_json["ac_num"] + " 問");
+        $("#short-num").text(user_json["short_num"] + " 問");
+        $("#fast-num").text(user_json["fast_num"] + " 問");
+        $("#first-num").text(user_json["first_num"] + " 問");
+
+        if (user_json["ac_rank"] > 0) $("#ac-rank").text(user_json["ac_rank"] + " 位");
+        if (user_json["short_rank"] > 0) $("#short-rank").text(user_json["short_rank"] + " 位");
+        if (user_json["fast_rank"] > 0) $("#fast-rank").text(user_json["fast_rank"] + " 位");
+        if (user_json["first_rank"] > 0) $("#first-rank").text(user_json["first_rank"] + " 位");
+
+        var abcs = ["abc_a", "abc_b", "abc_c", "abc_d", ];
+        for (var i = 0; i < abcs.length; i++) {
+            var doughnutData = [{
+                value: user_json[abcs[i]],
+                color: "#32CD32",
+                label: 'Accepted',
+            }, {
+                value: user_json["abc_num"] - user_json[abcs[i]],
+                color: "#58616A",
+            }];
+            new Chart(document.getElementById(abcs[i] + "_donuts").getContext("2d")).Doughnut(doughnutData);
+            $("#" + abcs[i] + "_num").text(user_json[abcs[i]] + "問 / " + user_json["abc_num"] + " 問");
+        }
+
+        var arcs = ["arc_a", "arc_b", "arc_c", "arc_d", ];
+        for (var i = 0; i < arcs.length; i++) {
+            var doughnutData = [{
+                value: user_json[arcs[i]],
+                color: "#32CD32",
+                label: 'Accepted',
+            }, {
+                value: user_json["arc_num"] - user_json[arcs[i]],
+                color: "#58616A",
+            }];
+            new Chart(document.getElementById(arcs[i] + "_donuts").getContext("2d")).Doughnut(doughnutData);
+            $("#" + arcs[i] + "_num").text(user_json[arcs[i]] + "問 / " + user_json["arc_num"] + " 問");
+        }
+
+        var dateKeys = [];
+        for (key in problems_json) {
+            if (problems_json.hasOwnProperty(key)) {
+                var date = problems_json[key]["ac_time"];
+                if (date.length > 0) {
+                    dateKeys.push(problems_json[key]["ac_time"]);
+                }
+            }
+        }
+        dateKeys.sort();
+        var data = [];
+        for (var i = 0; i < dateKeys.length; i++) {
+            data.push({
+                date: dateKeys[i],
+                value: (i + 1)
+            });
+        };
+
+
+        // 表示サイズを設定
+        var margin = {
+            top: 40,
+            right: 40,
+            bottom: 40,
+            left: 40
+        };
+        var width = $("#user-solved-parent").width();
+        console.log(width);
+        var size = {
+            width: width,
+            height: 400
+        };
+
+
+        var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse;
+
+        var svg = d3.select("#user-solved-problems")
+            .attr("width", size.width)
+            .attr("height", size.height)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        var x = d3.time.scale()
+            .range([0, size.width - margin.left - margin.right]);
+
+        var y = d3.scale.linear()
+            .range([size.height - margin.top - margin.bottom, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("bottom")
+            .tickFormat(d3.time.format("%Y-%b"));
+
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left");
+
+        var line = d3.svg.line()
+            .x(function(d) {
+                return x(d.date);
+            })
+            .y(function(d) {
+                return y(d.value);
+            });
+
+
+        data.forEach(function(d) {
+            d.date = parseDate(d.date);
+            d.value = +d.value;
+        });
+
+        x.domain(d3.extent(data, function(d) {
+            return d.date;
+        }));
+        y.domain(d3.extent(data, function(d) {
+            return d.value;
+        }));
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0, " + (size.height - margin.top - margin.bottom) + ")")
+            .call(xAxis);
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".7em")
+            .style("text-anchor", "end")
+            .text("Solved");
+
+        svg.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", line);
+
+    }).fail(function() {
+        console.log('error');
+    });
+
+}
+
+function showRanking(params) {
+    // Ranking Mode
+    $("#problem-search-block").remove();
+    $("#lead-text").remove();
+
+    var k = "";
+    if (params["ranking"] == 1) {
+        k = "";
+        $("#header-title").text('AtCoder AC 数ランキング');
+    }
+    if (params["ranking"] == 2) {
+        k = "short";
+        $("#header-title").text('AtCoder コードゴルフランキング');
+    }
+    if (params["ranking"] == 3) {
+        k = "fast";
+        $("#header-title").text('AtCoder 実行速度ランキング');
+    }
+    if (params["ranking"] == 4) {
+        k = "fa";
+        $("#header-title").text('AtCoder 最速提出ランキング');
+    }
+
+    $.when($.getJSON("../atcoder-api/ranking", {
+        "kind": k
+    })).done(function(ranking_json) {
+        rows = [];
+        for (var i = 0; i < Math.min(ranking_json.length, 500) && ranking_json[i]['count'] > 0; i++) {
+            rows.push({
+                rank: ranking_json[i]['rank'],
+                user_name: "<a href='./?kind=list&name=" + ranking_json[i]['user'] + "'>" + ranking_json[i]['user'] + "</a>",
+                count: ranking_json[i]['count'],
+            });
+        }
+        var $table = $('#all-ranking');
+        $table.bootstrapTable('append', rows);
+    }).fail(function() {
+        console.log('error');
+    });
+}
 
 function getParam() {
     var paramsArray = [];
