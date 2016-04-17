2d8a59cf0f367aac5239338d7df611a97b0ee4e0
kenkoooo <kenkou.n@gmail.com>
2016-04-18 00:13:43 +0900

    Split function

--- atcoder_api.go
+++ atcoder_api.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
 	"flag"
 	"fmt"
@@ -16,180 +15,13 @@ import (
 
 	as "./api_server"
 	ct "./crawl_tools"
-	sq "github.com/Masterminds/squirrel"
 	"github.com/Sirupsen/logrus"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type Problem struct {
-	Id      string `json:"id"`
-	Contest string `json:"contest"`
-	Name    string `json:"name"`
-
-	Shortest string `json:"-"`
-	Fastest  string `json:"-"`
-	First    string `json:"-"`
-
-	ShortestContest string `json:"-"`
-	FastestContest  string `json:"-"`
-	FirstContest    string `json:"-"`
-
-	ShortestURL string `json:"shortest_url"`
-	FastestURL  string `json:"fastest_url"`
-	FirstURL    string `json:"first_url"`
-
-	ShortestUser string `json:"shortest_user"`
-	FastestUser  string `json:"fastest_user"`
-	FirstUser    string `json:"first_user"`
-
-	SourceLength int `json:"source_length"`
-	ExecTime     int `json:"exec_time"`
-
-	Solvers    int `json:"solvers"`
-	Difficulty int `json:"difficulty"`
-
-	Status string              `json:"status"`
-	ACTime string              `json:"ac_time"`
-	Rivals map[string]struct{} `json:"rivals"`
-}
-
-type Submission struct {
-	Id           int
-	ProblemId    string
-	ContestId    string
-	User         string
-	Language     string
-	SourceLength int
-	Status       string
-	ExecTime     int
-	CreatedAt    string
-}
-
-func GetProblemList(db *sql.DB, logger *logrus.Logger, user string, rivals []string) []Problem {
-	ret := make(map[string]Problem)
-	{
-		rows, _ := sq.Select(
-			"p.id",
-			"p.contest",
-			"p.name",
-			"p.shortest_submission_id",
-			"p.fastest_submission_id",
-			"p.first_submission_id",
-			"p.difficulty",
-			"sh.user_name",
-			"fs.user_name",
-			"fa.user_name",
-			"sh.contest_id",
-			"fs.contest_id",
-			"fa.contest_id",
-			"sh.source_length",
-			"fs.exec_time",
-		).From("problems AS p").LeftJoin(
-			"submissions AS sh ON sh.id=p.shortest_submission_id").LeftJoin(
-			"submissions AS fs ON fs.id=p.fastest_submission_id").LeftJoin(
-			"submissions AS fa ON fa.id=p.first_submission_id").RunWith(db).Query()
-		for rows.Next() {
-			x := Problem{}
-			rows.Scan(
-				&x.Id,
-				&x.Contest,
-				&x.Name,
-				&x.Shortest,
-				&x.Fastest,
-				&x.First,
-				&x.Difficulty,
-				&x.ShortestUser,
-				&x.FastestUser,
-				&x.FirstUser,
-				&x.ShortestContest,
-				&x.FastestContest,
-				&x.FirstContest,
-				&x.SourceLength,
-				&x.ExecTime,
-			)
-			if x.SourceLength > 0 {
-
-				x.ShortestURL = "http://" + x.ShortestContest + ".contest.atcoder.jp/submissions/" + x.Shortest
-				x.FastestURL = "http://" + x.FastestContest + ".contest.atcoder.jp/submissions/" + x.Fastest
-				x.FirstURL = "http://" + x.FirstContest + ".contest.atcoder.jp/submissions/" + x.First
-			}
-			x.Rivals = make(map[string]struct{})
-			x.Status = ""
-			ret[x.Id] = x
-		}
-	}
-	{
-		rows, _ := sq.Select("COUNT(DISTINCT(user_name))", "problem_id").From("submissions").Where(
-			sq.Eq{"status": "AC"}).GroupBy("problem_id").RunWith(db).Query()
-		for rows.Next() {
-			s := ""
-			solver := 0
-			rows.Scan(&solver, &s)
-			x, ok := ret[s]
-			if ok {
-				x.Solvers = solver
-				ret[s] = x
-			}
-		}
-	}
-	if user != "" {
-		rows, _ := sq.Select("problem_id", "status", "created_time").From("submissions").Where(sq.Eq{
-			"user_name": user,
-		}).RunWith(db).Query()
-		for rows.Next() {
-			x := Submission{}
-			rows.Scan(
-				&x.ProblemId,
-				&x.Status,
-				&x.CreatedAt,
-			)
-
-			if ret[x.ProblemId].Status != "AC" {
-				tmp := ret[x.ProblemId]
-				tmp.Status = x.Status
-				if x.Status == "AC" {
-					tmp.ACTime = x.CreatedAt
-				}
-				ret[x.ProblemId] = tmp
-			}
-		}
-	}
-	if len(rivals) > 0 {
-		rows, _ := sq.Select("*").From("submissions").Where(sq.And{
-			sq.Eq{"user_name": rivals}, sq.Eq{"status": "AC"},
-		}).RunWith(db).Query()
-		for rows.Next() {
-			x := Submission{}
-			rows.Scan(
-				&x.Id,
-				&x.ProblemId,
-				&x.ContestId,
-				&x.User,
-				&x.Status,
-				&x.SourceLength,
-				&x.Language,
-				&x.ExecTime,
-				&x.CreatedAt,
-			)
-
-			ret[x.ProblemId].Rivals[x.User] = struct{}{}
-		}
-	}
-
-	ret_slice := []Problem{}
-	for _, value := range ret {
-		ret_slice = append(ret_slice, value)
-	}
-	logger.WithFields(logrus.Fields{
-		"user":   user,
-		"rivals": rivals,
-	}).Info("API request")
-	return ret_slice
-}
-
 func main() {
 	mu := sync.RWMutex{}
-	problem_cache := make(map[string][]Problem)
+	problem_cache := make(map[string][]as.Problem)
 	contest_cache := make(map[string][]as.Contest)
 	ranking_cache := make(map[string][]as.Ranking)
 	user_cache := make(map[string]as.User)
@@ -291,7 +123,7 @@ func main() {
 			}
 			problems, ok := problem_cache[key]
 			if !ok {
-				problems = GetProblemList(db, logger, user, rivals)
+				problems = as.GetProblemList(db, logger, user, rivals)
 				problem_cache[key] = problems
 				time.AfterFunc(time.Minute*3, func() {
 					mu.Lock()
