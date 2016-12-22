4f591ee9be8e13d9c83ddd1653014c508b6ed600
kenkoooo <kenkou.n@gmail.com>
2016-12-22 17:48:20 +0900

    See you React.

--- react-web/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Hello from "./Hello";
-
-ReactDOM.render(<Hello content="hello world"/>, document.getElementById('app'));
