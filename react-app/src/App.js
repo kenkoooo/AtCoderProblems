e78484e6838acbedbeb052feba4a782cd12f98aa
kenkoooo <kenkou.n@gmail.com>
2016-12-25 03:41:30 +0900

    俺、リアクトになろうと思うんだ・・・

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,30 @@
 import React, {Component} from 'react';
-import queryString from 'query-string';
+import Arguments from './Arguments';
 import logo from './logo.svg';
 import './App.css';
 
+function hoge() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h2>Welcome to Hoge!!</h2>
+      </div>
+      <p className="App-intro">
+        To get started, edit
+        <code>src/App.js</code>
+        and save to reload.
+      </p>
+    </div>
+  );
+}
+
 class App extends Component {
-    render() {
-        console.log(queryString.parse(location.search));
-        return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h2>Welcome to Hoge!!</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit
-                    <code>src/App.js</code>
-                    and save to reload.
-                </p>
-            </div>
-        );
-    }
+  render() {
+    const args = new Arguments();
+    console.log(args);
+    return hoge();
+  }
 }
 
 export default App;
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import NavigationBar from './NavigationBar';
 import './index.css';
 
 ReactDOM.render(
-    <App/>, document.getElementById('root'));
+  <App/>, document.getElementById('root'));
+ReactDOM.render(
+  <NavigationBar/>, document.getElementById('navbar'));
