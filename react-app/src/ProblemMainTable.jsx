49eaed2956116dae02fe6766dd8d8c380aac394c
kenkoooo <kenkou.n@gmail.com>
2016-12-27 10:54:57 +0900

    MainTable

--- react-app/src/ProblemMainTable.jsx
+++ react-app/src/ProblemMainTable.jsx
@@ -14,6 +14,10 @@ import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
 import MyUtils from './MyUtils';
 
 function problemLinkFormatter(cell, row) {
+  if (cell == null)
+    return (
+      <span>-</span>
+    );
   return (
     <a href={`https://${row.id}.contest.atcoder.jp/tasks/${cell.id}`} target="_blank">{cell.name}</a>
   );
@@ -34,23 +38,52 @@ class ProblemMainTable extends Component {
     };
 
     this.filterAndRender = this.filterAndRender.bind(this);
+    this.getTableColumns = this.getTableColumns.bind(this);
+    this.columnClassNameFormatter = this.columnClassNameFormatter.bind(this);
   }
 
   componentDidMount() {
-    MyUtils.getAPIPromise("./json/problems_simple.json", {}).then(json => this.setState({problems: json}));
-    MyUtils.getAPIPromise("./json/contests.json", {}).then(json => this.setState({contests: json}));
     MyUtils.getAPIPromise("/atcoder-api/problems", {
       user: this.state.user,
       rivals: this.state.rivals
-    }).then(json => this.setState({results: json}));
+    }).then(json => {
+      this.setState({results: json});
+      const solved = new Set();
+      const wrong = new Set();
+      const lost = new Set();
+      json.forEach(p => {
+        if (p.status === "AC") {
+          solved.add(p.id);
+        } else if (p.rivals.length > 0) {
+          lost.add(p.id);
+        } else {
+          wrong.add(p.id);
+        }
+      });
+      this.setState({solved: solved, wrong: wrong, lost: lost});
+      MyUtils.getAPIPromise("./json/problems_simple.json", {}).then(json => this.setState({problems: json}));
+      MyUtils.getAPIPromise("./json/contests.json", {}).then(json => this.setState({contests: json}));
+    });
   }
+
+  columnClassNameFormatter(value, row, rowIndex, columnIndex) {
+    if (value == null)
+      return "";
+    if (this.state.solved.has(value.id))
+      return 'success';
+    if (this.state.wrong.has(value.id))
+      return 'warning';
+    if (this.state.lost.has(value.id))
+      return 'danger';
+
+    }
 
   getTableColumns(props) {
     const s = "abcdef";
     const columns = new Array(props.num).fill(0).map((_, i) => {
       const c = s.charAt(i);
       return (
-        <TableHeaderColumn key={i} dataField={c} dataFormat={problemLinkFormatter}>{c.toUpperCase()}
+        <TableHeaderColumn key={i} dataField={c} dataFormat={problemLinkFormatter} columnClassName={this.columnClassNameFormatter}>{c.toUpperCase()}
           問題</TableHeaderColumn>
       );
     });
@@ -91,26 +124,37 @@ class ProblemMainTable extends Component {
           return -1;
         return 0;
       });
-      const o = {
-        id: k,
-        a: v[0],
-        b: v[1],
-        c: v[2],
-        d: v[3]
-      };
       if (v.length > 4) {
-        o.e = v[4];
-        o.f = v[5];
+        data.push({
+          id: k,
+          a: v[0],
+          b: v[1],
+          c: v[2],
+          d: v[3],
+          e: v[4],
+          f: v[5]
+        });
+      } else if (v.length > 2) {
+        data.push({id: k, a: v[0], b: v[1], c: v[2], d: v[3]});
+      } else {
+        data.push({id: k, c: v[0], d: v[1]});
       }
-      data.push(o);
+
     });
+    data.reverse();
 
     return (<this.getTableColumns num={props.num} data={data} header={props.header}/>);
   }
 
   render() {
-    const regex = /^[agc[0-9]*$/;
-    return (<this.filterAndRender regex={regex} num={6} header="AtCoder Grand Contest"/>);
+    const r = [/^agc[0-9]*$/, /^abc[0-9]*$/, /^arc[0-9]*$/];
+    return (
+      <Row>
+        <this.filterAndRender regex={r[0]} num={6} header="AtCoder Grand Contest"/>
+        <this.filterAndRender regex={r[1]} num={4} header="AtCoder Beginner Contest"/>
+        <this.filterAndRender regex={r[2]} num={4} header="AtCoder Regular Contest"/>
+      </Row>
+    );
   }
 }
 
