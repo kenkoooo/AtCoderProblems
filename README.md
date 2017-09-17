c4e4b2078130b95b061f0b605e39505830ec6c16
kenkoooo <kenkou.n@gmail.com>
2017-09-17 21:56:59 +0900

    setup rust new application

--- README.md
+++
@@ -1,3 +0,0 @@
-# AtCoderProblems
-
-AtCoder の問題を管理する非公式サイト AtCoder Problems http://kenkoooo.com/atcoder/ です。
--- react-app/src/NavigationBar.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import NavigationBar from './NavigationBar';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-    <NavigationBar/>, div);
-});
