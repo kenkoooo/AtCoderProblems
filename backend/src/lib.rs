dc832f8e0aecf548ac0990dd4b06d5cc201afcc3
kenkoooo <kenkou.n@gmail.com>
2017-09-25 01:32:39 +0900

    Test of Travis MySQL

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -5,9 +5,11 @@ extern crate serde_json;
 extern crate serde;
 extern crate chrono;
 extern crate toml;
+extern crate mysql;
 
 #[macro_use]
 extern crate serde_derive;
 
 pub mod scraper;
 pub mod conf;
+pub mod db;
