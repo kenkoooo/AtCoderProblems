665282c9f6562b584ea74a8ec6555c0701e56718
kenkoooo <kenkou.n@gmail.com>
2017-11-09 09:30:05 +0900

    Add contest scraper test

--- atcoder-problems/src/test/scala/com/kenkoooo/scraper/ContestScraperTest.scala
+++ atcoder-problems/src/test/scala/com/kenkoooo/scraper/ContestScraperTest.scala
@@ -1,11 +1,21 @@
 package com.kenkoooo.scraper
 
-import org.scalatest.FunSuite
+import org.scalatest.{FunSuite, Matchers}
 
-class ContestScraperTest extends FunSuite {
-  test("sc") {
+class ContestScraperTest extends FunSuite with Matchers {
+  test("scrape contest list") {
     val scraper = new ContestScraper
 
-    scraper.scrape(8)
+    var finish = false
+    var page = 1
+    while (!finish) {
+      val contests = scraper.scrape(page)
+      if (contests.isEmpty) {
+        finish = true
+      } else {
+        page += 1
+      }
+    }
+    page should be >= 9
   }
 }
