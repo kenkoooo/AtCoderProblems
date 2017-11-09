a49317f3ccf93d932f8ea3833de4bbd62b93b514
kenkoooo <kenkou.n@gmail.com>
2017-11-09 09:54:04 +0900

    Add problem scraper

---
+++ atcoder-problems/src/main/scala/com/kenkoooo/scraper/ProblemScraper.scala
@@ -0,0 +1,29 @@
+package com.kenkoooo.scraper
+
+import net.ruippeixotog.scalascraper.browser.JsoupBrowser
+import net.ruippeixotog.scalascraper.dsl.DSL._
+import net.ruippeixotog.scalascraper.scraper.ContentExtractors.{element, elements}
+
+class ProblemScraper {
+  private val browser = JsoupBrowser()
+
+  def scrape(contest: String): Array[Problem] = {
+    val url = s"${AtCoder.BaseUrl}contests/$contest/tasks"
+    val doc = browser.get(url)
+    val problems = for (tr <- doc >> elements("tbody > tr")) yield {
+      val rows = (for (td <- tr >> elements("td")) yield td).toArray
+      val problemTitle = s"${rows(0).text}. ${rows(1).text}"
+
+      val problemId = {
+        val pattern = "^.*?tasks/([a-zA-Z0-9-_]+)$".r
+        val problemUrl = (rows(0) >> element("a")).attr("href")
+        val pattern(id) = problemUrl
+        id
+      }
+      Problem(problemId, problemTitle)
+    }
+    problems.toArray
+  }
+}
+
+case class Problem(id: String, title: String)
---
+++ atcoder-problems/src/test/scala/com/kenkoooo/scraper/ProblemScraperTest.scala
@@ -0,0 +1,14 @@
+package com.kenkoooo.scraper
+
+import org.scalatest.{FunSuite, Matchers}
+
+class ProblemScraperTest extends FunSuite with Matchers {
+  test("scrape problems of ARC 084") {
+    val scraper = new ProblemScraper
+    val problems = scraper.scrape("arc084")
+
+    problems.length shouldBe 4
+    problems(0).id shouldBe "arc084_a"
+    problems(0).title shouldBe "C. Snuke Festival"
+  }
+}
