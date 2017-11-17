c1862644b11e42c02626515f18b85d8811a9c181
kenkoooo <kenkou.n@gmail.com>
2017-11-18 06:31:23 +0900

    setup typescript-react app

---
+++ atcoder-problems-frontend/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Hello } from "./components/Hello";
+
+ReactDOM.render(
+    <Hello compiler="TypeScript" framework="React" />, document.getElementById("example")
+);
