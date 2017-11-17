4bc5a73023f2fe4d80421b9cbfe14c2b0f236c62
kenkoooo <kenkou.n@gmail.com>
2017-11-18 06:48:04 +0900

    install query-string

--- atcoder-problems-frontend/src/components/Hello.tsx
+++ atcoder-problems-frontend/src/components/Hello.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import * as QueryString from "query-string";
 
 export interface HelloProps { compiler: string; framework: string; }
 
@@ -6,6 +7,8 @@ export interface HelloProps { compiler: string; framework: string; }
 // State is never set so we use the '{}' type.
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
+        let parsed = QueryString.parse(location.search);
+        console.log(parsed);
         return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
