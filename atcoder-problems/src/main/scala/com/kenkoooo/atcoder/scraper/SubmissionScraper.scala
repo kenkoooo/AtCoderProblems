ca03ec1ef327f0d2dc74e426f517b63d78dd3057
kenkoooo <kenkou.n@gmail.com>
2017-11-19 11:57:17 +0900

    try-catch submission scraping errors

--- atcoder-problems/src/main/scala/com/kenkoooo/atcoder/scraper/SubmissionScraper.scala
+++ atcoder-problems/src/main/scala/com/kenkoooo/atcoder/scraper/SubmissionScraper.scala
@@ -6,6 +6,8 @@ import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import org.apache.logging.log4j.scala.Logging
 
+import scala.util.{Failure, Success, Try}
+
 class SubmissionScraper extends Logging {
 
   /**
@@ -21,55 +23,63 @@ class SubmissionScraper extends Logging {
 
     logger.info(s"scraping $url...")
     val doc = browser.get(url)
-    val submissions = for (tr <- doc >> elements("tbody > tr")) yield {
-      val row = (for (td <- tr >> elements("td")) yield td).toArray
-      val time = row(0).text
+    val submissions = for (tr <- doc >> elements("tbody > tr"))
+      yield
+        Try {
+          val row = (for (td <- tr >> elements("td")) yield td).toArray
+          val time = row(0).text
 
-      val problemId = {
-        val pattern = "^.*?tasks/([a-zA-Z0-9-_]+)$".r
-        val problemUrl = (row(1) >> element("a")).attr("href")
-        val pattern(id) = problemUrl
-        id
-      }
+          val problemId = {
+            val pattern = "^.*?tasks/([a-zA-Z0-9-_]+)$".r
+            val problemUrl = (row(1) >> element("a")).attr("href")
+            val pattern(id) = problemUrl
+            id
+          }
 
-      val userId = {
-        val pattern = "^.*?users/([a-zA-Z0-9-_]+)$".r
-        val userUrl = (row(2) >> element("a")).attr("href")
-        val pattern(userId) = userUrl
-        userId
-      }
+          val userId = {
+            val pattern = "^.*?users/([a-zA-Z0-9-_]+)$".r
+            val userUrl = (row(2) >> element("a")).attr("href")
+            val pattern(userId) = userUrl
+            userId
+          }
 
-      val language = row(3).text
-      val point = row(4).text.toDouble
-      val length = row(5).text.replaceAll(" Byte", "").toInt
-      val result = row(6).text
+          val language = row(3).text
+          val point = row(4).text.toDouble
+          val length = row(5).text.replaceAll(" Byte", "").toInt
+          val result = row(6).text
 
-      val (executionTime, idUrl) = {
-        if (row.length == 10) {
-          (Some(row(7).text.replaceAll(" ms", "").toInt), (row(9) >> element("a")).attr("href"))
-        } else {
-          (None, (row(7) >> element("a")).attr("href"))
+          val (executionTime, idUrl) = {
+            if (row.length == 10) {
+              (Some(row(7).text.replaceAll(" ms", "").toInt), (row(9) >> element("a")).attr("href"))
+            } else {
+              (None, (row(7) >> element("a")).attr("href"))
+            }
+          }
+
+          val id = {
+            val pattern = "^.*?submissions/(\\d+)$".r
+            val pattern(idStr) = idUrl
+            idStr.toLong
+          }
+
+          Submission(
+            epochSecond = AtCoder.parseDateTimeToEpochSecond(time),
+            problemId = problemId,
+            userId = userId,
+            language = language,
+            point = point,
+            length = length,
+            result = result,
+            executionTime = executionTime,
+            id = id
+          )
+        } match {
+          case Success(submission) =>
+            Some(submission)
+          case Failure(e) =>
+            logger.catching(e)
+            None
         }
-      }
-
-      val id = {
-        val pattern = "^.*?submissions/(\\d+)$".r
-        val pattern(idStr) = idUrl
-        idStr.toLong
-      }
-
-      Submission(
-        epochSecond = AtCoder.parseDateTimeToEpochSecond(time),
-        problemId = problemId,
-        userId = userId,
-        language = language,
-        point = point,
-        length = length,
-        result = result,
-        executionTime = executionTime,
-        id = id
-      )
-    }
-    submissions.filter(_.result.matches("^[A-Z]+$")).toArray
+    submissions.flatten.filter(_.result.matches("^[A-Z]+$")).toArray
   }
 }
