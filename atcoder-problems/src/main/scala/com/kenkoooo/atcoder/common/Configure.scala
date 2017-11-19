1b3997c02c9e98b20728f4bd7d98c219f203ade2
kenkoooo <kenkou.n@gmail.com>
2017-11-19 14:56:44 +0900

    add config parameters

--- atcoder-problems/src/main/scala/com/kenkoooo/atcoder/common/Configure.scala
+++ atcoder-problems/src/main/scala/com/kenkoooo/atcoder/common/Configure.scala
@@ -1,7 +1,22 @@
 package com.kenkoooo.atcoder.common
 
-case class Configure(scraper: ScraperConfig, sql: SQLConfig)
+import java.io.File
+
+import com.typesafe.config.ConfigFactory.parseFile
+import pureconfig.loadConfig
+
+import scala.util.Try
+
+case class Configure(scraper: ScraperConfig, sql: SQLConfig, server: ServerConfig)
 
 case class ScraperConfig(threads: Int)
 
 case class SQLConfig(url: String, user: String, password: String)
+
+case class ServerConfig(port: Int)
+
+object Configure {
+  def apply(filepath: String): Try[Configure] = Try {
+    loadConfig[Configure](parseFile(new File(filepath))).right.get
+  }
+}
--- atcoder-problems/src/test/scala/com/kenkoooo/atcoder/common/ConfigureTest.scala
+++ atcoder-problems/src/test/scala/com/kenkoooo/atcoder/common/ConfigureTest.scala
@@ -8,8 +8,10 @@ import com.typesafe.config.ConfigFactory.parseString
 
 class ConfigureTest extends FunSuite with Matchers {
   test("parse JSON Configure") {
-    val config = loadConfig[Configure](parseString(Source.fromResource("test-conf.json").mkString))
-    config.right.get.scraper.threads shouldBe 11
-    config.right.get.sql.url shouldBe "sql-url"
+    val config =
+      loadConfig[Configure](parseString(Source.fromResource("test-conf.json").mkString)).right.get
+    config.scraper.threads shouldBe 11
+    config.sql.url shouldBe "sql-url"
+    config.server.port shouldBe 9090
   }
 }
