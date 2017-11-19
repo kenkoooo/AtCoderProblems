2d66bc2ef79467417bd8a2e2707513781f3b939a
kenkoooo <kenkou.n@gmail.com>
2017-11-19 22:33:03 +0900

    add experimental features

---
+++ atcoder-problems/src/main/scala/com/kenkoooo/atcoder/TestMain.scala
@@ -0,0 +1,15 @@
+package com.kenkoooo.atcoder
+
+import com.kenkoooo.atcoder.common.Configure
+import com.kenkoooo.atcoder.db.SqlClient
+
+import scala.util.{Failure, Success}
+
+object TestMain extends App {
+  Configure(args(0)) match {
+    case Success(config) =>
+      val sql = new SqlClient(config.sql.url, config.sql.user, config.sql.password)
+      println(sql.experiment.size)
+    case Failure(_) =>
+  }
+}
