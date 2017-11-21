f495a04f299e8a85b929feec362128e97c9ff53a
kenkoooo <kenkou.n@gmail.com>
2017-11-22 01:45:13 +0900

    add batch table updater

--- atcoder-problems/src/main/scala/com/kenkoooo/atcoder/StatisticsBatchMain.scala
+++ atcoder-problems/src/main/scala/com/kenkoooo/atcoder/StatisticsBatchMain.scala
@@ -1,3 +1,24 @@
 package com.kenkoooo.atcoder
 
-object StatisticsBatchMain {}
+import java.util.concurrent.{Executors, TimeUnit}
+
+import com.kenkoooo.atcoder.common.Configure
+import com.kenkoooo.atcoder.db.SqlClient
+import org.apache.logging.log4j.scala.Logging
+
+import scala.util.{Failure, Success}
+
+object StatisticsBatchMain extends App with Logging {
+  Configure(args(0)) match {
+    case Success(conf) =>
+      val sql =
+        new SqlClient(url = conf.sql.url, user = conf.sql.user, password = conf.sql.password)
+      val executor = Executors.newSingleThreadScheduledExecutor
+      executor.scheduleAtFixedRate(() => {
+        logger.info("start batch table update")
+        sql.batchUpdateStatisticTables()
+        logger.info("finished batch table update")
+      }, 0, 5, TimeUnit.SECONDS)
+    case Failure(e) => logger.catching(e)
+  }
+}
