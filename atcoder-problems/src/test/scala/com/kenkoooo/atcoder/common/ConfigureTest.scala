935dc8465b1929f6a89d73e0c31e0c73354ffcee
kenkoooo <kenkou.n@gmail.com>
2017-11-23 00:36:46 +0900

    add tests

--- atcoder-problems/src/test/scala/com/kenkoooo/atcoder/common/ConfigureTest.scala
+++ atcoder-problems/src/test/scala/com/kenkoooo/atcoder/common/ConfigureTest.scala
@@ -1,15 +1,11 @@
 package com.kenkoooo.atcoder.common
 
 import org.scalatest.{FunSuite, Matchers}
-import pureconfig.loadConfig
-
-import scala.io.Source
-import com.typesafe.config.ConfigFactory.parseString
 
 class ConfigureTest extends FunSuite with Matchers {
   test("parse JSON Configure") {
-    val config =
-      loadConfig[Configure](parseString(Source.fromResource("test-conf.json").mkString)).right.get
+    val path = getClass.getClassLoader.getResource("test-conf.json").getPath
+    val config = Configure(path).get
     config.scraper.threads shouldBe 11
     config.sql.url shouldBe "sql-url"
     config.server.port shouldBe 9090
