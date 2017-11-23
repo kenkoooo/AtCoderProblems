498d62854ab4a3604baad3c2b7528b5734dd0534
kenkoooo <kenkou.n@gmail.com>
2017-11-24 06:51:56 +0900

    add merged problem loader

---
+++ atcoder-problems/src/main/scala/com/kenkoooo/atcoder/model/MergedProblem.scala
@@ -0,0 +1,39 @@
+package com.kenkoooo.atcoder.model
+
+import com.kenkoooo.atcoder.common.TypeAnnotations.{ContestId, ProblemId, SubmissionId, UserId}
+import scalikejdbc._
+
+case class MergedProblem(id: ProblemId,
+                         contestId: ContestId,
+                         title: String,
+                         fastestSubmissionId: Option[SubmissionId],
+                         fastestUserId: Option[UserId],
+                         executionTime: Option[Int],
+                         firstSubmissionId: Option[SubmissionId],
+                         firstUserId: Option[UserId],
+                         shortestSubmissionId: Option[SubmissionId],
+                         shortestUserId: Option[UserId],
+                         sourceCodeLength: Option[Int],
+                         solverCount: Option[Int])
+
+object MergedProblem extends SQLSyntaxSupport[MergedProblem] {
+  def apply(problem: SyntaxProvider[Problem],
+            fastestSubmission: SyntaxProvider[Submission],
+            firstSubmission: SyntaxProvider[Submission],
+            shortestSubmission: SyntaxProvider[Submission],
+            solver: SyntaxProvider[ProblemSolver])(rs: WrappedResultSet): MergedProblem =
+    MergedProblem(
+      id = rs.string(problem.resultName.id),
+      contestId = rs.string(problem.resultName.contestId),
+      title = rs.string(problem.resultName.title),
+      fastestSubmissionId = rs.longOpt(fastestSubmission.resultName.id),
+      fastestUserId = rs.stringOpt(fastestSubmission.resultName.userId),
+      executionTime = rs.intOpt(fastestSubmission.resultName.executionTime),
+      firstSubmissionId = rs.longOpt(firstSubmission.resultName.id),
+      firstUserId = rs.stringOpt(firstSubmission.resultName.userId),
+      shortestSubmissionId = rs.longOpt(shortestSubmission.resultName.id),
+      shortestUserId = rs.stringOpt(shortestSubmission.resultName.userId),
+      sourceCodeLength = rs.intOpt(shortestSubmission.resultName.length),
+      solverCount = rs.intOpt(solver.resultName.userCount)
+    )
+}
