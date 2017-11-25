a9404010445df74c844b98cc4873acf6802528d1
kenkoooo <kenkou.n@gmail.com>
2017-11-26 08:37:10 +0900

    add hyperlink formatter

---
+++ atcoder-problems-frontend/src/utils/HtmlFormatter.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export class HtmlFormatter {
+    /**
+     * create hyperlink from url and link text
+     * @param url reference url
+     * @param text link text
+     */
+    static createLink(url: string, text: string): JSX.Element {
+        return (<a href={url} target="_blank">{text}</a>);
+    }
+}
