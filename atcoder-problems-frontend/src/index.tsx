5870fe4b6e48cb7f65e46100bb0aeca72c17823e
kenkoooo <kenkou.n@gmail.com>
2017-11-27 00:57:08 +0900

    format files

--- atcoder-problems-frontend/src/index.tsx
+++ atcoder-problems-frontend/src/index.tsx
@@ -5,9 +5,9 @@ import { Application } from "./components/Application";
 import { NavigationBar } from "./components/NavigationBar";
 
 ReactDOM.render(
-    <div>
-        <NavigationBar />
-        <Application />
-    </div>
-    , document.getElementById("root")
+  <div>
+    <NavigationBar />
+    <Application />
+  </div>,
+  document.getElementById("root")
 );
