f118a5088fa79b512714827fa864f5de0729ff40
kenkoooo <kenkou.n@gmail.com>
2017-11-28 02:27:39 +0900

    add serch form tests

---
+++ atcoder-problems-frontend/src/components/SearchForm.test.tsx
@@ -0,0 +1,18 @@
+import * as fs from "fs";
+import * as React from "react";
+import { SearchForm } from "./SearchForm";
+import { shallow, mount } from "enzyme";
+import { configure } from "enzyme";
+import * as Adapter from "enzyme-adapter-react-16";
+import toJson from "enzyme-to-json";
+import { SubmissionUtlis } from "../utils/SubmissionUtils";
+
+configure({ adapter: new Adapter() });
+test("render list view", () => {
+  let wrapper = mount(
+    <SearchForm
+      args={{ userId: "kenkoooo", rivals: ["chokudai", "iwiwi"], kind: "kind" }}
+    />
+  );
+  expect(toJson(wrapper)).toMatchSnapshot();
+});
