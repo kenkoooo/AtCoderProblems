7d6b6fc852f1e2cad9b0bd4e54fe33ce7008ac03
kenkoooo <kenkou.n@gmail.com>
2017-11-28 15:34:11 +0900

    move navbat to application

--- atcoder-problems-frontend/src/index.tsx
+++ atcoder-problems-frontend/src/index.tsx
@@ -2,11 +2,9 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Application } from "./components/Application";
-import { NavigationBar } from "./components/NavigationBar";
 
 ReactDOM.render(
   <div>
-    <NavigationBar />
     <Application />
   </div>,
   document.getElementById("root")
