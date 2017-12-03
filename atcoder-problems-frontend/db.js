d8fd42ff11ad1b24424f7b7017568ce6e006a9a3
kenkoooo <kenkou.n@gmail.com>
2017-12-03 16:15:52 +0900

    Split resource json files

---
+++ atcoder-problems-frontend/db.js
@@ -0,0 +1,12 @@
+module.exports = function() {
+  return {
+    "merged-problems": require("./resources/merged-problems.json"),
+    problems: require("./resources/problems.json"),
+    contests: require("./resources/contests.json"),
+    ac: require("./resources/ac.json"),
+    fast: require("./resources/fast.json"),
+    first: require("./resources/first.json"),
+    short: require("./resources/short.json"),
+    results: require("./resources/results.json")
+  };
+};
