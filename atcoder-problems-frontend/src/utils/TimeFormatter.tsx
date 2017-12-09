2b608d4f161126f2e0289cee57c4e2b9d2fb17e2
kenkoooo <kenkou.n@gmail.com>
2017-12-10 05:07:21 +0900

    Fix tests

--- atcoder-problems-frontend/src/utils/TimeFormatter.tsx
+++ atcoder-problems-frontend/src/utils/TimeFormatter.tsx
@@ -1,8 +1,8 @@
-import * as moment from "moment";
+import * as moment from "moment-timezone";
 
 export class TimeFormatter {
   static getDateString(currentTimeMillis: number): string {
     let date = moment(currentTimeMillis);
-    return date.local().format("Y-MM-DD");
+    return date.tz("Asia/Tokyo").format("Y-MM-DD");
   }
 }
