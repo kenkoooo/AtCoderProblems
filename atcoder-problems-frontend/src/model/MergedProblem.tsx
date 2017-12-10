74bb02b561cce15f9ef775ea5d1752cc0f07600d
kenkoooo <kenkou.n@gmail.com>
2017-12-10 09:48:47 +0900

    Format interface

--- atcoder-problems-frontend/src/model/MergedProblem.tsx
+++ atcoder-problems-frontend/src/model/MergedProblem.tsx
@@ -1,21 +1,25 @@
 import { Problem } from "./Problem";
 
 export interface MergedProblem extends Problem {
-  first_submission_id: number;
-  solver_count: number;
-  fastest_user_id: string;
-  execution_time: number;
-  shortest_user_id: string;
-  shortest_submission_id: number;
-  contestId: string;
   id: string;
-  fastest_submission_id: number;
-  first_user_id: string;
+  contestId: string;
+  solver_count: number;
   title: string;
-  source_code_length: number;
-  fastest_contest_id: string;
-  shortest_contest_id: string;
-  first_contest_id: string;
-  point: number;
-  predict: number;
+
+  first_user_id?: string;
+  first_contest_id?: string;
+  first_submission_id?: number;
+
+  fastest_user_id?: string;
+  fastest_contest_id?: string;
+  fastest_submission_id?: number;
+  execution_time?: number;
+
+  shortest_user_id?: string;
+  shortest_contest_id?: string;
+  shortest_submission_id?: number;
+  source_code_length?: number;
+
+  point?: number;
+  predict?: number;
 }
