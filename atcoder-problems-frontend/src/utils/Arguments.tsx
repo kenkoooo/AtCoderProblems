87beb073abea7dc6c6a83b8caba770a0649eb4e0
kenkoooo <kenkou.n@gmail.com>
2018-12-20 10:02:29 +0900

    Fixed TypeScript's compile errors

--- atcoder-problems-frontend/src/utils/Arguments.tsx
+++ atcoder-problems-frontend/src/utils/Arguments.tsx
@@ -1,4 +1,5 @@
 import * as QueryString from "query-string";
+import { string } from "prop-types";
 
 export interface Arguments {
   userId: string;
@@ -10,10 +11,10 @@ export interface Arguments {
 export class ArgumentParser {
   static parse(): Arguments {
     let params = QueryString.parse(location.search);
-    let userId: string = "user" in params ? params["user"] : "";
-    let rivals: string = "rivals" in params ? params["rivals"] : "";
-    let kind: string = "kind" in params ? params["kind"] : "category";
-    let ranking: string = "ranking" in params ? params["ranking"] : "ac";
+    let userId: string = ("user" in params && params["user"] instanceof string) ? params["user"].toString() : "";
+    let rivals: string = ("rivals" in params && params["rivals"] instanceof string) ? params["rivals"].toString() : "";
+    let kind: string = ("kind" in params && params["kind"] instanceof string) ? params["kind"].toString() : "category";
+    let ranking: string = ("ranking" in params && params["ranking"] instanceof string) ? params["ranking"].toString() : "ac";
     return {
       userId: userId,
       rivals: rivals.split(",").filter(r => r.length > 0),
