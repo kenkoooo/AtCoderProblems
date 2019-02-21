f54c5559d902c02ede1713e25af210d8fc69ff1b
Kenko Nakamura <kenkou.n@gmail.com>
2019-02-22 00:50:41 +0900

    Replace Moment.js to date-fns

--- atcoder-problems-frontend/src/utils/TimeFormatter.test.tsx
+++ atcoder-problems-frontend/src/utils/TimeFormatter.test.tsx
@@ -1,5 +1,5 @@
 import { TimeFormatter } from "./TimeFormatter";
 test("format time", () => {
-  let currentTimeMillis = 1511879207885;
-  expect(TimeFormatter.getDateString(currentTimeMillis)).toEqual("2017-11-28");
+  let currentTimeMillis = 1550761200000;
+  expect(TimeFormatter.getDateString(currentTimeMillis)).toEqual("2019-02-22");
 });
--- atcoder-problems-frontend/src/utils/TimeFormatter.tsx
+++ atcoder-problems-frontend/src/utils/TimeFormatter.tsx
@@ -1,8 +1,8 @@
-import * as moment from "moment-timezone";
+import { format } from "date-fns";
 
 export class TimeFormatter {
   static getDateString(currentTimeMillis: number): string {
-    let date = moment(currentTimeMillis);
-    return date.tz("Asia/Tokyo").format("Y-MM-DD");
+    let tokyo_string = new Date(currentTimeMillis).toLocaleString("en-US", { timeZone: "Asia/Tokyo" });
+    return format(new Date(tokyo_string), "YYYY-MM-DD");
   }
 }
