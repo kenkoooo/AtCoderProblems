3a4f8b7e76971c9c1176419f01c410c0c8dd553c
Kenko Nakamura <kenkou.n@gmail.com>
2019-03-04 22:24:27 +0900

    Small Refactoring

--- atcoder-problems-backend/src/main/scala/com/kenkoooo/atcoder/runner/AllSubmissionScrapingRunner.scala
+++ atcoder-problems-backend/src/main/scala/com/kenkoooo/atcoder/runner/AllSubmissionScrapingRunner.scala
@@ -8,17 +8,17 @@ import com.kenkoooo.atcoder.scraper.SubmissionScraper
   * runner of scraper to scrape all the submissions
   *
   * @param contestLoader     [[ContestLoader]] to get contest list
-  * @param sqlInsert         [[SqlInsert]]
+  * @param sqlInsert         [[SqlInsert]] to insert data to SQL
   * @param contests          the list of [[Contest]] to scrape
   * @param page              the page number to scrape
   * @param submissionScraper [[SubmissionScraper]] used in scraping
   */
 class AllSubmissionScrapingRunner(contestLoader: ContestLoader,
                                   sqlInsert: SqlInsert,
-                                  contests: List[Contest],
+                                  override val contests: List[Contest],
                                   private[runner] val page: Int = Submission.FirstPageNumber,
                                   submissionScraper: SubmissionScraper)
-    extends SubmissionScrapingRunner(contests) {
+    extends SubmissionScrapingRunner {
   override def scrapeOnePage(): AllSubmissionScrapingRunner = {
     val contest = contests.head
     val submissions = submissionScraper.scrape(contest.id, page)
