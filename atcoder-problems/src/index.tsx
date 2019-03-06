d164d5d61a6e93a79f464ca56f82d9d1bd15cf06
Kenko Nakamura <kenkou.n@gmail.com>
2019-03-06 23:25:16 +0900

    Moved App.tsx

--- atcoder-problems/src/index.tsx
+++ atcoder-problems/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
