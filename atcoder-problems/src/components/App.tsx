946306aa221ab05c690f971d244d1d624136c871
Kenko Nakamura <kenkou.n@gmail.com>
2019-03-06 22:44:40 +0900

    Working template

--- atcoder-problems/src/components/App.tsx
+++ atcoder-problems/src/components/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, NavLink as RouterLink, Switch } from "react-router-dom";
+import { NavLink, Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 function Home() {
   return (<p>a</p>);
@@ -11,12 +12,36 @@ function Work() {
 class App extends Component {
   render() {
     return (
-      <HashRouter>
+
+      <Router>
         <div>
+          <Navbar color="light" light expand="md">
+            <NavbarBrand tag={RouterLink} to="/">reactstrap</NavbarBrand>
+            <NavbarToggler />
+            <Collapse navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink tag={RouterLink} to="/b" className="nav-link">Components</NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink href="https://github.com/reactstrap/reactstrap" className="nav-link">GitHub</NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav caret>Options</DropdownToggle>
+                  <DropdownMenu right>
+                    <DropdownItem>Option 1</DropdownItem>
+                    <DropdownItem>Option 2</DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>Reset</DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </Nav>
+            </Collapse>
+          </Navbar>
           <Route exact path="/" component={Home} />
           <Route path="/b" component={Work} />
         </div>
-      </HashRouter>
+      </Router>
     );
   }
 }
--- atcoder-problems/src/index.tsx
+++ atcoder-problems/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
