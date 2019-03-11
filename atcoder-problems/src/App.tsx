697aa85a134f03ff8967a30041114237995ddc71
kenkoooo <kenkou.n@gmail.com>
2019-03-11 17:26:53 +0900

    Using HashBrowser

--- atcoder-problems/src/App.tsx
+++ atcoder-problems/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { Container } from "reactstrap";
 
 import ACRanking from "./pages/ACRanking";
