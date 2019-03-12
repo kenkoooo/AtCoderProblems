e7b533dfd1752e98836bb343c2a8656704566d27
Kenkou Nakamura <knakamura@indeed.com>
2019-03-12 10:23:09 +0900

    Refactored

--- atcoder-problems-frontend/src/utils/index.ts
+++ atcoder-problems-frontend/src/utils/index.ts
@@ -1,15 +1,15 @@
-export const isAccepted = (result: string) => result === 'AC';
+export const isAccepted = (result: string) => result === "AC";
 export const ordinalSuffixOf = (i: number) => {
-	const j = i % 10;
-	const k = i % 100;
-	if (j == 1 && k != 11) {
-		return 'st';
-	}
-	if (j == 2 && k != 12) {
-		return 'nd';
-	}
-	if (j == 3 && k != 13) {
-		return 'rd';
-	}
-	return 'th';
+  const j = i % 10;
+  const k = i % 100;
+  if (j == 1 && k != 11) {
+    return "st";
+  }
+  if (j == 2 && k != 12) {
+    return "nd";
+  }
+  if (j == 3 && k != 13) {
+    return "rd";
+  }
+  return "th";
 };
