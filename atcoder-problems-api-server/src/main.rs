5968c46f2bc26770f370c99a21ac5b4340f7559a
Kenko Nakamura <kenkou.n@gmail.com>
2019-03-15 00:57:38 +0900

    test postgresql connection

--- atcoder-problems-api-server/src/main.rs
+++ atcoder-problems-api-server/src/main.rs
@@ -1,3 +1,48 @@
+use postgres::{Connection, TlsMode};
+
 fn main() {
-    println!("Hello, world!");
+    let s = std::io::stdin();
+    let mut sc = Scanner { stdin: s.lock() };
+    let password: String = sc.read();
+    let conn = Connection::connect(
+        format!(
+            "postgresql://ubuntu:{}@atcoder.cxu3byr36ara.ap-northeast-1.rds.amazonaws.com/atcoder",
+            password
+        ),
+        TlsMode::None,
+    );
+    match conn {
+        Ok(_) => {}
+        Err(e) => {
+            println!("{:#?}", e);
+        }
+    }
+}
+
+pub struct Scanner<R> {
+    stdin: R,
+}
+
+impl<R: std::io::Read> Scanner<R> {
+    pub fn read<T: std::str::FromStr>(&mut self) -> T {
+        use std::io::Read;
+        let buf = self
+            .stdin
+            .by_ref()
+            .bytes()
+            .map(|b| b.unwrap())
+            .skip_while(|&b| b == b' ' || b == b'\n' || b == b'\r')
+            .take_while(|&b| b != b' ' && b != b'\n' && b != b'\r')
+            .collect::<Vec<_>>();
+        unsafe { std::str::from_utf8_unchecked(&buf) }
+            .parse()
+            .ok()
+            .expect("Parse error.")
+    }
+    pub fn vec<T: std::str::FromStr>(&mut self, n: usize) -> Vec<T> {
+        (0..n).map(|_| self.read()).collect()
+    }
+    pub fn chars(&mut self) -> Vec<char> {
+        self.read::<String>().chars().collect()
+    }
 }
