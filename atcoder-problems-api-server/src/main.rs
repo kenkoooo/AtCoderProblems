aa648df3e74c02130abcbf99b0d1cd3cb29e7232
Kenko Nakamura <kenkou.n@gmail.com>
2019-03-15 05:28:19 +0900

    separate files

--- atcoder-problems-api-server/src/main.rs
+++ atcoder-problems-api-server/src/main.rs
@@ -1,29 +1,12 @@
 use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
 
 use actix_web::{http, server, App, HttpRequest, HttpResponse};
 use postgres::types::{FromSql, ToSql};
 use postgres::{Connection, TlsMode};
 use regex::Regex;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 
-#[derive(Deserialize, Debug, Clone)]
-struct Config {
-    postgresql_user: String,
-    postgresql_pass: String,
-    postgresql_host: String,
-}
-impl Config {
-    fn create_from_file<P: AsRef<Path>>(path: P) -> Result<Config, Box<Error>> {
-        let file = File::open(path)?;
-        let reader = BufReader::new(file);
-        let config = serde_json::from_reader(reader)?;
-        Ok(config)
-    }
-}
+use atcoder_problems_api_server::config::Config;
 
 #[derive(Serialize, Debug)]
 struct Submission {
