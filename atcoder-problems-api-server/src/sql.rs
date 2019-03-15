d751a031796773e26c95b674fdb1c461bd8e40e3
Kenko Nakamura <kenkou.n@gmail.com>
2019-03-15 22:30:47 +0900

    Simplify

--- atcoder-problems-api-server/src/sql.rs
+++ atcoder-problems-api-server/src/sql.rs
@@ -25,7 +25,40 @@ impl SqlConnector {
 
 impl ConnectorTrait for SqlConnector {
     fn get_submissions(&self, user: &str) -> Result<Vec<Submission>, String> {
-        get_submissions(user, &self.conn)
+        let query = r#"
+            SELECT
+                id, 
+                epoch_second, 
+                problem_id, 
+                contest_id, 
+                user_id, 
+                language, 
+                point, 
+                length, 
+                result, 
+                execution_time
+            FROM submissions
+            WHERE user_id=$1"#;
+        let rows = self
+            .conn
+            .query(query, &[&user])
+            .map_err(|e| format!("{:?}", e))?;
+        let submissions = rows
+            .iter()
+            .map(|row| Submission {
+                id: row.get("id"),
+                epoch_second: row.get("epoch_second"),
+                problem_id: row.get("problem_id"),
+                contest_id: row.get("contest_id"),
+                user_id: row.get("user_id"),
+                language: row.get("language"),
+                point: row.get("point"),
+                length: row.get("length"),
+                result: row.get("result"),
+                execution_time: row.get("execution_time"),
+            })
+            .collect::<Vec<_>>();
+        Ok(submissions)
     }
 
     fn get_user_info(&self, user_id: &str) -> Result<UserInfo, String> {
@@ -41,42 +74,6 @@ impl ConnectorTrait for SqlConnector {
             rated_point_sum_rank,
         })
     }
-}
-
-fn get_submissions(user: &str, conn: &Connection) -> Result<Vec<Submission>, String> {
-    let query = r#"
-            SELECT
-                id, 
-                epoch_second, 
-                problem_id, 
-                contest_id, 
-                user_id, 
-                language, 
-                point, 
-                length, 
-                result, 
-                execution_time
-            FROM submissions
-            WHERE user_id=$1"#;
-    let rows = conn
-        .query(query, &[&user])
-        .map_err(|e| format!("{:?}", e))?;
-    let submissions = rows
-        .iter()
-        .map(|row| Submission {
-            id: row.get("id"),
-            epoch_second: row.get("epoch_second"),
-            problem_id: row.get("problem_id"),
-            contest_id: row.get("contest_id"),
-            user_id: row.get("user_id"),
-            language: row.get("language"),
-            point: row.get("point"),
-            length: row.get("length"),
-            result: row.get("result"),
-            execution_time: row.get("execution_time"),
-        })
-        .collect::<Vec<_>>();
-    Ok(submissions)
 }
 
 fn get_count_rank<T: FromSql + ToSql>(
