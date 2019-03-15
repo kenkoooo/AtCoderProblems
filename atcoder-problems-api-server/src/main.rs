c8ddf3966e3621be6925203c2f41b36491f13699
Kenkou Nakamura <knakamura@indeed.com>
2019-03-15 15:44:09 +0900

    Added CORS

--- atcoder-problems-api-server/src/main.rs
+++ atcoder-problems-api-server/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 
+use actix_web::middleware::cors::Cors;
 use actix_web::{http, server, App, HttpRequest, HttpResponse};
 use regex::Regex;
 
@@ -26,9 +27,17 @@ fn main() {
     let config = Config::create_from_file(&args[1]).unwrap();
 
     server::new(move || {
-        App::with_state(config.clone())
-            .route("/results", http::Method::GET, result_api)
-            .route("/v2/user_info", http::Method::GET, user_info_api)
+        App::with_state(config.clone()).configure(|app| {
+            Cors::for_app(app)
+                .allowed_origin("*")
+                .resource("/results", |r| {
+                    r.method(http::Method::GET).with(result_api);
+                })
+                .resource("/v2/user_info", |r| {
+                    r.method(http::Method::GET).with(user_info_api);
+                })
+                .register()
+        })
     })
     .bind("0.0.0.0:8080")
     .unwrap()
