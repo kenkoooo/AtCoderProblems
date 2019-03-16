f2be80009d6728bd12c41601f1bebe4778607c30
Kenko Nakamura <kenkou.n@gmail.com>
2019-03-17 02:07:41 +0900

    refactor

--- atcoder-problems-api-server/src/main.rs
+++ atcoder-problems-api-server/src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
 
-use actix_web::middleware::cors::Cors;
-use actix_web::{http, server, App};
+use actix_web::{server, App};
 
 use atcoder_problems_api_server::api;
 use atcoder_problems_api_server::config::Config;
@@ -16,20 +15,8 @@ fn main() {
         &config.postgresql_host,
     );
 
-    server::new(move || {
-        App::with_state(connector.clone()).configure(|app| {
-            Cors::for_app(app)
-                .allowed_origin("*")
-                .resource("/results", |r| {
-                    r.method(http::Method::GET).with(api::result_api);
-                })
-                .resource("/v2/user_info", |r| {
-                    r.method(http::Method::GET).with(api::user_info_api);
-                })
-                .register()
-        })
-    })
-    .bind("0.0.0.0:8080")
-    .unwrap()
-    .run();
+    server::new(move || App::with_state(connector.clone()).configure(api::server_config))
+        .bind("0.0.0.0:8080")
+        .unwrap()
+        .run();
 }
