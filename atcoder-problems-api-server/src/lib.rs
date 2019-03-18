327b23a769297c3fc3bf7879e887db4f9134925c
Kenkou Nakamura <knakamura@indeed.com>
2019-03-18 17:46:23 +0900

    Added tests

--- atcoder-problems-api-server/src/lib.rs
+++ atcoder-problems-api-server/src/lib.rs
@@ -2,9 +2,9 @@ pub mod api;
 pub mod config;
 pub mod sql;
 
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Submission {
     id: i64,
     epoch_second: i64,
@@ -18,47 +18,11 @@ pub struct Submission {
     execution_time: Option<i32>,
 }
 
-#[derive(Serialize, Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct UserInfo {
     user_id: String,
     accepted_count: i32,
     accepted_count_rank: usize,
     rated_point_sum: f64,
     rated_point_sum_rank: usize,
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use actix_web::http;
-    use actix_web::test::TestServer;
-    use sql::ConnectorTrait;
-
-    #[derive(Clone)]
-    struct TestConnector {}
-    impl ConnectorTrait for TestConnector {
-        fn get_submissions(&self, user_id: &str) -> Result<Vec<Submission>, String> {
-            unimplemented!()
-        }
-        fn get_user_info(&self, user_id: &str) -> Result<UserInfo, String> {
-            unimplemented!()
-        }
-    }
-
-    #[test]
-    fn name() {
-        let conn = TestConnector {};
-        let mut srv = TestServer::build_with_state(move || conn.clone()).start(|app| {
-            app.resource("/results", |r| {
-                r.method(http::Method::GET).with(api::result_api);
-            })
-            .resource("/v2/user_info", |r| {
-                r.method(http::Method::GET).with(api::user_info_api);
-            });
-        });
-
-        let req = srv.client(http::Method::GET, "/results").finish().unwrap();
-        let response = srv.execute(req.send()).unwrap();
-        assert!(response.status().is_success());
-    }
 }
