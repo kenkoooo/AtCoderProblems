5c5afe4384bc48d54417e74337794b0acbd26286
Kenko Nakamura <kenkou.n@gmail.com>
2019-03-19 04:03:49 +0900

    Removed backend

--- atcoder-problems-backend/src/main/scala/com/kenkoooo/atcoder/runner/SubmissionScrapingRunner.scala
+++
@@ -1,8 +0,0 @@
-package com.kenkoooo.atcoder.runner
-
-import com.kenkoooo.atcoder.model.Contest
-
-abstract class SubmissionScrapingRunner(private[runner] val contests: List[Contest]) {
-  require(contests.nonEmpty, "contest list must not be empty")
-  def scrapeOnePage(): SubmissionScrapingRunner
-}
