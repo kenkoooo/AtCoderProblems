2369e648285f0679b4872c0d587f81b84f301496
kenkoooo <kenkou.n@gmail.com>
2019-03-18 21:43:24 +0900

    Added Logger

--- atcoder-problems-api-server/src/main.rs
+++ atcoder-problems-api-server/src/main.rs
@@ -3,11 +3,15 @@ use std::env;
 use actix_web::{http, server, App, HttpResponse};
 
 use actix_web::middleware::cors::Cors;
+use actix_web::middleware::Logger;
 use atcoder_problems_api_server::api::*;
 use atcoder_problems_api_server::config::Config;
 use atcoder_problems_api_server::sql::SqlConnector;
 
 fn main() {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     let args: Vec<String> = env::args().collect();
     let config = Config::create_from_file(&args[1]).unwrap();
     let connector = SqlConnector::new(
@@ -17,18 +21,21 @@ fn main() {
     );
 
     server::new(move || {
-        App::with_state(connector.clone()).configure(|app| {
-            Cors::for_app(app)
-                .allowed_origin("*")
-                .resource("/", |r| r.f(|_| HttpResponse::Ok()))
-                .resource("/results", |r| {
-                    r.method(http::Method::GET).with(result_api);
-                })
-                .resource("/v2/user_info", |r| {
-                    r.method(http::Method::GET).with(user_info_api);
-                })
-                .register()
-        })
+        App::with_state(connector.clone())
+            .configure(|app| {
+                Cors::for_app(app)
+                    .allowed_origin("*")
+                    .resource("/", |r| r.f(|_| HttpResponse::Ok()))
+                    .resource("/results", |r| {
+                        r.method(http::Method::GET).with(result_api);
+                    })
+                    .resource("/v2/user_info", |r| {
+                        r.method(http::Method::GET).with(user_info_api);
+                    })
+                    .register()
+            })
+            .middleware(Logger::default())
+            .middleware(Logger::new("%a %{User-Agent}i"))
     })
     .bind("0.0.0.0:8080")
     .unwrap()
