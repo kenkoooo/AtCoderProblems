557a94ccd0036c14c8a3ab517709022349cb4b50
Kenko Nakamura <kenkou.n@gmail.com>
2019-03-22 04:17:05 +0900

    Make submission_crawler single thread

--- atcoder-problems-scraper/src/bin/submission_crawler.rs
+++ atcoder-problems-scraper/src/bin/submission_crawler.rs
@@ -2,80 +2,100 @@ use atcoder_problems_scraper::scraper;
 use atcoder_problems_scraper::sql::SqlClient;
 use chrono::{Duration, Utc};
 use diesel::pg::PgConnection;
+use diesel::Connection;
 use env_logger;
 use log::info;
-use r2d2;
-use r2d2_diesel::ConnectionManager;
 use std::env;
-use std::{thread, time};
+
+enum Runner {
+    AllSubmissions,
+    RecentContests,
+    NewSubmissions,
+}
+struct State {
+    page: usize,
+    contests: Vec<String>,
+    runner: Runner,
+}
 
 fn main() {
     env::set_var("RUST_LOG", "info");
     env_logger::init();
 
     let url = env::var("SQL_URL").expect("SQL_URL must be set.");
-    let manager = ConnectionManager::<PgConnection>::new(url.as_str());
-    let pool = r2d2::Pool::builder()
-        .build(manager)
-        .expect("Failed to create pool.");
-    let all_submission_crawler = {
-        let pool = pool.clone();
-        thread::spawn(move || loop {
-            let conn = pool.get().expect("Failed to get a connection.");
-            let contests = conn.get_contests().unwrap();
-            for contest in contests.iter() {
-                let max_page = scraper::get_max_submission_page(&contest.id).unwrap();
-                for page in (1..=max_page).rev() {
-                    info!("all submission crawling: {} {}", contest.id, page);
-                    let submissions = scraper::scrape_submissions(&contest.id, page);
-                    conn.insert_submissions(&submissions).unwrap();
-                    thread::sleep(time::Duration::from_millis(500));
-                }
-            }
-        })
-    };
+    let conn = PgConnection::establish(&url).expect("Failed to connect PostgreSQL.");
 
-    let new_submission_crawler = {
-        let pool = pool.clone();
-        thread::spawn(move || loop {
-            let conn = pool.get().expect("Failed to get a connection.");
-            let contests = conn.get_contests().unwrap();
-            for contest in contests.iter() {
-                for page in 1..=2 {
-                    info!("new submission crawling: {} {}", contest.id, page);
-                    let submissions = scraper::scrape_submissions(&contest.id, page);
-                    conn.insert_submissions(&submissions).unwrap();
-                    thread::sleep(time::Duration::from_millis(500));
-                }
-            }
-        })
-    };
+    let mut states = [
+        State {
+            page: 0,
+            contests: vec![],
+            runner: Runner::AllSubmissions,
+        },
+        State {
+            page: 0,
+            contests: vec![],
+            runner: Runner::RecentContests,
+        },
+        State {
+            page: 0,
+            contests: vec![],
+            runner: Runner::NewSubmissions,
+        },
+    ];
 
-    let recent_submission_crawler = thread::spawn(move || loop {
-        let conn = pool.get().expect("Failed to get a connection.");
-        let now = Utc::now().timestamp();
-        let contests = conn
-            .get_contests()
-            .unwrap()
-            .into_iter()
-            .filter(|contest| now - contest.start_epoch_second < Duration::days(7).num_seconds())
-            .collect::<Vec<_>>();
-        if contests.is_empty() {
-            thread::sleep(time::Duration::from_secs(3600));
-        } else {
-            for contest in contests.iter() {
-                let max_page = scraper::get_max_submission_page(&contest.id).unwrap();
-                for page in (1..=max_page).rev() {
-                    info!("recent submission crawling: {} {}", contest.id, page);
-                    let submissions = scraper::scrape_submissions(&contest.id, page);
-                    conn.insert_submissions(&submissions).unwrap();
-                    thread::sleep(time::Duration::from_millis(500));
+    loop {
+        for state in states.iter_mut() {
+            if state.page > 0 {
+                assert!(state.contests.len() > 0);
+                let page = state.page;
+                let contest_id = state.contests[0].clone();
+                info!("crawling: {} {}", contest_id, page);
+                let submissions = scraper::scrape_submissions(&contest_id, page);
+                conn.insert_submissions(&submissions).unwrap();
+                state.page -= 1;
+                if state.page == 0 {
+                    state.contests.remove(0);
+                }
+            } else if state.contests.len() > 0 {
+                let contest_id = state.contests[0].clone();
+                match state.runner {
+                    Runner::AllSubmissions | Runner::RecentContests => {
+                        // all | recent
+                        let max_page = scraper::get_max_submission_page(&contest_id)
+                            .expect("Failed to scrape page list");
+                        state.page = max_page;
+                    }
+                    Runner::NewSubmissions => {
+                        state.page = 2;
+                    }
+                }
+            } else {
+                assert_eq!(state.page, 0);
+                assert!(state.contests.is_empty());
+                match state.runner {
+                    Runner::AllSubmissions | Runner::NewSubmissions => {
+                        // all | new
+                        state.contests = conn
+                            .get_contests()
+                            .expect("Failed to get contests")
+                            .into_iter()
+                            .map(|c| c.id)
+                            .collect();
+                    }
+                    Runner::RecentContests => {
+                        let now = Utc::now().timestamp();
+                        state.contests = conn
+                            .get_contests()
+                            .expect("Failed to load contests")
+                            .into_iter()
+                            .filter(|contest| {
+                                now - contest.start_epoch_second < Duration::days(7).num_seconds()
+                            })
+                            .map(|c| c.id)
+                            .collect::<Vec<_>>();
+                    }
                 }
             }
         }
-    });
-
-    all_submission_crawler.join().unwrap();
-    new_submission_crawler.join().unwrap();
-    recent_submission_crawler.join().unwrap();
+    }
 }
