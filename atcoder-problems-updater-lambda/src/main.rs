18cd508123ec8df8373092961f988f0d3392480b
Kenko Nakamura <kenkou.n@gmail.com>
2019-03-22 22:24:38 +0900

    Replace println! with info!

--- atcoder-problems-updater-lambda/src/main.rs
+++ atcoder-problems-updater-lambda/src/main.rs
@@ -1,4 +1,8 @@
 #[macro_use]
+extern crate log;
+extern crate simple_logger;
+
+#[macro_use]
 extern crate lambda_runtime as lambda;
 extern crate openssl;
 
@@ -10,7 +14,9 @@ use std::env;
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    simple_logger::init_with_level(log::Level::Info)?;
     openssl_probe::init_ssl_cert_env_vars();
+    info!("Started!");
     lambda!(my_handler);
 
     Ok(())
@@ -20,25 +26,25 @@ fn my_handler(_: String, c: lambda::Context) -> Result<String, HandlerError> {
     let url = env::var("SQL_URL").map_err(|_| c.new_error("SQL_URL must be set."))?;
     let conn = PgConnection::establish(&url).map_err(|_| c.new_error("Failed to connect."))?;
 
-    println!("Executing update_accepted_count...");
+    info!("Executing update_accepted_count...");
     conn.update_accepted_count()
         .map_err(|_| c.new_error("Failed update_accepted_count"))?;
-    println!("Executing update_problem_solver_count...");
+    info!("Executing update_problem_solver_count...");
     conn.update_problem_solver_count()
         .map_err(|_| c.new_error("Failed update_problem_solver_count"))?;
-    println!("Executing update_rated_point_sums...");
+    info!("Executing update_rated_point_sums...");
     conn.update_rated_point_sums()
         .map_err(|_| c.new_error("Failed update_rated_point_sums"))?;
-    println!("Executing update_language_count...");
+    info!("Executing update_language_count...");
     conn.update_language_count()
         .map_err(|_| c.new_error("Failed update_language_count"))?;
-    println!("Executing update_great_submissions...");
+    info!("Executing update_great_submissions...");
     conn.update_great_submissions()
         .map_err(|_| c.new_error("Failed update_great_submissions"))?;
-    println!("Executing aggregate_great_submissions...");
+    info!("Executing aggregate_great_submissions...");
     conn.aggregate_great_submissions()
         .map_err(|_| c.new_error("Failed aggregate_great_submissions"))?;
-    println!("Executing update_problem_points...");
+    info!("Executing update_problem_points...");
     conn.update_problem_points()
         .map_err(|_| c.new_error("Failed update_problem_points"))?;
     Ok("Finished".to_owned())
