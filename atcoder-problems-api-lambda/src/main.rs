71d947f7740bbed0951002902bb71a987ac8cafe
kenkoooo <kenkou.n@gmail.com>
2019-03-26 19:41:50 +0900

    Added epoch_second filter

--- atcoder-problems-api-lambda/src/main.rs
+++ atcoder-problems-api-lambda/src/main.rs
@@ -11,9 +11,8 @@ use diesel::prelude::*;
 use diesel::{Connection, PgConnection};
 use lambda::error::HandlerError;
 use openssl_probe;
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use serde_json;
-use serde_json::Value;
 use simple_logger;
 use std::collections::HashMap;
 use std::env;
@@ -30,12 +29,38 @@ struct CustomOutput {
 }
 
 #[derive(Serialize)]
-struct UserInfo<'a> {
-    user_id: &'a str,
+struct UserInfo {
+    user_id: String,
     accepted_count: i32,
     accepted_count_rank: i64,
     rated_point_sum: f64,
     rated_point_sum_rank: i64,
+}
+
+#[derive(Deserialize)]
+struct LambdaInput {
+    #[serde(rename = "pathParameters")]
+    path_parameters: LambdaInputPathParameters,
+
+    #[serde(rename = "queryStringParameters")]
+    query_string_parameters: LambdaInputQueryParameters,
+}
+
+#[derive(Deserialize)]
+struct LambdaInputPathParameters {
+    #[serde(rename = "proxy")]
+    path: String,
+}
+
+#[derive(Deserialize)]
+struct LambdaInputQueryParameters {
+    user: Option<String>,
+    from: Option<String>,
+}
+
+enum SubmissionAPIParam<'a> {
+    UserSubmission { user_id: &'a str },
+    TimeSubmission { from_epoch_second: i64 },
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -45,7 +70,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn my_handler(e: Value, c: lambda::Context) -> Result<CustomOutput, HandlerError> {
+fn map_params<'a>(params: &'a LambdaInputQueryParameters) -> Option<SubmissionAPIParam<'a>> {
+    params
+        .user
+        .as_ref()
+        .map(|user_id| SubmissionAPIParam::UserSubmission { user_id })
+        .or_else(|| {
+            params
+                .from
+                .as_ref()
+                .and_then(|from| from.parse::<i64>().ok())
+                .map(|from_epoch_second| SubmissionAPIParam::TimeSubmission { from_epoch_second })
+        })
+}
+
+fn my_handler(e: LambdaInput, c: lambda::Context) -> Result<CustomOutput, HandlerError> {
     let url = env::var("SQL_URL").map_err(|_| c.new_error("SQL_URL must be set"))?;
     let conn =
         PgConnection::establish(&url).map_err(|_| c.new_error("Failed to connect to SQL"))?;
@@ -53,34 +92,41 @@ fn my_handler(e: Value, c: lambda::Context) -> Result<CustomOutput, HandlerError
     let mut headers = HashMap::new();
     headers.insert("Access-Control-Allow-Origin".to_owned(), "*".to_owned());
 
-    let path = e
-        .get("pathParameters")
-        .ok_or_else(|| c.new_error("Input doesn't have pathParameters"))?
-        .get("proxy")
-        .ok_or_else(|| c.new_error("Input doesn't have pathParameters.proxy"))?
-        .as_str()
-        .ok_or_else(|| c.new_error("pathParameters.proxy is not a string"))?;
-    let user_id = e
-        .get("queryStringParameters")
-        .ok_or_else(|| c.new_error("Input doesn't have queryStringParameters"))?
-        .get("user")
-        .ok_or_else(|| c.new_error("Input doesn't have queryStringParameters.user"))?
-        .as_str()
-        .ok_or_else(|| c.new_error("queryStringParameters.user is not a string"))?;
+    let path = e.path_parameters.path;
+    let params = e.query_string_parameters;
 
-    info!("Loading {}:{}", path, user_id);
-    match path {
+    match path.as_str() {
         "results" => {
-            let submissions = submissions::table
-                .filter(submissions::user_id.eq(user_id))
-                .load::<Submission>(&conn)
-                .map_err(|_| c.new_error("Failed to load submissions"))?;
-            serde_json::to_string(&submissions)
-                .map_err(|_| c.new_error("Failed to convert submissions to JSON"))
+            info!("Submission API");
+            let params = map_params(&params).ok_or_else(|| c.new_error("Failed to load params"))?;
+            match params {
+                SubmissionAPIParam::UserSubmission { user_id } => {
+                    let submissions = submissions::table
+                        .filter(submissions::user_id.eq(user_id))
+                        .load::<Submission>(&conn)
+                        .map_err(|_| c.new_error("Failed to load submissions"))?;
+                    serde_json::to_string(&submissions)
+                        .map_err(|_| c.new_error("Failed to convert submissions to JSON"))
+                }
+                SubmissionAPIParam::TimeSubmission { from_epoch_second } => {
+                    let submissions = submissions::table
+                        .filter(submissions::epoch_second.ge(from_epoch_second))
+                        .order_by(submissions::epoch_second.asc())
+                        .limit(1000)
+                        .load::<Submission>(&conn)
+                        .map_err(|_| c.new_error("Failed to load submissions"))?;
+                    serde_json::to_string(&submissions)
+                        .map_err(|_| c.new_error("Failed to convert submissions to JSON"))
+                }
+            }
         }
         "v2/user_info" => {
+            info!("UserInfo API");
+            let user_id = params
+                .user
+                .ok_or_else(|| c.new_error("user_id is required"))?;
             let accepted_count = accepted_count::table
-                .filter(accepted_count::user_id.eq(user_id))
+                .filter(accepted_count::user_id.eq(&user_id))
                 .select(accepted_count::problem_count)
                 .first::<i32>(&conn)
                 .map_err(|_| c.new_error("Failed to load accepted_count"))?;
@@ -90,7 +136,7 @@ fn my_handler(e: Value, c: lambda::Context) -> Result<CustomOutput, HandlerError
                 .first::<i64>(&conn)
                 .map_err(|_| c.new_error("Failed to load accepted_count_rank"))?;
             let rated_point_sum = rated_point_sum::table
-                .filter(rated_point_sum::user_id.eq(user_id))
+                .filter(rated_point_sum::user_id.eq(&user_id))
                 .select(rated_point_sum::point_sum)
                 .first::<f64>(&conn)
                 .map_err(|_| c.new_error("Failed to load rated_point_sum"))?;
