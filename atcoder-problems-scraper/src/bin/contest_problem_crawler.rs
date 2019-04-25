14c1671131aa699dabe084eab37a40ef2d6d20bf
kenkoooo <kenkou.n@gmail.com>
2019-04-25 19:32:23 +0900

    Made contest_problem_crawler a single task

--- atcoder-problems-scraper/src/bin/contest_problem_crawler.rs
+++ atcoder-problems-scraper/src/bin/contest_problem_crawler.rs
@@ -14,48 +14,43 @@ fn main() {
     let url = env::var("SQL_URL").expect("SQL_URL must be set.");
     let conn = PgConnection::establish(&url).unwrap();
 
-    loop {
-        info!("Starting...");
-        let contests: Vec<_> = (1..)
-            .map(|page| {
-                info!("crawling contest page-{}", page);
-                match scraper::scrape_contests(page) {
-                    Ok(contests) => {
-                        thread::sleep(time::Duration::from_secs(1));
-                        contests
-                    }
-                    Err(_) => Vec::new(),
+    info!("Starting...");
+    let contests: Vec<_> = (1..)
+        .map(|page| {
+            info!("crawling contest page-{}", page);
+            match scraper::scrape_contests(page) {
+                Ok(contests) => {
+                    thread::sleep(time::Duration::from_secs(1));
+                    contests
                 }
-            })
-            .take_while(|contests| !contests.is_empty())
-            .flatten()
-            .collect();
-
-        info!("There are {} contests.", contests.len());
-        conn.insert_contests(&contests).unwrap();
-
-        for contest in contests.into_iter() {
-            info!("Crawling problems of {},,,", contest.id);
-            match scraper::scrape_problems(&contest.id) {
-                Ok(problems) => {
-                    info!("Inserting {} problems...", problems.len());
-                    conn.insert_problems(&problems)
-                        .expect("Failed to insert problems");
-                    conn.insert_contest_problem_pair(
-                        &problems
-                            .iter()
-                            .map(|problem| (problem.contest_id.as_str(), problem.id.as_str()))
-                            .collect::<Vec<_>>(),
-                    )
-                    .expect("Failed to insert contest-problem pairs");
-                }
-                Err(e) => error!("{}", e),
+                Err(_) => Vec::new(),
             }
+        })
+        .take_while(|contests| !contests.is_empty())
+        .flatten()
+        .collect();
 
-            thread::sleep(time::Duration::from_secs(5));
+    info!("There are {} contests.", contests.len());
+    conn.insert_contests(&contests).unwrap();
+
+    for contest in contests.into_iter() {
+        info!("Crawling problems of {},,,", contest.id);
+        match scraper::scrape_problems(&contest.id) {
+            Ok(problems) => {
+                info!("Inserting {} problems...", problems.len());
+                conn.insert_problems(&problems)
+                    .expect("Failed to insert problems");
+                conn.insert_contest_problem_pair(
+                    &problems
+                        .iter()
+                        .map(|problem| (problem.contest_id.as_str(), problem.id.as_str()))
+                        .collect::<Vec<_>>(),
+                )
+                .expect("Failed to insert contest-problem pairs");
+            }
+            Err(e) => error!("{}", e),
         }
 
-        info!("Suspending...");
-        thread::sleep(time::Duration::from_secs(3600));
+        thread::sleep(time::Duration::from_secs(5));
     }
 }
