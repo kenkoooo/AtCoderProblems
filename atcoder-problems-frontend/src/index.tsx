da372ab137bd5121ead278a941a4bcfe63789dcb
Yang-33 <kasai.yuta0810@gmail.com>
2019-05-14 23:37:31 +0900

    Added css for small screen

--- atcoder-problems-frontend/src/index.tsx
+++ atcoder-problems-frontend/src/index.tsx
@@ -1,10 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
