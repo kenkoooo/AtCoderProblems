ff61938e877ec2be728e6f2b7c37facb4f662447
kenkoooo <kenkou.n@gmail.com>
2019-06-08 02:45:43 +0900

    Refactoring

--- atcoder-problems-backend/src/sql.rs
+++ atcoder-problems-backend/src/sql.rs
@@ -1,10 +1,10 @@
 mod accepted_count;
 pub mod client;
-pub mod delta_update;
 mod language_count;
 pub mod models;
 mod rated_point_sum;
 pub mod schema;
+mod submission_client;
 pub mod update;
 
 pub const FIRST_AGC_EPOCH_SECOND: i64 = 1_468_670_400;
@@ -13,3 +13,4 @@ pub const UNRATED_STATE: &str = "-";
 pub use accepted_count::AcceptedCountUpdater;
 pub use language_count::LanguageCountUpdater;
 pub use rated_point_sum::RatedPointSumUpdater;
+pub use submission_client::{SubmissionClient, SubmissionRequest};
---
+++ atcoder-problems-backend/src/sql/submission_client.rs
@@ -0,0 +1,39 @@
+use crate::sql::models::Submission;
+use crate::sql::schema::submissions;
+
+use diesel::prelude::*;
+use diesel::{PgConnection, QueryResult};
+
+pub enum SubmissionRequest<'a> {
+    UserAll { user_id: &'a str },
+    UsersAccepted { user_ids: &'a [&'a str] },
+    FromTime { from_second: i64, count: i64 },
+    RecentAccepted { count: i64 },
+}
+
+pub trait SubmissionClient {
+    fn get_submissions(&self, request: SubmissionRequest) -> QueryResult<Vec<Submission>>;
+}
+
+impl SubmissionClient for PgConnection {
+    fn get_submissions(&self, request: SubmissionRequest) -> QueryResult<Vec<Submission>> {
+        match request {
+            SubmissionRequest::UserAll { user_id } => submissions::table
+                .filter(submissions::user_id.eq(user_id))
+                .load(self),
+            SubmissionRequest::FromTime { from_second, count } => submissions::table
+                .filter(submissions::epoch_second.ge(from_second))
+                .limit(count)
+                .load(self),
+            SubmissionRequest::RecentAccepted { count } => submissions::table
+                .filter(submissions::result.eq("AC"))
+                .order(submissions::id.desc())
+                .limit(count)
+                .load(self),
+            SubmissionRequest::UsersAccepted { user_ids } => submissions::table
+                .filter(submissions::result.eq("AC"))
+                .filter(submissions::user_id.eq_any(user_ids))
+                .load(self),
+        }
+    }
+}
