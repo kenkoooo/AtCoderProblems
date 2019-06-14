efe24013d21c5fa8246b036e745c0c0187f9492d
kenkoooo <kenkou.n@gmail.com>
2019-06-15 04:26:10 +0900

    Moved to crawler

--- atcoder-problems-backend/src/scraper.rs
+++ atcoder-problems-backend/src/scraper.rs
@@ -1,11 +1,11 @@
 mod contest;
 mod problem;
-mod results;
 mod submission;
 
-use crate::sql::models::{Contest, Problem, Submission};
+use crate::sql::models::{Contest, Performance, Problem, Submission};
 
-pub use self::results::get_performances;
+use serde::Deserialize;
+use std::collections::BTreeMap;
 
 const ATCODER_HOST: &str = "https://atcoder.jp";
 
@@ -25,6 +25,7 @@ pub trait ScraperTrait {
         page: Option<usize>,
     ) -> Option<(Vec<Submission>, usize)>;
     fn scrape_problems(&self, contest_id: &str) -> Option<Vec<Problem>>;
+    fn scrape_performances(&self, contest_id: &str) -> Option<Vec<Performance>>;
 }
 
 pub struct Scraper;
@@ -56,5 +57,70 @@ impl ScraperTrait for Scraper {
         let url = format!("{}/contests/{}/tasks", ATCODER_HOST, contest_id);
         let page_html = get_html(&url).ok()?;
         problem::scrape(&page_html, contest_id)
+    }
+
+    fn scrape_performances(&self, contest_id: &str) -> Option<Vec<Performance>> {
+        #[derive(Deserialize, Debug, Clone)]
+        struct ContestPerformance {
+            #[serde(rename = "Place")]
+            place: u64,
+
+            #[serde(rename = "Performance")]
+            inner_performance: i64,
+        }
+
+        #[derive(Deserialize, Debug, Clone)]
+        struct ContestStandings {
+            #[serde(rename = "StandingsData")]
+            standings: Vec<ContestStanding>,
+        }
+
+        #[derive(Deserialize, Debug, Clone)]
+        struct ContestStanding {
+            #[serde(rename = "Rank")]
+            place: u64,
+
+            #[serde(rename = "UserScreenName")]
+            user_id: String,
+        }
+
+        let url = format!("{}/contests/{}/standings/json", ATCODER_HOST, contest_id);
+        let standings = reqwest::get(&url)
+            .ok()?
+            .json::<ContestStandings>()
+            .ok()?
+            .standings
+            .into_iter()
+            .map(|s| (s.place, s.user_id))
+            .collect::<BTreeMap<_, _>>();
+
+        let url = format!("{}/contests/{}/results/json", ATCODER_HOST, contest_id);
+        let performances = reqwest::get(&url)
+            .ok()?
+            .json::<Vec<ContestPerformance>>()
+            .ok()?
+            .into_iter()
+            .filter_map(|p| {
+                standings.get(&p.place).map(|user_id| Performance {
+                    inner_performance: p.inner_performance,
+                    user_id: user_id.to_string(),
+                    contest_id: contest_id.to_string(),
+                })
+            })
+            .collect::<Vec<_>>();
+        Some(performances)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_scrape_performances() {
+        let contest_id = "soundhound2018-summer-qual";
+        let scraper = Scraper;
+        let performances = scraper.scrape_performances(contest_id).unwrap();
+        assert_eq!(performances.len(), 1852);
     }
 }
