3388371a44491d9836ceefb6a8fd03f37cc00a1c
kenkoooo <kenkou.n@gmail.com>
2019-06-14 13:02:36 +0900

    Added SplitToSegments

--- atcoder-problems-backend/src/utils.rs
+++ atcoder-problems-backend/src/utils.rs
@@ -1,4 +1,5 @@
 use crate::sql::models::{Contest, Performance};
+use std::cmp;
 use std::collections::BTreeSet;
 
 pub fn extract_non_performance_contests<'a>(
@@ -13,6 +14,23 @@ pub fn extract_non_performance_contests<'a>(
         .iter()
         .filter(|c| !set.contains(c.id.as_str()))
         .collect()
+}
+
+pub trait SplitToSegments<T> {
+    fn split_to_segments(&self, size: usize) -> Vec<&[T]>;
+}
+
+impl<T> SplitToSegments<T> for [T] {
+    fn split_to_segments(&self, size: usize) -> Vec<&[T]> {
+        let mut result = vec![];
+        let mut cur = self;
+        while !cur.is_empty() {
+            let (left, right) = cur.split_at(cmp::min(size, cur.len()));
+            result.push(left);
+            cur = right;
+        }
+        result
+    }
 }
 
 #[cfg(test)]
@@ -40,5 +58,15 @@ mod tests {
         let filtered = extract_non_performance_contests(&contests, &performances);
         assert_eq!(filtered.len(), 1);
         assert_eq!(filtered[0].id.as_str(), "contest2");
+    }
+
+    #[test]
+    fn test_split_to_segments() {
+        let values = (0..25000usize).collect::<Vec<usize>>();
+        let segments = values.split_to_segments(10000);
+        assert_eq!(segments.len(), 3);
+        assert_eq!(segments[0].len(), 10000);
+        assert_eq!(segments[1].len(), 10000);
+        assert_eq!(segments[2].len(), 5000);
     }
 }
