33d8ba0e7c90f312349ab779f6e4c199803877de
kenkoooo <kenkou.n@gmail.com>
2019-06-18 22:49:30 +0900

    Added TimeSubmissionsHandler

--- atcoder-problems-backend/src/lambda.rs
+++ atcoder-problems-backend/src/lambda.rs
@@ -1,7 +1,9 @@
 mod io;
+mod time_submissions;
 mod user_info;
 mod user_submissions;
 
 pub use io::{LambdaInput, LambdaOutput};
+pub use time_submissions::TimeSubmissionsHandler;
 pub use user_info::UserInfoHandler;
 pub use user_submissions::UserSubmissionsHandler;
