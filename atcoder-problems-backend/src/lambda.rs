54d08b3ddfa31eedf88499a7134a7bd918828f8e
kenkoooo <kenkou.n@gmail.com>
2019-06-19 09:54:02 +0900

    Fixed privacy

--- atcoder-problems-backend/src/lambda.rs
+++ atcoder-problems-backend/src/lambda.rs
@@ -3,7 +3,7 @@ mod time_submissions;
 mod user_info;
 mod user_submissions;
 
-pub use io::{LambdaInput, LambdaOutput};
+pub(crate) use io::{LambdaInput, LambdaOutput};
 pub use time_submissions::TimeSubmissionsHandler;
 pub use user_info::UserInfoHandler;
 pub use user_submissions::UserSubmissionsHandler;
