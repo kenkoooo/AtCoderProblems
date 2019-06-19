11a3cf055ae11c91d4db0a5a1a398c63bbc8b8b9
kenkoooo <kenkou.n@gmail.com>
2019-06-19 10:09:55 +0900

    Added DatabaseUpdateHandler

--- atcoder-problems-backend/src/bin/lambda_update.rs
+++ atcoder-problems-backend/src/bin/lambda_update.rs
@@ -1,13 +1,7 @@
-use atcoder_problems_backend::error::MapHandlerError;
-use atcoder_problems_backend::sql::models::Submission;
-use atcoder_problems_backend::sql::ProblemInfoUpdater;
-use atcoder_problems_backend::sql::{
-    AcceptedCountClient, LanguageCountClient, ProblemsSubmissionUpdater, RatedPointSumClient,
-    SubmissionClient, SubmissionRequest,
-};
-use diesel::{Connection, PgConnection};
-use lambda_runtime::{error::HandlerError, lambda, Context};
-use log::{self, info};
+use atcoder_problems_backend::lambda::DatabaseUpdateHandler;
+
+use lambda_runtime;
+use log;
 use openssl_probe;
 use simple_logger;
 use std::env;
@@ -16,43 +10,7 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     simple_logger::init_with_level(log::Level::Info)?;
     openssl_probe::init_ssl_cert_env_vars();
-    info!("Started!");
-    lambda!(handler);
-
-    Ok(())
-}
-
-fn handler(_: String, _: Context) -> Result<String, HandlerError> {
     let url = env::var("SQL_URL")?;
-    let conn: PgConnection = PgConnection::establish(&url).map_handler_error()?;
-
-    let submissions: Vec<Submission> = conn
-        .get_submissions(SubmissionRequest::AllAccepted)
-        .map_handler_error()?;
-
-    info!("There are {} AC submissions.", submissions.len());
-
-    info!("Executing update_accepted_count...");
-    conn.update_accepted_count(&submissions)
-        .map_handler_error()?;
-
-    info!("Executing update_problem_solver_count...");
-    conn.update_solver_count().map_handler_error()?;
-
-    info!("Executing update_rated_point_sums...");
-    conn.update_rated_point_sum(&submissions)
-        .map_handler_error()?;
-
-    info!("Executing update_language_count...");
-    conn.update_language_count(&submissions)
-        .map_handler_error()?;
-
-    info!("Executing update_submissions_of_problems...");
-    conn.update_submissions_of_problems(&submissions)
-        .map_handler_error()?;
-
-    info!("Executing update_problem_points...");
-    conn.update_problem_points().map_handler_error()?;
-
-    Ok("Finished".to_owned())
+    lambda_runtime::start(DatabaseUpdateHandler::new(&url)?, None);
+    Ok(())
 }
