a9f0a3bfe8b414ee000343e0410ee3e64892a908
kenkoooo <kenkou.n@gmail.com>
2019-08-12 18:57:17 +0900

    Refined ListPage

--- atcoder-problems-frontend/src/pages/ListPage/SmallTable.tsx
+++ atcoder-problems-frontend/src/pages/ListPage/SmallTable.tsx
@@ -1,79 +1,65 @@
 import React from "react";
 import { isAccepted } from "../../utils";
 import Table from "reactstrap/lib/Table";
-import Row from "reactstrap/lib/Row";
+import Submission from "../../interfaces/Submission";
+import { List, Map, Set } from "immutable";
+import MergedProblem from "../../interfaces/MergedProblem";
 
 interface Props {
-  problems: { point?: number | null; rivals: string[]; status: string }[];
-  user_id: string | undefined;
+  mergedProblems: List<MergedProblem>;
+  submissions: Map<string, List<Submission>>;
+  userIds: List<string>;
 }
 
-const SmallTable = ({ problems, user_id }: Props) => {
-  const point_count_map = new Map<number, number>();
-  const user_count_map = new Map<string, Map<number, number>>();
-  problems.forEach(p => {
-    if (p.point) {
-      const count = point_count_map.get(p.point);
-      if (count) {
-        point_count_map.set(p.point, count + 1);
-      } else {
-        point_count_map.set(p.point, 1);
-      }
-      if (isAccepted(p.status) && user_id) {
-        const map = user_count_map.get(user_id);
-        if (map) {
-          const count = map.get(p.point);
-          if (count) {
-            map.set(p.point, count + 1);
-          } else {
-            map.set(p.point, 1);
-          }
-        } else {
-          const m = new Map<number, number>();
-          m.set(p.point, 1);
-          user_count_map.set(user_id, m);
-        }
-      }
-    }
-  });
-
-  const point_count = Array.from(point_count_map)
+const SmallTable = ({ submissions, userIds, mergedProblems }: Props) => {
+  const userPointCount = userIds.map(userId => ({
+    userId,
+    points: submissions
+      .map(list =>
+        list
+          .filter(s => s.user_id === userId)
+          .filter(s => isAccepted(s.result))
+          .map(s => s.point)
+          .first(undefined)
+      )
+      .valueSeq()
+      .reduce(
+        (map, p) => (p ? map.update(p, 0, count => count + 1) : map),
+        Map<number, number>()
+      )
+  }));
+  const totalCount = mergedProblems
+    .reduce(
+      (map, p) => (p.point ? map.update(p.point, 0, count => count + 1) : map),
+      Map<number, number>()
+    )
+    .entrySeq()
     .map(([point, count]) => ({ point, count }))
     .sort((a, b) => a.point - b.point);
-
-  const user_count = Array.from(user_count_map).map(([user_id, map]) => ({
-    user_id,
-    map
-  }));
 
   return (
     <Table striped bordered hover responsive>
       <thead>
         <tr>
           <th>Point</th>
-          {point_count.map(({ point }) => (
+          {totalCount.map(({ point }) => (
             <th key={point}>{point}</th>
           ))}
         </tr>
         <tr>
           <th>Total</th>
-          {point_count.map(({ point, count }) => (
+          {totalCount.map(({ point, count }) => (
             <th key={point}>{count}</th>
           ))}
         </tr>
       </thead>
       <tbody>
-        {user_count.map(({ map, user_id }) => (
-          <tr key={user_id}>
-            <td>{user_id}</td>
-            {point_count.map(({ point }) => {
-              const count = map.get(point);
-              if (count != undefined) {
-                return <td key={point}>{count}</td>;
-              } else {
-                return <td key={point}>0</td>;
-              }
-            })}
+        {userPointCount.map(({ userId, points }) => (
+          <tr key={userId}>
+            <td>{userId}</td>
+            {totalCount.map(({ point }) => (
+              <td key={point}>{points.get(point, 0)}</td>
+            ))}
           </tr>
         ))}
       </tbody>
