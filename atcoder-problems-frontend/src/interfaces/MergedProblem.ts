01f775b2ec1ed3470986f1e5029bb9f02dff786c
kenkoooo <kenkou.n@gmail.com>
2019-08-13 16:25:55 +0900

    Added type guard

--- atcoder-problems-frontend/src/interfaces/MergedProblem.ts
+++ atcoder-problems-frontend/src/interfaces/MergedProblem.ts
@@ -26,3 +26,32 @@ export default interface MergedProblem {
   readonly predict?: number | null;
   readonly point?: number | null;
 }
+
+export const isMergedProblem = (obj: any): obj is MergedProblem =>
+  typeof obj.id === "string" &&
+  typeof obj.contest_id === "string" &&
+  typeof obj.title === "string" &&
+  (typeof obj.first_user_id === "string" || obj.first_user_id === null) &&
+  (typeof obj.first_contest_id === "string" || obj.first_contest_id === null) &&
+  (typeof obj.first_submission_id === "number" ||
+    obj.first_submission_id === null) &&
+  (typeof obj.fastest_user_id === "string" || obj.fastest_user_id === null) &&
+  (typeof obj.fastest_contest_id === "string" ||
+    obj.fastest_contest_id === null) &&
+  (typeof obj.fastest_submission_id === "number" ||
+    obj.fastest_submission_id === null) &&
+  (typeof obj.execution_time === "number" || obj.execution_time === null) &&
+  (typeof obj.shortest_user_id === "string" || obj.shortest_user_id === null) &&
+  (typeof obj.shortest_contest_id === "string" ||
+    obj.shortest_contest_id === null) &&
+  (typeof obj.shortest_submission_id === "number" ||
+    obj.shortest_submission_id === null) &&
+  (typeof obj.source_code_length === "number" ||
+    obj.source_code_length === null) &&
+  (typeof obj.solver_count === "number" || obj.solver_count === null) &&
+  (typeof obj.predict === "number" ||
+    typeof obj.predict === "undefined" ||
+    obj.predict === null) &&
+  (typeof obj.point === "number" ||
+    typeof obj.point === "undefined" ||
+    obj.point === null);
---
+++ atcoder-problems-frontend/src/interfaces/index.test.ts
@@ -0,0 +1,53 @@
+import { isContest } from "./Contest";
+
+describe("Contest type guard", () => {
+  it("should return true", () => {
+    expect(
+      isContest({
+        start_epoch_second: 10,
+        rate_change: "",
+        id: "",
+        duration_second: 1,
+        title: ""
+      })
+    ).toBeTruthy();
+  });
+  it("should return false for undefined fields", () => {
+    expect(
+      isContest({
+        rate_change: "",
+        id: "",
+        duration_second: 1,
+        title: ""
+      })
+    ).toBeFalsy();
+    expect(
+      isContest({
+        start_epoch_second: 10,
+        id: "",
+        duration_second: 1,
+        title: ""
+      })
+    ).toBeFalsy();
+  });
+  it("should return false for different types", () => {
+    expect(
+      isContest({
+        start_epoch_second: 10,
+        rate_change: "",
+        id: "",
+        duration_second: 1,
+        title: 0
+      })
+    ).toBeFalsy();
+    expect(
+      isContest({
+        start_epoch_second: 10,
+        rate_change: "",
+        id: 1,
+        duration_second: 1,
+        title: ""
+      })
+    ).toBeFalsy();
+  });
+});
