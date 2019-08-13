a5ec5444e88e4e687a77bcceda64bfab72caae02
kenkoooo <kenkou.n@gmail.com>
2019-08-13 20:56:21 +0900

    Added API type guard

--- atcoder-problems-frontend/src/interfaces/UserInfo.ts
+++ atcoder-problems-frontend/src/interfaces/UserInfo.ts
@@ -5,3 +5,10 @@ export default interface UserInfo {
   readonly user_id: string;
   readonly accepted_count: number;
 }
+
+export const isUserInfo = (obj: any): obj is UserInfo =>
+  typeof obj.user_id === "string" &&
+  typeof obj.accepted_count === "number" &&
+  typeof obj.accepted_count_rank === "number" &&
+  typeof obj.rated_point_sum === "number" &&
+  typeof obj.rated_point_sum_rank === "number";
