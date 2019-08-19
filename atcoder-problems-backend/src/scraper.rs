184a1023937a46fbfec76e8e2b2b10a15b81eef8
kenkoooo <kenkou.n@gmail.com>
2019-08-19 14:57:12 +0900

    Replaced scrapers with client

--- atcoder-problems-backend/src/scraper.rs
+++ atcoder-problems-backend/src/scraper.rs
@@ -1,68 +1,17 @@
-mod contest;
-mod problem;
-mod submission;
-
-use crate::sql::models::{Contest, Performance, Problem, Submission};
-
-use reqwest::header::{HeaderMap, HeaderValue, ACCEPT, ACCEPT_ENCODING};
+use crate::sql::models::Performance;
+use reqwest;
 use serde::Deserialize;
 use std::collections::BTreeMap;
 
 const ATCODER_HOST: &str = "https://atcoder.jp";
 
-fn get_html(url: &str) -> reqwest::Result<String> {
-    let mut headers = HeaderMap::new();
-    headers.insert(ACCEPT, HeaderValue::from_static("text/html"));
-    headers.insert(ACCEPT_ENCODING, HeaderValue::from_static("gzip"));
-    reqwest::Client::new()
-        .get(url)
-        .headers(headers)
-        .send()?
-        .text()
-}
-
 pub trait ScraperTrait {
-    fn scrape_contests(&self, page: usize) -> Option<Vec<Contest>>;
-    fn scrape_submissions(
-        &self,
-        contest_id: &str,
-        page: Option<usize>,
-    ) -> Option<(Vec<Submission>, usize)>;
-    fn scrape_problems(&self, contest_id: &str) -> Option<Vec<Problem>>;
     fn scrape_performances(&self, contest_id: &str) -> Option<Vec<Performance>>;
 }
 
 pub struct Scraper;
 
 impl ScraperTrait for Scraper {
-    fn scrape_contests(&self, page: usize) -> Option<Vec<Contest>> {
-        let url = format!("{}/contests/archive?lang=ja&page={}", ATCODER_HOST, page);
-        let html = get_html(&url).ok()?;
-        contest::scrape(&html)
-    }
-
-    fn scrape_submissions(
-        &self,
-        contest_id: &str,
-        page: Option<usize>,
-    ) -> Option<(Vec<Submission>, usize)> {
-        let page = page.unwrap_or(1);
-        let url = format!(
-            "{}/contests/{}/submissions?page={}",
-            ATCODER_HOST, contest_id, page
-        );
-        let html = get_html(&url).ok()?;
-        let submissions = submission::scrape(&html, contest_id)?;
-        let max_page = submission::scrape_submission_page_count(&html)?;
-        Some((submissions, max_page))
-    }
-
-    fn scrape_problems(&self, contest_id: &str) -> Option<Vec<Problem>> {
-        let url = format!("{}/contests/{}/tasks", ATCODER_HOST, contest_id);
-        let page_html = get_html(&url).ok()?;
-        problem::scrape(&page_html, contest_id)
-    }
-
     fn scrape_performances(&self, contest_id: &str) -> Option<Vec<Performance>> {
         #[derive(Deserialize, Debug, Clone)]
         struct ContestPerformance {
