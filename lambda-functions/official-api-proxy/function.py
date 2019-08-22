215d9f1384a50aa648535b040c0363913e0d56c3
kenkoooo <kenkou.n@gmail.com>
2019-08-22 14:59:34 +0900

    Fixed some bugs in Lambda

--- lambda-functions/official-api-proxy/function.py
+++ lambda-functions/official-api-proxy/function.py
@@ -1,21 +1,22 @@
 import re
 
 import requests
-
+import json
 
 ATCODER_API_PATTERN = re.compile(r"^users/.+/history/json$")
 
 
 def handler(event, context):
-    path = event.get("path")
+    print(json.dumps(event))
+    path = event["pathParameters"]["proxy"]
     if not ATCODER_API_PATTERN.match(path):
         return "The path requested is not supported."
     resp = requests.get(f"https://atcoder.jp/{path}").json()
-    return resp
-
-
-if __name__ == '__main__':
-    event = {
-        "path": "users/amylase/history/json"
+    return {
+        "isBase64Encoded": False,
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": json.dumps(resp)
     }
-    print(handler(event, None))
