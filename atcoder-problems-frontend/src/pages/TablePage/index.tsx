b8ca85dc09b19e08dff1ef76fb4531b8d172e15c
koyumeishi <koyumeishi@users.noreply.github.com>
2019-09-12 08:19:09 +0900

    Improve rendering speed of Table Page Add hidden DOM and lazy rendering

--- atcoder-problems-frontend/src/pages/TablePage/index.tsx
+++ atcoder-problems-frontend/src/pages/TablePage/index.tsx
@@ -20,6 +20,15 @@ import TableTabButtons, {TableTab} from "./TableTab";
 import HelpBadgeTooltip from "../../components/HelpBadgeTooltip";
 import {TableHeaderColumn} from "react-bootstrap-table";
 
+const ContestWrapper: React.FC<{display: boolean, children: any}> = props => {
+  console.log("contestwrapper");
+  return (
+    <div style={{display: props.display? "" : "none"}}>
+      {props.children}
+    </div>
+  );
+};
+
 export const statusLabelToTableColor = (label: StatusLabel) => {
   switch (label) {
     case StatusLabel.Success:
@@ -40,101 +49,122 @@ interface Props {
   rivals: List<string>;
   submissions: Map<ProblemId, List<Submission>>;
   contests: Map<ContestId, Contest>;
-  contestToProblems: Map<ContestId, List<Problem>>;
+  contestToProblemsId: Map<ContestId, List<ProblemId>>;
+  problems: Map<ProblemId, Problem>;
   statusLabelMap: Map<ProblemId, ProblemStatus>;
   activeTab: TableTab;
   showAccepted: boolean;
 }
+
+let abc: Map<ContestId, Contest> = Map<ContestId, Contest>();
+let arc: Map<ContestId, Contest> = Map<ContestId, Contest>();
+let agc: Map<ContestId, Contest> = Map<ContestId, Contest>();
+let othersRated: Map<ContestId, Contest> = Map<ContestId, Contest>();
+let others: Map<ContestId, Contest> = Map<ContestId, Contest>();
+let contestToProblems: Map<ContestId, List<Problem>> = Map<ContestId, List<Problem>>();
+let rendered: Set<TableTab> = new Set<TableTab>();
+let contestInitialized: boolean = false;
 
 const TablePage: React.FC<Props> = props => {
   const {
     userId,
     rivals,
     contests,
-    contestToProblems,
+    contestToProblemsId,
+    problems,
     submissions,
     statusLabelMap,
     activeTab,
     showAccepted,
   } = props;
 
-  const abc = contests.filter((v, k) => k.match(/^abc\d{3}$/));
-  const arc = contests.filter((v, k) => k.match(/^arc\d{3}$/));
-  const agc = contests.filter((v, k) => k.match(/^agc\d{3}$/));
-  let ratedContests = new Set();
-  abc.forEach((v, k) => ratedContests.add(k));
-  arc.forEach((v, k) => ratedContests.add(k));
-  agc.forEach((v, k) => ratedContests.add(k));
-  const othersRated = contests
-    .filter((v,k) => !ratedContests.has(k) )
-    .filter((v,k) => v.rate_change !== "-" )
-    .filter((v,k) => v.start_epoch_second >= 1468670400); // agc001
-  othersRated.forEach((v, k) => ratedContests.add(k));
-  const others = contests.filter((v,k) => !ratedContests.has(k) );
+  rendered.add(activeTab);
+
+  if(contestInitialized === false && contests && contests.size !== 0){
+    contestInitialized = true;
+    contestToProblems = contestToProblemsId.map(list =>
+      list
+        .map(id => problems.get(id))
+        .filter(
+          (problem: Problem | undefined): problem is Problem =>
+            problem !== undefined
+        )
+    );
+    abc = contests.filter((v, k) => k.match(/^abc\d{3}$/));
+    arc = contests.filter((v, k) => k.match(/^arc\d{3}$/));
+    agc = contests.filter((v, k) => k.match(/^agc\d{3}$/));
+    let ratedContests = new Set<string>();
+    abc.forEach((v, k) => ratedContests.add(k));
+    arc.forEach((v, k) => ratedContests.add(k));
+    agc.forEach((v, k) => ratedContests.add(k));
+    othersRated = contests
+      .filter((v,k) => !ratedContests.has(k) )
+      .filter((v,k) => v.rate_change !== "-" )
+      .filter((v,k) => v.start_epoch_second >= 1468670400); // agc001
+    othersRated.forEach((v, k) => ratedContests.add(k));
+    others = contests.filter((v,k) => !ratedContests.has(k) );
+  }
 
   return (
     <div>
       <Options />
       <TableTabButtons />
-      {
-        activeTab === TableTab.ABC ?
-          (
-            <AtCoderRegularTable
-              showSolved={showAccepted}
-              contests={abc}
-              title="AtCoder Beginner Contest"
-              contestToProblems={contestToProblems}
-              statusLabelMap={statusLabelMap}
-            />
-          ) :
-        activeTab === TableTab.ARC ?
-          (
-            <AtCoderRegularTable
-              showSolved={showAccepted}
-              contests={arc}
-              title="AtCoder Regular Contest"
-              contestToProblems={contestToProblems}
-              statusLabelMap={statusLabelMap}
-            />
-          ) :
-        activeTab === TableTab.AGC ?
-          (
-            <AtCoderRegularTable
-              showSolved={showAccepted}
-              contests={agc}
-              title="AtCoder Grand Contest"
-              contestToProblems={contestToProblems}
-              statusLabelMap={statusLabelMap}
-            />
-          ) :
-        activeTab === TableTab.OtherRatedContests ?
-          (
-            <ContestTable
-              contests={othersRated}
-              title="Other Rated Contests"
-              contestToProblems={contestToProblems}
-              showSolved={showAccepted}
-              submissions={submissions}
-              userId={userId}
-              rivals={rivals}
-              statusLabelMap={statusLabelMap}
-            />
-          ) :
-        activeTab === TableTab.OtherContests ?
-          (
-            <ContestTable
-              contests={others}
-              title="Other Contests"
-              contestToProblems={contestToProblems}
-              showSolved={showAccepted}
-              submissions={submissions}
-              userId={userId}
-              rivals={rivals}
-              statusLabelMap={statusLabelMap}
-            />
-          ) :
-        (null)
-      }
+      <ContestWrapper display={activeTab === TableTab.ABC}>
+        <AtCoderRegularTable
+          showSolved={showAccepted}
+          contests={abc}
+          title="AtCoder Beginner Contest"
+          contestToProblems={contestToProblems}
+          statusLabelMap={statusLabelMap}
+          rendered={rendered.has(TableTab.ABC)}
+        />
+      </ContestWrapper>
+      <ContestWrapper display={activeTab === TableTab.ARC}>
+        <AtCoderRegularTable
+          showSolved={showAccepted}
+          contests={arc}
+          title="AtCoder Regular Contest"
+          contestToProblems={contestToProblems}
+          statusLabelMap={statusLabelMap}
+          rendered={rendered.has(TableTab.ARC)}
+        />
+      </ContestWrapper>
+      <ContestWrapper display={activeTab === TableTab.AGC}>
+        <AtCoderRegularTable
+          showSolved={showAccepted}
+          contests={agc}
+          title="AtCoder Grand Contest"
+          contestToProblems={contestToProblems}
+          statusLabelMap={statusLabelMap}
+          rendered={rendered.has(TableTab.AGC)}
+        />
+      </ContestWrapper>
+      <ContestWrapper display={activeTab === TableTab.OtherRatedContests}>
+        <ContestTable
+          contests={othersRated}
+          title="Other Rated Contests"
+          contestToProblems={contestToProblems}
+          showSolved={showAccepted}
+          submissions={submissions}
+          userId={userId}
+          rivals={rivals}
+          statusLabelMap={statusLabelMap}
+          rendered={rendered.has(TableTab.OtherRatedContests)}
+        />
+      </ContestWrapper>
+      <ContestWrapper display={activeTab === TableTab.OtherContests}>
+        <ContestTable
+          contests={others}
+          title="Other Contests"
+          contestToProblems={contestToProblems}
+          showSolved={showAccepted}
+          submissions={submissions}
+          userId={userId}
+          rivals={rivals}
+          statusLabelMap={statusLabelMap}
+          rendered={rendered.has(TableTab.OtherContests)}
+        />
+      </ContestWrapper>
     </div>
   );
 }
@@ -142,14 +172,8 @@ const TablePage: React.FC<Props> = props => {
 const stateToProps = (state: State) => ({
   userId: state.users.userId,
   rivals: state.users.rivals,
-  contestToProblems: state.contestToProblems.map(list =>
-    list
-      .map(id => state.problems.get(id))
-      .filter(
-        (problem: Problem | undefined): problem is Problem =>
-          problem !== undefined
-      )
-  ),
+  contestToProblemsId: state.contestToProblems,
+  problems: state.problems,
   contests: state.contests,
   submissions: state.submissions,
   statusLabelMap: state.cache.statusLabelMap,
