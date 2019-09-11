356519d724736ff320a8b70238bedbc0679ab7e7
kenkoooo <kenkou.n@gmail.com>
2019-09-11 15:23:15 +0900

    Update run.sh

--- sql-backup/run.sh
+++ sql-backup/run.sh
@@ -4,4 +4,5 @@ export SQL_PORT=5432
 echo ${SQL_HOST}:${SQL_PORT}:${SQL_DB}:${SQL_USER}:${SQL_PASS} > ~/.pgpass
 chmod 0600 ~/.pgpass
 pg_dump -h ${SQL_HOST} -w -U ${SQL_USER} ${SQL_DB} > backup.sql
-aws s3 cp backup.sql s3://kenkoooo/atcoder.sql --acl public-read
+psql -h ${SQL_HOST} -w -U ${SQL_USER} -d ${SQL_DB} -c "\copy submissions TO './submissions.csv' WITH (FORMAT CSV, HEADER)"
+aws s3 cp submissions.csv s3://kenkoooo/atcoder.sql --acl public-read
