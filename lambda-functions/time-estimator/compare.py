9be3bd15400bc40b9dd3ee8b9bd779e1926a3a9e
amylase <amylase.inquiry@gmail.com>
2019-10-19 19:07:02 +0900

    fixed random seed.

--- lambda-functions/time-estimator/compare.py
+++ lambda-functions/time-estimator/compare.py
@@ -1,4 +1,20 @@
 import json
+import math
+
+
+def calc_average_normalized_loglikelihood(models, keys=None) -> float:
+    keys = keys or models.keys()
+    accum_normalized_logloss = 0.
+    for key in keys:
+        if key not in models:
+            accum_normalized_logloss += math.log(0.5)
+            continue
+        model = models[key]
+        if "irt_loglikelihood" not in model or "irt_users" not in model:
+            accum_normalized_logloss += math.log(0.5)
+            continue
+        accum_normalized_logloss += model["irt_loglikelihood"] / model["irt_users"]
+    return accum_normalized_logloss / len(keys)
 
 
 if __name__ == '__main__':
@@ -25,3 +41,10 @@ if __name__ == '__main__':
         new_difficulty = new_models[problem_key]["difficulty"]
         if abs(new_difficulty - old_difficulty) > 100 and old_difficulty > 0:
             print(f"{problem_key}: {old_difficulty:.02f} -> {new_difficulty:.02f} d = {new_difficulty - old_difficulty:-.02f}")
+
+    print(f"likelihood")
+    all_keys = old_models.keys() | new_models.keys()
+    old_score = calc_average_normalized_loglikelihood(old_models, all_keys)
+    new_score = calc_average_normalized_loglikelihood(new_models, all_keys)
+    improvement = (new_score - old_score) / old_score
+    print(f"{old_score:.03f} -> {new_score:.03f} ({improvement * 100:.03f}% improvement)")
