4dd3cf47dcbc11cba7833d6db8e053a122dd2c14
kenkoooo <kenkou.n@gmail.com>
2019-11-21 01:44:48 +0900

    Added including count button

--- atcoder-problems-frontend/src/pages/ListPage/DifficultyTable.tsx
+++ atcoder-problems-frontend/src/pages/ListPage/DifficultyTable.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { isAccepted } from "../../utils";
 import Table from "reactstrap/lib/Table";
 import Submission from "../../interfaces/Submission";
@@ -7,6 +7,7 @@ import MergedProblem from "../../interfaces/MergedProblem";
 import ProblemModel from "../../interfaces/ProblemModel";
 import { DifficultyCircle } from "../../components/DifficultyCircle";
 import { ProblemId } from "../../interfaces/State";
+import { Button, ButtonGroup } from "reactstrap";
 
 interface Props {
   mergedProblems: Map<ProblemId, MergedProblem>;
@@ -22,26 +23,32 @@ const DIFF_MAX = 4000;
 
 const problemToDifficultyLevel = (
   problem: MergedProblem,
-  problemModels: Map<ProblemId, ProblemModel>
+  problemModels: Map<ProblemId, ProblemModel>,
+  showExperimental: boolean
 ) => {
-  const difficulty: number | undefined = problemModels.getIn(
-    [problem.id, "difficulty"],
-    undefined
-  );
-  if (difficulty === undefined) {
+  const problemModel = problemModels.get(problem.id);
+  if (problemModel === undefined) {
+    return undefined;
+  }
+  if (problemModel.is_experimental && !showExperimental) {
+    return undefined;
+  }
+  if (problemModel.difficulty === undefined) {
     return undefined;
   } else {
-    return Math.floor(Math.min(DIFF_MAX, difficulty) / DIFF_WIDTH);
+    return Math.floor(Math.min(DIFF_MAX, problemModel.difficulty) / DIFF_WIDTH);
   }
 };
 
-const DifficultyTable = ({
-  submissions,
-  userIds,
-  mergedProblems,
-  problemModels,
-  setFilterFunc
-}: Props) => {
+const DifficultyTable = (props: Props) => {
+  const {
+    submissions,
+    userIds,
+    mergedProblems,
+    problemModels,
+    setFilterFunc
+  } = props;
+  const [includingExperimental, setIncludingExperimental] = useState(true);
   const difficulties: List<{ from: number; to: number }> = Range(
     0,
     4400,
@@ -63,7 +70,13 @@ const DifficultyTable = ({
             .filter(s => isAccepted(s.result))
             .map(s => mergedProblems.get(s.problem_id))
             .filter((p): p is MergedProblem => p !== undefined)
-            .map(problem => problemToDifficultyLevel(problem, problemModels))
+            .map(problem =>
+              problemToDifficultyLevel(
+                problem,
+                problemModels,
+                includingExperimental
+              )
+            )
             .first(undefined)
         )
         .filter((d: number | undefined): d is number => d !== undefined)
@@ -75,7 +88,9 @@ const DifficultyTable = ({
       return { userId, difficultyLevelCounts };
     });
   const totalCount = mergedProblems
-    .map(problem => problemToDifficultyLevel(problem, problemModels))
+    .map(problem =>
+      problemToDifficultyLevel(problem, problemModels, includingExperimental)
+    )
     .filter((d): d is number => d !== undefined)
     .reduce(
       (countMap, difficultyLevel) =>
@@ -87,53 +102,62 @@ const DifficultyTable = ({
     .sort((a, b) => a.difficultyLevel - b.difficultyLevel);
 
   return (
-    <Table striped bordered hover responsive>
-      <thead>
-        <tr>
-          <th>Difficulty</th>
-          {totalCount.map(({ difficultyLevel }) => (
-            <th key={difficultyLevel} style={{ whiteSpace: "nowrap" }}>
-              <a
-                href={window.location.hash}
-                onClick={() =>
-                  setFilterFunc(
-                    difficulties.get(difficultyLevel, { from: 0 }).from,
-                    difficulties.get(difficultyLevel, { to: INF_POINT }).to
-                  )
-                }
-              >
-                <DifficultyCircle
-                  difficulty={
-                    difficulties.get(difficultyLevel, { from: INF_POINT })
-                      .from + 399
-                  }
-                  id={`difficulty-table-level-${difficultyLevel}`}
-                />
-                {difficulties.get(difficultyLevel, { from: 0 }).from}-
-              </a>
-            </th>
-          ))}
-        </tr>
-        <tr>
-          <th>Total</th>
-          {totalCount.map(({ difficultyLevel, count }) => (
-            <th key={difficultyLevel}>{count}</th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {userPointCount.map(({ userId, difficultyLevelCounts }) => (
-          <tr key={userId}>
-            <td>{userId}</td>
+    <>
+      <ButtonGroup className="mb-2">
+        <Button
+          onClick={() => setIncludingExperimental(!includingExperimental)}
+        >
+          {includingExperimental ? "Including 🧪" : "Excluding 🧪"}
+        </Button>
+      </ButtonGroup>
+      <Table striped bordered hover responsive>
+        <thead>
+          <tr>
+            <th>Difficulty</th>
             {totalCount.map(({ difficultyLevel }) => (
-              <td key={difficultyLevel}>
-                {difficultyLevelCounts.get(difficultyLevel, 0)}
-              </td>
+              <th key={difficultyLevel} style={{ whiteSpace: "nowrap" }}>
+                <a
+                  href={window.location.hash}
+                  onClick={() =>
+                    setFilterFunc(
+                      difficulties.get(difficultyLevel, { from: 0 }).from,
+                      difficulties.get(difficultyLevel, { to: INF_POINT }).to
+                    )
+                  }
+                >
+                  <DifficultyCircle
+                    difficulty={
+                      difficulties.get(difficultyLevel, { from: INF_POINT })
+                        .from + 399
+                    }
+                    id={`difficulty-table-level-${difficultyLevel}`}
+                  />
+                  {difficulties.get(difficultyLevel, { from: 0 }).from}-
+                </a>
+              </th>
             ))}
           </tr>
-        ))}
-      </tbody>
-    </Table>
+          <tr>
+            <th>Total</th>
+            {totalCount.map(({ difficultyLevel, count }) => (
+              <th key={difficultyLevel}>{count}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {userPointCount.map(({ userId, difficultyLevelCounts }) => (
+            <tr key={userId}>
+              <td>{userId}</td>
+              {totalCount.map(({ difficultyLevel }) => (
+                <td key={difficultyLevel}>
+                  {difficultyLevelCounts.get(difficultyLevel, 0)}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
   );
 };
 
