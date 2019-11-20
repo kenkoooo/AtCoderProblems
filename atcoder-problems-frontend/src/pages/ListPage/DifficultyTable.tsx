d46ff2c555fc2e03244bec9f8755508ad29423ba
kenkoooo <kenkou.n@gmail.com>
2019-11-21 01:24:13 +0900

    Refactoring

--- atcoder-problems-frontend/src/pages/ListPage/DifficultyTable.tsx
+++ atcoder-problems-frontend/src/pages/ListPage/DifficultyTable.tsx
@@ -6,16 +6,34 @@ import { List, Map, Range } from "immutable";
 import MergedProblem from "../../interfaces/MergedProblem";
 import ProblemModel from "../../interfaces/ProblemModel";
 import { DifficultyCircle } from "../../components/DifficultyCircle";
+import { ProblemId } from "../../interfaces/State";
 
 interface Props {
-  mergedProblems: Map<string, MergedProblem>;
-  submissions: Map<string, List<Submission>>;
+  mergedProblems: Map<ProblemId, MergedProblem>;
+  submissions: Map<ProblemId, List<Submission>>;
   userIds: List<string>;
-  problemModels: Map<string, ProblemModel>;
-  setFilterFunc: (from: number, to: number) => any;
+  problemModels: Map<ProblemId, ProblemModel>;
+  setFilterFunc: (from: number, to: number) => void;
 }
 
 const INF_POINT = 1e18;
+const DIFF_WIDTH = 400;
+const DIFF_MAX = 4000;
+
+const problemToDifficultyLevel = (
+  problem: MergedProblem,
+  problemModels: Map<ProblemId, ProblemModel>
+) => {
+  const difficulty: number | undefined = problemModels.getIn(
+    [problem.id, "difficulty"],
+    undefined
+  );
+  if (difficulty === undefined) {
+    return undefined;
+  } else {
+    return Math.floor(Math.min(DIFF_MAX, difficulty) / DIFF_WIDTH);
+  }
+};
 
 const DifficultyTable = ({
   submissions,
@@ -24,45 +42,44 @@ const DifficultyTable = ({
   problemModels,
   setFilterFunc
 }: Props) => {
-  const difficulties: List<{ from: number; to: number }> = Range(0, 4400, 400)
+  const difficulties: List<{ from: number; to: number }> = Range(
+    0,
+    4400,
+    DIFF_WIDTH
+  )
     .map(from => ({
       from,
-      to: from === 4000 ? INF_POINT : from + 399
+      to: from === DIFF_MAX ? INF_POINT : from + DIFF_WIDTH - 1
     }))
     .toList();
   const userPointCount = userIds
     .filter(userId => userId.length > 0)
-    .map(userId => ({
-      userId,
-      difficultyLevelCounts: submissions
-        .map(list =>
-          list
+    .map(userId => {
+      const difficultyLevelCounts = submissions
+        .valueSeq()
+        .map(problemSubmissions =>
+          problemSubmissions
             .filter(s => s.user_id === userId)
             .filter(s => isAccepted(s.result))
             .map(s => mergedProblems.get(s.problem_id))
-            .filter(p => p && problemModels.has(p.id))
             .filter((p): p is MergedProblem => p !== undefined)
-            .map(p => problemModels.getIn([p.id, "difficulty"], undefined))
+            .map(problem => problemToDifficultyLevel(problem, problemModels))
             .first(undefined)
         )
-        .valueSeq()
         .filter((d: number | undefined): d is number => d !== undefined)
         .reduce(
-          (map, difficulty) =>
-            map.update(
-              Math.floor(Math.min(4000, difficulty) / 400),
-              0,
-              count => count + 1
-            ),
+          (countMap, difficultyLevel) =>
+            countMap.update(difficultyLevel, 0, count => count + 1),
           Map<number, number>()
-        )
-    }));
+        );
+      return { userId, difficultyLevelCounts };
+    });
   const totalCount = mergedProblems
-    .map(p => problemModels.getIn([p.id, "difficulty"], undefined))
+    .map(problem => problemToDifficultyLevel(problem, problemModels))
     .filter((d): d is number => d !== undefined)
-    .map(d => Math.floor(Math.min(4000, d) / 400))
     .reduce(
-      (map, d) => map.update(d, 0, count => count + 1),
+      (countMap, difficultyLevel) =>
+        countMap.update(difficultyLevel, 0, count => count + 1),
       Map<number, number>()
     )
     .entrySeq()
