c0899ed857c3300b77b26ded9fa694990ee54840
kenkoooo <kenkou.n@gmail.com>
2019-12-04 21:20:29 +0900

    WIP

--- atcoder-problems-backend/src/server.rs
+++ atcoder-problems-backend/src/server.rs
@@ -1,22 +1,34 @@
+use actix_web::http::header::IF_NONE_MATCH;
 use actix_web::{web, HttpRequest, HttpResponse};
 use diesel::PgConnection;
 
+pub mod time_submissions;
 pub mod user_info;
 pub mod user_submissions;
+pub mod utils;
 
 pub(crate) type ConnectionManager = diesel::r2d2::ConnectionManager<PgConnection>;
 pub(crate) type Pool = diesel::r2d2::Pool<ConnectionManager>;
 
-pub(crate) fn request_with_connection<F>(
-    request: HttpRequest,
-    pool: web::Data<Pool>,
-    f: F,
-) -> HttpResponse
+pub(crate) fn request_with_connection<F>(pool: web::Data<Pool>, f: F) -> HttpResponse
 where
-    F: Fn(HttpRequest, &PgConnection) -> HttpResponse,
+    F: FnOnce(&PgConnection) -> HttpResponse,
 {
     match pool.get() {
-        Ok(conn) => f(request, &conn),
+        Ok(conn) => f(&conn),
         _ => HttpResponse::ServiceUnavailable().finish(),
+    }
+}
+
+pub(crate) trait EtagExtractor {
+    fn extract_etag(&self) -> &str;
+}
+
+impl EtagExtractor for HttpRequest {
+    fn extract_etag(&self) -> &str {
+        self.headers()
+            .get(IF_NONE_MATCH)
+            .and_then(|value| value.to_str().ok())
+            .unwrap_or_else(|| "no etag")
     }
 }
---
+++ atcoder-problems-backend/src/server/time_submissions.rs
@@ -0,0 +1,43 @@
+use crate::error::Result;
+use crate::server::{request_with_connection, Pool};
+use crate::server::{utils, EtagExtractor};
+use crate::sql::models::Submission;
+use crate::sql::{SubmissionClient, SubmissionRequest};
+use actix_web::http::header::ETAG;
+use actix_web::{web, HttpRequest, HttpResponse};
+
+pub async fn get_time_submissions(
+    request: HttpRequest,
+    path: web::Path<(i64,)>,
+    pool: web::Data<Pool>,
+) -> HttpResponse {
+    let from_epoch_second = path.0;
+    request_with_connection(pool, |conn| {
+        let etag = request.extract_etag();
+        match inner(conn, from_epoch_second, etag) {
+            Ok(r) => match r {
+                Some((s, e)) => HttpResponse::Ok().header(ETAG, e).json(s),
+                None => HttpResponse::NotModified().finish(),
+            },
+            _ => HttpResponse::BadRequest().finish(),
+        }
+    })
+}
+
+fn inner<C: SubmissionClient>(
+    c: &C,
+    from_epoch_second: i64,
+    etag: &str,
+) -> Result<Option<(Vec<Submission>, String)>> {
+    let submissions: Vec<_> = c.get_submissions(SubmissionRequest::FromTime {
+        from_second: from_epoch_second,
+        count: 1000,
+    })?;
+    let max_id = submissions.iter().map(|s| s.id).max().unwrap_or(0);
+    let new_etag = utils::calc_etag_for_time(from_epoch_second, max_id);
+    if new_etag.as_str() == etag {
+        Ok(None)
+    } else {
+        Ok(Some((submissions, new_etag)))
+    }
+}
