2b7d58fd86b7084c4d0c2c5a87e76332437db058
kenkoooo <kenkou.n@gmail.com>
2019-12-11 17:09:21 +0900

    refactoring

--- atcoder-problems-backend/src/server/time_submissions.rs
+++ atcoder-problems-backend/src/server/time_submissions.rs
@@ -4,25 +4,26 @@ use crate::sql::{SubmissionClient, SubmissionRequest};
 use tide::{Request, Response};
 
 pub(crate) async fn get_time_submissions(request: Request<AppData>) -> Response {
-    request
+    if let Some(from_epoch_second) = request
         .param::<String>("from")
         .ok()
         .and_then(|s| s.parse::<i64>().ok())
-        .map(|from_epoch_second| {
-            request.state().respond(|conn| {
-                let submissions: Vec<_> = conn.get_submissions(SubmissionRequest::FromTime {
-                    from_second: from_epoch_second,
-                    count: 1000,
-                })?;
-                let etag = request.extract_etag();
-                let max_id = submissions.iter().map(|s| s.id).max().unwrap_or(0);
-                let new_etag = utils::calc_etag_for_time(from_epoch_second, max_id);
-                if new_etag.as_str() == etag {
-                    Ok(Response::not_modified())
-                } else {
-                    Ok(Response::etagged(&new_etag).body_json(&submissions)?)
-                }
-            })
+    {
+        request.state().respond(|conn| {
+            let submissions: Vec<_> = conn.get_submissions(SubmissionRequest::FromTime {
+                from_second: from_epoch_second,
+                count: 1000,
+            })?;
+            let etag = request.extract_etag();
+            let max_id = submissions.iter().map(|s| s.id).max().unwrap_or(0);
+            let new_etag = utils::calc_etag_for_time(from_epoch_second, max_id);
+            if new_etag.as_str() == etag {
+                Ok(Response::not_modified())
+            } else {
+                Ok(Response::etagged(&new_etag).body_json(&submissions)?)
+            }
         })
-        .unwrap_or_else(|| Response::new(503))
+    } else {
+        Response::internal_error()
+    }
 }
