11f31b1bb449cd3d993b5a01b65f692a9a72fb80
kenkoooo <kenkou.n@gmail.com>
2019-12-15 11:52:01 +0900

    Added tests

--- atcoder-problems-backend/tests/test_server_e2e_internal.rs
+++ atcoder-problems-backend/tests/test_server_e2e_internal.rs
@@ -58,8 +58,123 @@ fn test_list() {
 
             let response = surf::get(url("/atcoder-api/v3/internal/list/get", port))
                 .set_header("Cookie", format!("token={}", token))
+                .recv_string()
+                .await?;
+            assert_eq!(&response, "[]");
+
+            let mut map = BTreeMap::new();
+            map.insert("list_name", "a");
+            let mut response = surf::post(url("/atcoder-api/v3/internal/list/create", port))
+                .set_header("Cookie", format!("token={}", token))
+                .body_json(&map)
+                .unwrap()
+                .await?;
+            assert!(response.status().is_success(), "{:?}", response);
+            let value: Value = response.body_json().await?;
+            let internal_list_id = value.get("internal_list_id").unwrap().as_str().unwrap();
+
+            let response = surf::get(url("/atcoder-api/v3/internal/list/get", port))
+                .set_header("Cookie", format!("token={}", token))
+                .recv_json::<Value>()
+                .await?;
+            let expected: Value = serde_json::from_str(&format!(
+                r#"[{{
+                    "internal_list_id": "{}",
+                    "internal_list_name": "a",
+                    "items": []
+                    }}]"#,
+                internal_list_id
+            ))
+            .unwrap();
+            assert_eq!(response, expected);
+
+            let mut map = BTreeMap::new();
+            map.insert("internal_list_id", internal_list_id);
+            map.insert("name", "b");
+            let response = surf::post(url("/atcoder-api/v3/internal/list/update", port))
+                .set_header("Cookie", format!("token={}", token))
+                .body_json(&map)
+                .unwrap()
                 .await?;
             assert!(response.status().is_success());
+            let response = surf::get(url("/atcoder-api/v3/internal/list/get", port))
+                .set_header("Cookie", format!("token={}", token))
+                .recv_json::<Value>()
+                .await?;
+            let expected: Value = serde_json::from_str(&format!(
+                r#"[{{
+                    "internal_list_id": "{}",
+                    "internal_list_name": "b",
+                    "items": []
+                    }}]"#,
+                internal_list_id
+            ))
+            .unwrap();
+            assert_eq!(response, expected);
+
+            let mut map = BTreeMap::new();
+            map.insert("internal_list_id", &internal_list_id);
+            let response = surf::post(url("/atcoder-api/v3/internal/list/delete", port))
+                .set_header("Cookie", format!("token={}", token))
+                .body_json(&map)
+                .unwrap()
+                .await?;
+            assert!(response.status().is_success());
+
+            let response = surf::get(url("/atcoder-api/v3/internal/list/get", port))
+                .set_header("Cookie", format!("token={}", token))
+                .recv_string()
+                .await?;
+            assert_eq!(&response, "[]");
+
+            Ok(())
+        });
+        server.race(client).await.unwrap();
+    });
+}
+#[test]
+fn test_invalid_token() {
+    task::block_on(async {
+        let port = setup();
+        let server = utils::start_server_handle(MockAuth, port);
+        let client = utils::run_client_handle(async move {
+            let response = surf::get(url("/atcoder-api/v3/internal/list/get", port))
+                .set_header("Cookie", "token=invalid-token")
+                .await?;
+            assert!(!response.status().is_success());
+
+            let mut map = BTreeMap::new();
+            map.insert("list_name", "a");
+            let response = surf::post(url("/atcoder-api/v3/internal/list/create", port))
+                .set_header("Cookie", "token=invalid-token")
+                .await?;
+            assert!(!response.status().is_success());
+
+            Ok(())
+        });
+        server.race(client).await.unwrap();
+    });
+}
+
+#[test]
+fn test_list_item() {
+    task::block_on(async {
+        let port = setup();
+        let server = utils::start_server_handle(MockAuth, port);
+        let client = utils::run_client_handle(async move {
+            let response = surf::get(url(
+                &format!("/atcoder-api/v3/authorize?code={}", VALID_CODE),
+                port,
+            ))
+            .await?;
+            let cookie = response.header("set-cookie").unwrap();
+            let token = cookie.split("=").skip(1).next().unwrap();
+
+            let response = surf::get(url("/atcoder-api/v3/internal/list/get", port))
+                .set_header("Cookie", format!("token={}", token))
+                .recv_string()
+                .await?;
+            assert_eq!(&response, "[]");
 
             let mut map = BTreeMap::new();
             map.insert("list_name", "a");
@@ -98,9 +213,9 @@ fn test_list() {
 
             let response = surf::get(url("/atcoder-api/v3/internal/list/get", port))
                 .set_header("Cookie", format!("token={}", token))
-                .recv_json::<Value>()
+                .recv_string()
                 .await?;
-            assert!(response.as_array().unwrap().is_empty());
+            assert_eq!(&response, "[]");
 
             Ok(())
         });
