157a5845918c91ecf38738cfd26eaa211b008f82
kenkoooo <kenkou.n@gmail.com>
2019-12-16 17:28:43 +0900

    refactoring

--- atcoder-problems-backend/src/server/problem_list.rs
+++ atcoder-problems-backend/src/server/problem_list.rs
@@ -1,34 +1,22 @@
-use crate::error::Result;
-use crate::server::utils::RequestUnpack;
-use crate::server::{AppData, Authentication, CommonRequest, CommonResponse, PooledConnection};
+use crate::server::utils::{RequestUnpack, UnwrapResponse};
+use crate::server::{AppData, Authentication, CommonResponse};
 use crate::sql::internal::problem_list_manager::ProblemListManager;
 
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use tide::{Request, Response};
 
-pub(crate) async fn get_list<A: Clone + Authentication>(request: Request<AppData<A>>) -> Response {
-    fn unpack_request<A>(request: Request<AppData<A>>) -> Result<(String, PooledConnection)> {
-        let token = request.get_cookie("token")?;
-        let conn = request.state().pool.get()?;
-        Ok((token, conn))
-    }
-    fn construct_response(conn: PooledConnection, internal_user_id: String) -> Result<Response> {
-        let list = conn.get_list(&internal_user_id)?;
-        let response = Response::ok().body_json(&list)?;
-        Ok(response)
-    }
-
-    let client = request.state().authentication.clone();
-    match unpack_request(request) {
-        Ok((token, conn)) => match client.get_user_id(&token).await {
-            Ok(internal_user_id) => match construct_response(conn, internal_user_id) {
-                Ok(response) => response,
-                _ => Response::internal_error(),
-            },
-            _ => Response::bad_request(),
-        },
-        Err(_) => Response::bad_request(),
-    }
+pub(crate) async fn get_list<A: Authentication + Clone + Send + Sync + 'static>(
+    request: Request<AppData<A>>,
+) -> Response {
+    request
+        .get_unpack()
+        .await
+        .and_then(|(conn, user_id)| conn.get_list(&user_id))
+        .and_then(|list| {
+            let response = Response::ok().body_json(&list)?;
+            Ok(response)
+        })
+        .unwrap_response()
 }
 
 pub(crate) async fn create_list<A: Authentication + Clone + Send + Sync + 'static>(
@@ -38,26 +26,20 @@ pub(crate) async fn create_list<A: Authentication + Clone + Send + Sync + 'stati
     struct Query {
         list_name: String,
     }
-    #[derive(Serialize)]
-    struct Created {
-        internal_list_id: String,
-    }
-    fn create_response(conn: PooledConnection, user_id: &str, list_name: &str) -> Result<Response> {
-        let internal_list_id = conn.create_list(user_id, list_name)?;
-        let created = Created { internal_list_id };
-        let response = Response::ok().body_json(&created)?;
-        Ok(response)
-    }
 
-    match request.post_unpack::<Query>().await {
-        Ok((query, conn, internal_user_id)) => {
-            match create_response(conn, &internal_user_id, &query.list_name) {
-                Ok(response) => response,
-                Err(_) => Response::internal_error(),
-            }
-        }
-        Err(_) => Response::bad_request(),
-    }
+    request
+        .post_unpack::<Query>()
+        .await
+        .and_then(|(query, conn, internal_user_id)| {
+            let list_id = conn.create_list(&internal_user_id, &query.list_name)?;
+            Ok(list_id)
+        })
+        .and_then(|internal_list_id| {
+            let body = serde_json::json!({ "internal_list_id": format!("{}", internal_list_id) });
+            let response = Response::ok().body_json(&body)?;
+            Ok(response)
+        })
+        .unwrap_response()
 }
 
 pub(crate) async fn delete_list<A>(request: Request<AppData<A>>) -> Response
@@ -68,13 +50,12 @@ where
     struct Q {
         internal_list_id: String,
     }
-    match request.post_unpack::<Q>().await {
-        Ok((query, conn, _)) => match conn.delete_list(&query.internal_list_id) {
-            Ok(_) => Response::ok(),
-            _ => Response::internal_error(),
-        },
-        Err(_) => Response::bad_request(),
-    }
+    request
+        .post_unpack::<Q>()
+        .await
+        .and_then(|(query, conn, _)| conn.delete_list(&query.internal_list_id))
+        .map(|_| Response::ok())
+        .unwrap_response()
 }
 
 pub(crate) async fn update_list<A>(request: Request<AppData<A>>) -> Response
@@ -86,13 +67,12 @@ where
         internal_list_id: String,
         name: String,
     }
-    match request.post_unpack::<Q>().await {
-        Ok((query, conn, _)) => match conn.update_list(&query.internal_list_id, &query.name) {
-            Ok(_) => Response::ok(),
-            _ => Response::internal_error(),
-        },
-        Err(_) => Response::bad_request(),
-    }
+    request
+        .post_unpack::<Q>()
+        .await
+        .and_then(|(query, conn, _)| conn.update_list(&query.internal_list_id, &query.name))
+        .map(|_| Response::ok())
+        .unwrap_response()
 }
 
 pub(crate) async fn add_item<A>(request: Request<AppData<A>>) -> Response
@@ -104,13 +84,12 @@ where
         internal_list_id: String,
         problem_id: String,
     }
-    match request.post_unpack::<Q>().await {
-        Ok((query, conn, _)) => match conn.add_item(&query.internal_list_id, &query.problem_id) {
-            Ok(_) => Response::ok(),
-            _ => Response::internal_error(),
-        },
-        Err(_) => Response::bad_request(),
-    }
+    request
+        .post_unpack::<Q>()
+        .await
+        .and_then(|(query, conn, _)| conn.add_item(&query.internal_list_id, &query.problem_id))
+        .map(|_| Response::ok())
+        .unwrap_response()
 }
 
 pub(crate) async fn update_item<A>(request: Request<AppData<A>>) -> Response
@@ -123,15 +102,14 @@ where
         problem_id: String,
         memo: String,
     }
-    match request.post_unpack::<Q>().await {
-        Ok((query, conn, _)) => {
-            match conn.update_item(&query.internal_list_id, &query.problem_id, &query.memo) {
-                Ok(_) => Response::ok(),
-                _ => Response::internal_error(),
-            }
-        }
-        Err(_) => Response::bad_request(),
-    }
+    request
+        .post_unpack::<Q>()
+        .await
+        .and_then(|(query, conn, _)| {
+            conn.update_item(&query.internal_list_id, &query.problem_id, &query.memo)
+        })
+        .map(|_| Response::ok())
+        .unwrap_response()
 }
 
 pub(crate) async fn delete_item<A>(request: Request<AppData<A>>) -> Response
@@ -143,13 +121,10 @@ where
         internal_list_id: String,
         problem_id: String,
     }
-    match request.post_unpack::<Q>().await {
-        Ok((query, conn, _)) => {
-            match conn.delete_item(&query.internal_list_id, &query.problem_id) {
-                Ok(_) => Response::ok(),
-                _ => Response::internal_error(),
-            }
-        }
-        Err(_) => Response::bad_request(),
-    }
+    request
+        .post_unpack::<Q>()
+        .await
+        .and_then(|(query, conn, _)| conn.delete_item(&query.internal_list_id, &query.problem_id))
+        .map(|_| Response::ok())
+        .unwrap_response()
 }
