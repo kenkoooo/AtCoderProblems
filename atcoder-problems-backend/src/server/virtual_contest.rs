c34f9efbe424e3cdf97375d2653d0306b41ca39e
kenkoooo <kenkou.n@gmail.com>
2019-12-16 17:39:29 +0900

    refactoring

--- atcoder-problems-backend/src/server/virtual_contest.rs
+++ atcoder-problems-backend/src/server/virtual_contest.rs
@@ -1,10 +1,8 @@
-use crate::error::Result;
-use crate::server::utils::RequestUnpack;
-use crate::server::{AppData, Authentication, CommonResponse, PooledConnection};
+use crate::server::utils::{RequestUnpack, UnwrapResponse};
+use crate::server::{AppData, Authentication, CommonResponse};
 use crate::sql::internal::virtual_contest_manager::VirtualContestManager;
 
 use serde::Deserialize;
-use std::collections::BTreeMap;
 use tide::{Request, Response};
 
 pub(crate) async fn create_contest<A: Authentication + Clone + Send + Sync + 'static>(
@@ -17,27 +15,25 @@ pub(crate) async fn create_contest<A: Authentication + Clone + Send + Sync + 'st
         start_epoch_second: i64,
         duration_second: i64,
     }
-    fn response(conn: PooledConnection, q: Q, user_id: &str) -> Result<Response> {
-        let contest_id = conn.create_contest(
-            &q.title,
-            &q.memo,
-            user_id,
-            q.start_epoch_second,
-            q.duration_second,
-        )?;
-        let mut map = BTreeMap::new();
-        map.insert("contest_id", contest_id);
-        let response = Response::ok().body_json(&map)?;
-        Ok(response)
-    }
-
-    match request.post_unpack::<Q>().await {
-        Ok((q, conn, internal_user_id)) => match response(conn, q, &internal_user_id) {
-            Ok(response) => response,
-            _ => Response::internal_error(),
-        },
-        _ => Response::bad_request(),
-    }
+    request
+        .post_unpack::<Q>()
+        .await
+        .and_then(|(q, conn, user_id)| {
+            let contest_id = conn.create_contest(
+                &q.title,
+                &q.memo,
+                &user_id,
+                q.start_epoch_second,
+                q.duration_second,
+            )?;
+            Ok(contest_id)
+        })
+        .and_then(|contest_id| {
+            let body = serde_json::json!({ "contest_id": format!("{}", contest_id) });
+            let response = Response::ok().body_json(&body)?;
+            Ok(response)
+        })
+        .unwrap_response()
 }
 
 pub(crate) async fn update_contest<A: Authentication + Clone + Send + Sync + 'static>(
@@ -52,19 +48,20 @@ pub(crate) async fn update_contest<A: Authentication + Clone + Send + Sync + 'st
         duration_second: i64,
     }
 
-    match request.post_unpack::<Q>().await {
-        Ok((q, conn, _)) => match conn.update_contest(
-            &q.id,
-            &q.title,
-            &q.memo,
-            q.start_epoch_second,
-            q.duration_second,
-        ) {
-            Ok(_) => Response::ok(),
-            _ => Response::internal_error(),
-        },
-        _ => Response::bad_request(),
-    }
+    request
+        .post_unpack::<Q>()
+        .await
+        .and_then(|(q, conn, _)| {
+            conn.update_contest(
+                &q.id,
+                &q.title,
+                &q.memo,
+                q.start_epoch_second,
+                q.duration_second,
+            )
+        })
+        .map(|_| Response::ok())
+        .unwrap_response()
 }
 
 pub(crate) async fn add_item<A: Authentication + Clone + Send + Sync + 'static>(
@@ -75,14 +72,12 @@ pub(crate) async fn add_item<A: Authentication + Clone + Send + Sync + 'static>(
         contest_id: String,
         problem_id: String,
     }
-
-    match request.post_unpack::<Q>().await {
-        Ok((q, conn, user_id)) => match conn.add_item(&q.contest_id, &q.problem_id, &user_id) {
-            Ok(_) => Response::ok(),
-            _ => Response::internal_error(),
-        },
-        _ => Response::bad_request(),
-    }
+    request
+        .post_unpack::<Q>()
+        .await
+        .and_then(|(q, conn, user_id)| conn.add_item(&q.contest_id, &q.problem_id, &user_id))
+        .map(|_| Response::ok())
+        .unwrap_response()
 }
 
 pub(crate) async fn delete_item<A: Authentication + Clone + Send + Sync + 'static>(
@@ -94,43 +89,34 @@ pub(crate) async fn delete_item<A: Authentication + Clone + Send + Sync + 'stati
         problem_id: String,
     }
 
-    match request.post_unpack::<Q>().await {
-        Ok((q, conn, user_id)) => match conn.remove_item(&q.contest_id, &q.problem_id, &user_id) {
-            Ok(_) => Response::ok(),
-            _ => Response::internal_error(),
-        },
-        _ => Response::bad_request(),
-    }
+    request
+        .post_unpack::<Q>()
+        .await
+        .and_then(|(q, conn, user_id)| conn.remove_item(&q.contest_id, &q.problem_id, &user_id))
+        .map(|_| Response::ok())
+        .unwrap_response()
 }
 
 pub(crate) async fn get_my_contests<A: Authentication + Clone + Send + Sync + 'static>(
     request: Request<AppData<A>>,
 ) -> Response {
-    match request.get_unpack().await {
-        Ok((conn, user_id)) => match conn.get_own_contests(&user_id) {
-            Ok(contests) => match Response::ok().body_json(&contests) {
-                Ok(response) => response,
-                Err(_) => Response::internal_error(),
-            },
-            _ => Response::internal_error(),
-        },
-        _ => Response::bad_request(),
-    }
+    request
+        .get_unpack()
+        .await
+        .and_then(|(conn, user_id)| conn.get_own_contests(&user_id))
+        .and_then(|contests| Ok(Response::ok().body_json(&contests)?))
+        .unwrap_response()
 }
 
 pub(crate) async fn get_participated<A: Authentication + Clone + Send + Sync + 'static>(
     request: Request<AppData<A>>,
 ) -> Response {
-    match request.get_unpack().await {
-        Ok((conn, user_id)) => match conn.get_participated_contests(&user_id) {
-            Ok(contests) => match Response::ok().body_json(&contests) {
-                Ok(response) => response,
-                Err(_) => Response::internal_error(),
-            },
-            _ => Response::internal_error(),
-        },
-        _ => Response::bad_request(),
-    }
+    request
+        .get_unpack()
+        .await
+        .and_then(|(conn, user_id)| conn.get_participated_contests(&user_id))
+        .and_then(|contests| Ok(Response::ok().body_json(&contests)?))
+        .unwrap_response()
 }
 
 pub(crate) async fn get_single_contest<A>(request: Request<AppData<A>>) -> Response {
@@ -151,12 +137,10 @@ pub(crate) async fn join_contest<A: Authentication + Clone + Send + Sync + 'stat
     struct Q {
         contest_id: String,
     }
-
-    match request.post_unpack::<Q>().await {
-        Ok((q, conn, user_id)) => match conn.join_contest(&q.contest_id, &user_id) {
-            Ok(_) => Response::ok(),
-            _ => Response::internal_error(),
-        },
-        _ => Response::bad_request(),
-    }
+    request
+        .post_unpack::<Q>()
+        .await
+        .and_then(|(q, conn, user_id)| conn.join_contest(&q.contest_id, &user_id))
+        .map(|_| Response::ok())
+        .unwrap_response()
 }
