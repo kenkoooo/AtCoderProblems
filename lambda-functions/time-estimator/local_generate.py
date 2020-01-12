c16a2ac314d28aae7c2801fbc803d1da1873a5a9
amylase <amylase.inquiry@gmail.com>
2020-01-12 20:13:23 +0900

    login to atcoder before downloading standings

--- lambda-functions/time-estimator/local_generate.py
+++ lambda-functions/time-estimator/local_generate.py
@@ -2,11 +2,12 @@ import json
 from pathlib import Path
 from shutil import move
 
-from function import run
+from function import run, login
 
 if __name__ == '__main__':
     if not Path("problem-models-old.json").exists() and Path("problem-models.json").exists():
         move("problem-models.json", "problem-models-old.json")
-    results = run(None, True)
+    session = login(None, None)  # set your credential before use. Do not commit it!
+    results = run(None, True, session)
     with open("problem-models.json", "w") as f:
         json.dump(results, f)
