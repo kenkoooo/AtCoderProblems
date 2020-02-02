bfac90cfdc90d3bb4529ab2c121d31f76757b7b2
kenkoooo <kenkou.n@gmail.com>
2020-02-03 00:28:43 +0900

    Fix 100% CPU usage problem

--- atcoder-problems-backend/src/bin/crawl_recent_submissions.rs
+++ atcoder-problems-backend/src/bin/crawl_recent_submissions.rs
@@ -1,9 +1,17 @@
 use algorithm_problem_client::AtCoderClient;
 use atcoder_problems_backend::crawler::RecentCrawler;
+use atcoder_problems_backend::error::Result;
+use atcoder_problems_backend::sql::connect;
 use diesel::{Connection, PgConnection};
 use futures::executor::block_on;
 use log::{error, info};
-use std::env;
+use std::{env, thread, time};
+
+async fn crawl(url: &str) -> Result<()> {
+    let db = connect(url)?;
+    let crawler = RecentCrawler::new(db, AtCoderClient::default());
+    crawler.crawl().await
+}
 
 fn main() {
     simple_logger::init_with_level(log::Level::Info).unwrap();
@@ -11,19 +19,10 @@ fn main() {
     let url = env::var("SQL_URL").expect("SQL_URL must be set.");
 
     loop {
-        match PgConnection::establish(&url) {
-            Ok(conn) => {
-                let crawler = RecentCrawler::new(conn, AtCoderClient::default());
-                match block_on(crawler.crawl()) {
-                    Ok(_) => {}
-                    Err(e) => {
-                        error!("Failed to crawl submissions: {:?}", e);
-                    }
-                }
-            }
-            Err(e) => {
-                error!("Failed to connect to PostgreSQL {:?}", e);
-            }
+        info!("Start new loop");
+        if let Err(e) = block_on(crawl(&url)) {
+            log::error!("{:?}", e);
+            thread::sleep(time::Duration::from_millis(1000));
         }
     }
 }
