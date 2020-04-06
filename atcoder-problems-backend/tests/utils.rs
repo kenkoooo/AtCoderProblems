f0cd1cad39a4303e93c6c60669b6d1f379782f7f
kenkoooo <kenkou.n@gmail.com>
2020-04-07 00:08:09 +0900

    refactoring

--- atcoder-problems-backend/tests/utils.rs
+++ atcoder-problems-backend/tests/utils.rs
@@ -25,29 +25,3 @@ fn initialize(conn: &PgConnection) {
     file.read_to_string(&mut sql).unwrap();
     conn.batch_execute(&sql).unwrap();
 }
-
-#[cfg(test)]
-pub fn start_server_handle<A>(
-    auth: A,
-    port: u16,
-) -> task::JoinHandle<std::result::Result<(), surf::Exception>>
-where
-    A: Authentication + Send + Sync + 'static + Clone,
-{
-    task::spawn(async move {
-        let pool = initialize_pool(SQL_URL).unwrap();
-        run_server(pool, auth, port).await.unwrap();
-        Ok(())
-    })
-}
-
-#[cfg(test)]
-pub fn run_client_handle<F>(future: F) -> task::JoinHandle<std::result::Result<(), surf::Exception>>
-where
-    F: Future<Output = std::result::Result<(), surf::Exception>> + Send + 'static,
-{
-    task::spawn(async {
-        task::sleep(std::time::Duration::from_millis(1000)).await;
-        future.await
-    })
-}
