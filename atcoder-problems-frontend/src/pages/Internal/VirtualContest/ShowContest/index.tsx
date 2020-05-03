87645022ccf96066c0416412f9b7ac7bafa9b202
kenkoooo <kenkou.n@gmail.com>
2020-05-04 01:44:14 +0900

    Auto-Refresh, resolved #511

--- atcoder-problems-frontend/src/pages/Internal/VirtualContest/ShowContest/index.tsx
+++ atcoder-problems-frontend/src/pages/Internal/VirtualContest/ShowContest/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Redirect, useHistory, useParams } from "react-router-dom";
 import { connect, PromiseState } from "react-refetch";
 import {
@@ -10,46 +10,30 @@ import {
   Spinner,
   Table
 } from "reactstrap";
+import Octicon, { Check, Sync } from "@primer/octicons-react";
 import * as CachedApi from "../../../../utils/CachedApiClient";
-import { fetchVirtualContestSubmission } from "../../../../utils/CachedApiClient";
-import { List, Map as ImmutableMap } from "immutable";
+import { Map as ImmutableMap } from "immutable";
 import { ProblemId } from "../../../../interfaces/Status";
 import { CONTEST_JOIN, contestGetUrl, USER_GET } from "../../ApiUrl";
-import Submission from "../../../../interfaces/Submission";
 import MergedProblem from "../../../../interfaces/MergedProblem";
 import ProblemModel from "../../../../interfaces/ProblemModel";
-import { isAccepted } from "../../../../utils";
 import {
   formatMomentDateTimeDay,
   parseSecond
 } from "../../../../utils/DateUtil";
-import {
-  formatMode,
-  UserResponse,
-  VirtualContest,
-  VirtualContestItem
-} from "../../types";
+import { formatMode, UserResponse, VirtualContest } from "../../types";
 import TweetButton from "../../../../components/TweetButton";
 import { GITHUB_LOGIN_LINK } from "../../../../utils/Url";
-import {
-  BestSubmissionEntry,
-  calcPerformance,
-  extractBestSubmissions
-} from "./util";
-import ContestTable from "./ContestTable";
+import { ContestTable } from "./ContestTable";
 import Timer from "../../../../components/Timer";
 import { ACCOUNT_INFO } from "../../../../utils/RouterPath";
-
-interface ShowingVirtualContest extends VirtualContest {
-  map: ImmutableMap<ProblemId, List<Submission>> | undefined;
-}
 
 interface OuterProps {
   contestId: string;
 }
 
 interface InnerProps extends OuterProps {
-  contestInfoFetch: PromiseState<ShowingVirtualContest>;
+  contestInfoFetch: PromiseState<VirtualContest>;
   userInfoGet: PromiseState<UserResponse | null>;
   joinContest: () => void;
   joinContestPost: PromiseState<{} | null>;
@@ -57,62 +41,10 @@ interface InnerProps extends OuterProps {
   problemModelGet: PromiseState<ImmutableMap<ProblemId, ProblemModel>>;
 }
 
-function getEstimatedPerformances(
-  contestInfo: ShowingVirtualContest,
-  bestSubmissions: BestSubmissionEntry[],
-  start: number,
-  problems: VirtualContestItem[],
-  modelMap: ImmutableMap<ProblemId, ProblemModel>
-) {
-  return contestInfo.participants.map(userId => {
-    const onlySolvedData = bestSubmissions
-      .filter(b => b.userId === userId)
-      .filter(b => {
-        const result = b.bestSubmissionInfo?.bestSubmission.result;
-        return result && isAccepted(result);
-      })
-      .map(b =>
-        b.bestSubmissionInfo
-          ? {
-              time: b.bestSubmissionInfo.bestSubmission.epoch_second,
-              id: b.problemId
-            }
-          : undefined
-      )
-      .filter((obj): obj is { time: number; id: string } => obj !== undefined)
-      .sort((a, b) => a.time - b.time)
-      .reduce(
-        ({ prev, list }, entry) => ({
-          list: list.push({
-            problemId: entry.id,
-            solved: true,
-            time: entry.time - prev
-          }),
-          prev: entry.time
-        }),
-        {
-          list: List<{ problemId: string; time: number; solved: boolean }>(),
-          prev: start
-        }
-      ).list;
-    const solvedData = problems.map(p => {
-      const problemId = p.id;
-      const entry = onlySolvedData.find(e => e.problemId === problemId);
-      return entry ? entry : { problemId: p.id, time: 0, solved: false };
-    });
-    const performance = calcPerformance(solvedData, modelMap);
-    return { performance, userId };
-  });
-}
-
 const InnerShowContest = (props: InnerProps) => {
+  const [autoRefreshEnabled, setAutoRefreshEnabled] = useState(false);
   const history = useHistory();
-  const {
-    contestInfoFetch,
-    userInfoGet,
-    problemMapFetch,
-    problemModelGet
-  } = props;
+  const { contestInfoFetch, userInfoGet, problemMapFetch } = props;
 
   if (contestInfoFetch.pending) {
     return <Spinner style={{ width: "3rem", height: "3rem" }} />;
@@ -132,12 +64,6 @@ const InnerShowContest = (props: InnerProps) => {
     userInfoGet.fulfilled && userInfoGet.value
       ? userInfoGet.value.internal_user_id
       : null;
-  const submissionMap = contestInfo.map
-    ? contestInfo.map
-    : ImmutableMap<ProblemId, List<Submission>>();
-  const modelMap = problemModelGet.fulfilled
-    ? problemModelGet.value
-    : ImmutableMap<ProblemId, ProblemModel>();
 
   const atCoderUserId = rawAtCoderUserId ? rawAtCoderUserId : "";
   const isLoggedIn = internalUserId !== null;
@@ -150,34 +76,8 @@ const InnerShowContest = (props: InnerProps) => {
   const now = Math.floor(Date.now() / 1000);
   const canJoin = !alreadyJoined && userIdIsSet && now < end;
   const isOwner = contestInfo.owner_user_id === internalUserId;
-  const problems = contestInfo.problems;
-
-  const bestSubmissions = extractBestSubmissions(
-    new Map(
-      submissionMap
-        .entrySeq()
-        .map(([problemId, submissions]): [string, Submission[]] => [
-          problemId,
-          submissions.toArray()
-        ])
-        .toArray()
-    ),
-    contestInfo.participants,
-    problems.map(item => item.id)
-  );
-
   const enableEstimatedPerformances =
     contestInfo.participants.length * contestInfo.problems.length <= 100;
-
-  const estimatedPerformances = enableEstimatedPerformances
-    ? getEstimatedPerformances(
-        contestInfo,
-        bestSubmissions,
-        start,
-        problems,
-        modelMap
-      )
-    : [];
 
   const showProblems = start < now;
   return (
@@ -252,7 +152,15 @@ const InnerShowContest = (props: InnerProps) => {
               </Button>
             ) : null}
             <TweetButton title={contestInfo.title} id={contestInfo.id} />
-          </ButtonGroup>
+          </ButtonGroup>{" "}
+          <Button
+            outline={!autoRefreshEnabled}
+            active={autoRefreshEnabled}
+            onClick={() => setAutoRefreshEnabled(!autoRefreshEnabled)}
+          >
+            <Octicon icon={autoRefreshEnabled ? Check : Sync} /> Auto Refresh{" "}
+            {autoRefreshEnabled ? "Enabled" : "Disabled"}
+          </Button>
         </Col>
       </Row>
 
@@ -274,9 +182,10 @@ const InnerShowContest = (props: InnerProps) => {
             })}
             mode={contestInfo.mode}
             users={contestInfo.participants}
-            bestSubmissions={bestSubmissions}
-            estimatedPerformances={estimatedPerformances}
+            enableEstimatedPerformances={enableEstimatedPerformances}
             start={start}
+            end={end}
+            enableAutoRefresh={autoRefreshEnabled}
           />
         </Col>
       </Row>
@@ -285,22 +194,6 @@ const InnerShowContest = (props: InnerProps) => {
 };
 
 const ShowContest = connect<OuterProps, InnerProps>((props: OuterProps) => {
-  const fetchContestInfo = () =>
-    fetch(contestGetUrl(props.contestId))
-      .then(response => response.json())
-      .then(contest => {
-        const start = contest.start_epoch_second;
-        const end = contest.start_epoch_second + contest.duration_second;
-        const users = contest.participants;
-        const problems = contest.problems.map(
-          (item: VirtualContestItem) => item.id
-        );
-        return fetchSubmissions(start, end, users, problems).then(map => ({
-          map,
-          ...contest
-        }));
-      });
-
   return {
     problemModelGet: {
       comparison: null,
@@ -310,9 +203,8 @@ const ShowContest = connect<OuterProps, InnerProps>((props: OuterProps) => {
       url: USER_GET
     },
     contestInfoFetch: {
-      comparison: null,
       force: true,
-      value: fetchContestInfo
+      url: contestGetUrl(props.contestId)
     },
     problemMapFetch: {
       comparison: null,
@@ -328,9 +220,8 @@ const ShowContest = connect<OuterProps, InnerProps>((props: OuterProps) => {
         body: JSON.stringify({ contest_id: props.contestId }),
         andThen: () => ({
           contestInfoFetch: {
-            comparison: null,
-            force: true,
-            value: fetchContestInfo
+            url: contestGetUrl(props.contestId),
+            force: true
           }
         })
       }
@@ -338,26 +229,6 @@ const ShowContest = connect<OuterProps, InnerProps>((props: OuterProps) => {
     joinContestPost: { value: null }
   };
 })(InnerShowContest);
-
-const fetchSubmissions = async (
-  start: number,
-  end: number,
-  users: string[],
-  problems: string[]
-) => {
-  const result = await fetchVirtualContestSubmission(
-    users,
-    problems,
-    start,
-    end
-  );
-  return result
-    .filter(s => s.result !== "CE")
-    .reduce((map, s) => {
-      const list = map.get(s.problem_id, List<Submission>());
-      return map.set(s.problem_id, list.push(s));
-    }, ImmutableMap<ProblemId, List<Submission>>());
-};
 
 export default () => {
   const { contestId } = useParams();
