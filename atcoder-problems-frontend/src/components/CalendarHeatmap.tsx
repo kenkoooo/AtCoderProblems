1b5673f79f3876df819d63a19b07e1d593989f61
Jamie Choi <jamie.choi.mail.2002@gmail.com>
2020-05-21 17:51:46 +0800

    feat(frontend): implement difficulty heatmap

--- atcoder-problems-frontend/src/components/CalendarHeatmap.tsx
+++ atcoder-problems-frontend/src/components/CalendarHeatmap.tsx
@@ -3,19 +3,23 @@ import { UncontrolledTooltip } from "reactstrap";
 import { Range, Map } from "immutable";
 import moment from "moment";
 import { formatMomentDate, getNextSunday, getToday } from "../utils/DateUtil";
+import Submission from "../interfaces/Submission";
 
 const WEEKDAY = 7;
 const WEEKS = 53;
-const COLORS = ["#ebedf0", "#c6e48b", "#7bc96f", "#239a3b", "#196127"];
 
-interface Props {
-  dateLabels: string[];
-  formatTooltip?: (date: string, count: number) => string;
+type DateStr = string;
+interface Props<State> {
+  submissions: Submission[];
+  reducer: (date: DateStr, subs: Submission[]) => State;
+  formatDate: (sub: Submission) => DateStr;
+  formatTooltip: (date: DateStr, state: State) => string;
+  getColor: (date: DateStr, state: State) => string;
 }
 
-const CalendarHeatmap: React.FC<Props> = (props) => {
-  const { dateLabels } = props;
-
+const CalendarHeatmap: React.FC<Props<any>> = <T extends {}>(
+  props: Props<T>
+) => {
   const today = getToday();
   const nextSunday = getNextSunday(today);
 
@@ -24,17 +28,26 @@ const CalendarHeatmap: React.FC<Props> = (props) => {
 
   const countMap = Range(0, WEEKS * WEEKDAY)
     .map((i) => moment(startDate).add(i, "day"))
-    .map((date) => ({ label: formatMomentDate(date), count: 0 }))
-
-    .concat(dateLabels.map((label) => ({ label, count: 1 })))
+    .map((date) => ({
+      label: formatMomentDate(date),
+      submissions: [] as Submission[],
+    }))
+    .concat(
+      props.submissions.map((submission: Submission) => ({
+        label: props.formatDate(submission),
+        submissions: [submission],
+      }))
+    )
     .filter(({ label }) => label >= startLabel)
     .reduce(
-      (map, { label, count }) => map.set(label, map.get(label, 0) + count),
-      Map<string, number>()
-    );
+      (map, { label, submissions }) =>
+        map.set(label, map.get(label, [] as Submission[]).concat(submissions)),
+      Map<string, Submission[]>()
+    )
+    .map((value, key) => props.reducer(key, value));
   const tableData = countMap
     .entrySeq()
-    .map(([date, count]) => ({ count, date }))
+    .map(([date, state]) => ({ state, date }))
     .sort((a, b) => a.date.localeCompare(b.date))
     .toArray();
 
@@ -44,8 +57,8 @@ const CalendarHeatmap: React.FC<Props> = (props) => {
   return (
     <div style={{ width: "100%" }}>
       <svg viewBox={`0 0 ${width} ${height}`} style={{ width: "100%" }}>
-        {tableData.map(({ date, count }, i) => {
-          const color = COLORS[Math.min(count, COLORS.length - 1)];
+        {tableData.map(({ date, state }, i) => {
+          const color = props.getColor(date, state);
           const week = Math.floor(i / WEEKDAY);
           const day = i % WEEKDAY;
           return (
@@ -62,16 +75,14 @@ const CalendarHeatmap: React.FC<Props> = (props) => {
         })}
       </svg>
 
-      {tableData.map(({ date, count }) => (
+      {tableData.map(({ date, state }) => (
         <UncontrolledTooltip
           delay={{ show: 0, hide: 0 }}
           key={date}
           placement="right"
           target={`rect-${date}`}
         >
-          {props.formatTooltip
-            ? props.formatTooltip(date, count)
-            : `${date}: ${count}`}
+          {props.formatTooltip(date, state)}
         </UncontrolledTooltip>
       ))}
     </div>
