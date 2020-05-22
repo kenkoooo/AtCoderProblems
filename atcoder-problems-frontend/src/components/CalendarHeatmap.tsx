226b3769faab08bd0917de9aa95c586dd6eb89c5
kenkoooo <kenkou.n@gmail.com>
2020-05-22 13:58:13 +0900

    refactoring

--- atcoder-problems-frontend/src/components/CalendarHeatmap.tsx
+++ atcoder-problems-frontend/src/components/CalendarHeatmap.tsx
@@ -1,46 +1,30 @@
 import React from "react";
 import { UncontrolledTooltip } from "reactstrap";
-import { Range, Map as ImmutableMap } from "immutable";
-import moment from "moment";
-import { formatMomentDate, getNextSunday, getToday } from "../utils/DateUtil";
 
-const WEEKDAY = 7;
-const WEEKS = 53;
+const COLOR_GREY = "#ebedf0";
 
-interface Props<State> {
-  tableData: ImmutableMap<string, State>;
-  defaultValue: State;
-  formatTooltip: (date: string, state: State) => string;
-  getColor: (date: string, state: State) => string;
+interface Props {
+  tableData: { date: string; value?: number }[];
+  formatTooltip: (date: string, value: number) => string;
+  getColor: (date: string, value: number) => string;
+  columns: number;
+  rows: number;
 }
 
-function CalendarHeatmap<T>(props: Props<T>): React.ReactElement {
-  const today = getToday();
-  const nextSunday = getNextSunday(today);
-
-  const startDate = nextSunday.date(nextSunday.date() - WEEKS * WEEKDAY);
-
-  // Generate all entries by date in range, including empty entries.
-  const tableData = Range(0, WEEKS * WEEKDAY)
-    .map((i) => moment(startDate).add(i, "day"))
-    .map((date): string => formatMomentDate(date))
-    .map((date) => ({
-      date,
-      state: props.tableData.get(date, props.defaultValue),
-    }))
-    .sort((a, b) => a.date.localeCompare(b.date))
-    .toArray();
+export function CalendarHeatmap(props: Props): React.ReactElement {
+  const { tableData } = props;
 
   const blockWidth = 10;
-  const width = blockWidth * WEEKS;
-  const height = blockWidth * WEEKDAY;
+  const width = blockWidth * props.columns;
+  const height = blockWidth * props.rows;
   return (
     <div style={{ width: "100%" }}>
       <svg viewBox={`0 0 ${width} ${height}`} style={{ width: "100%" }}>
-        {tableData.map(({ date, state }, i) => {
-          const color = props.getColor(date, state);
-          const week = Math.floor(i / WEEKDAY);
-          const day = i % WEEKDAY;
+        {tableData.map(({ date, value }, i) => {
+          const color =
+            value === undefined ? COLOR_GREY : props.getColor(date, value);
+          const week = Math.floor(i / props.rows);
+          const day = i % props.rows;
           return (
             <rect
               key={date}
@@ -55,18 +39,19 @@ function CalendarHeatmap<T>(props: Props<T>): React.ReactElement {
         })}
       </svg>
 
-      {tableData.map(({ date, state }) => (
-        <UncontrolledTooltip
-          delay={{ show: 0, hide: 0 }}
-          key={date}
-          placement="right"
-          target={`rect-${date}`}
-        >
-          {props.formatTooltip(date, state)}
-        </UncontrolledTooltip>
-      ))}
+      {tableData.map(
+        ({ date, value }) =>
+          value !== undefined && (
+            <UncontrolledTooltip
+              delay={{ show: 0, hide: 0 }}
+              key={date}
+              placement="right"
+              target={`rect-${date}`}
+            >
+              {props.formatTooltip(date, value)}
+            </UncontrolledTooltip>
+          )
+      )}
     </div>
   );
 }
-
-export default CalendarHeatmap;
