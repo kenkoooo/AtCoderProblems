b556a17a3544e0c3230fc248d011f40b4cd7afbf
kenkoooo <kenkou.n@gmail.com>
2020-05-22 14:05:52 +0900

    Fixed test

--- atcoder-problems-frontend/src/pages/UserPage/ProgressChartBlock/FilteringHeatmap.test.tsx
+++ atcoder-problems-frontend/src/pages/UserPage/ProgressChartBlock/FilteringHeatmap.test.tsx
@@ -16,13 +16,13 @@ const emptySubmission: Submission = {
 const submissions = [
   {
     ...emptySubmission,
-    epoch_second: 1,
+    id: 1,
     result: "AC",
     problem_id: "problem_1",
   },
   {
     ...emptySubmission,
-    epoch_second: 2,
+    id: 2,
     result: "AC",
     problem_id: "problem_1",
   },
@@ -32,6 +32,6 @@ describe("filter", () => {
   it("unique ac submissions", () => {
     const filtered = filterSubmissions(submissions, "Unique AC");
     expect(filtered.length).toBe(1);
-    expect(filtered[0].epoch_second).toBe(1);
+    expect(filtered[0].id).toBe(1);
   });
 });
