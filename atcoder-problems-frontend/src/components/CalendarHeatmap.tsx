eceb0633cb139396e4754bf75e4dd723d6475b19
iilj <u.eeoou@gmail.com>
2020-05-31 06:37:10 +0900

    Add labels to CalaendarHeatmap

--- atcoder-problems-frontend/src/components/CalendarHeatmap.tsx
+++ atcoder-problems-frontend/src/components/CalendarHeatmap.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { UncontrolledTooltip } from "reactstrap";
+import { parseDateLabel } from "../utils/DateUtil";
 
 const COLOR_GREY = "#ebedf0";
+const DAY_NAMES_SHORT = ["Mon", "Wed", "Fri"];
 
 interface Props {
   tableData: { date: string; value?: number }[];
@@ -15,12 +17,45 @@ export function CalendarHeatmap(props: Props): React.ReactElement {
   const { tableData } = props;
 
   const blockWidth = 10;
-  const width = blockWidth * props.columns;
-  const height = blockWidth * props.rows;
+  const xOffset = blockWidth * 1.6;
+  const yOffset = blockWidth;
+  const width = xOffset + blockWidth * props.columns;
+  const height = yOffset + blockWidth * props.rows;
   return (
     <div style={{ width: "100%" }}>
       <svg viewBox={`0 0 ${width} ${height}`} style={{ width: "100%" }}>
+        {DAY_NAMES_SHORT.map((day_name, i) => (
+          <text
+            key={day_name}
+            x={0}
+            y={yOffset * 0.7 + (2 * i + 2) * blockWidth}
+            fill="gray"
+            fontSize={7}
+          >
+            {day_name}
+          </text>
+        ))}
+        {tableData.map(({ date }, i) => {
+          const week = Math.floor(i / props.rows);
+          const day = i % props.rows;
+          const d = parseDateLabel(date);
+          if (day === 0 && d.date() <= 7) {
+            return (
+              <text
+                key={`text-${date}`}
+                x={xOffset + week * blockWidth}
+                y={yOffset * 0.7}
+                fill="gray"
+                fontSize={7}
+              >
+                {d.format("MMM")}
+              </text>
+            );
+          }
+          return null;
+        })}
         {tableData.map(({ date, value }, i) => {
+          if (parseDateLabel(date).isAfter()) return null;
           const color =
             value === undefined ? COLOR_GREY : props.getColor(date, value);
           const week = Math.floor(i / props.rows);
@@ -29,10 +64,10 @@ export function CalendarHeatmap(props: Props): React.ReactElement {
             <rect
               key={date}
               id={`rect-${date}`}
-              x={week * blockWidth}
-              y={day * blockWidth}
-              width={blockWidth}
-              height={blockWidth}
+              x={xOffset + week * blockWidth}
+              y={yOffset + day * blockWidth}
+              width={blockWidth * 0.95}
+              height={blockWidth * 0.95}
               fill={color}
             />
           );
