e3dea23b9748c0ec1996fbcf15b68a89c8c788df
Southball <jamie.choi.mail.2002@gmail.com>
2020-06-20 02:51:56 +0800

    feat(frontend): add basic support for dark/light theme

---
+++ atcoder-problems-frontend/src/components/ThemeSelector.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
+import { Helmet } from "react-helmet";
+import { useLocalStorage } from "../utils/LocalStorage";
+
+type Theme = "light" | "dark";
+
+export const ThemeSelector: React.FC = () => {
+  const [theme, setTheme] = useLocalStorage<Theme>("theme", "light");
+
+  return (
+    <>
+      <Helmet>
+        <html className={`theme-${theme}`} />
+      </Helmet>
+
+      <UncontrolledDropdown nav inNavbar>
+        <DropdownToggle nav caret>
+          Theme
+        </DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem
+            tag="a"
+            style={{ cursor: "pointer" }}
+            onClick={(): void => setTheme("light")}
+          >
+            Light
+          </DropdownItem>
+          <DropdownItem
+            tag="a"
+            style={{ cursor: "pointer" }}
+            onClick={(): void => setTheme("dark")}
+          >
+            Dark
+          </DropdownItem>
+        </DropdownMenu>
+      </UncontrolledDropdown>
+    </>
+  );
+};
