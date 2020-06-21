e1928be543493176ebe482b3122fd4b0919f2e54
Southball <jamie.choi.mail.2002@gmail.com>
2020-06-21 19:11:58 +0800

    fix(frontend): improve colors in dark theme

--- atcoder-problems-frontend/src/App.tsx
+++ atcoder-problems-frontend/src/App.tsx
@@ -28,86 +28,94 @@ import SingleProblemList from "./pages/Internal/ProblemList/SingleProblemList";
 import { RecentSubmissions } from "./pages/RecentSubmissions";
 import { TrainingPage } from "./pages/TrainingPage";
 import { ACCOUNT_INFO } from "./utils/RouterPath";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <div>
-        <div className="sticky-top">
-          <NavigationBar />
+    <ThemeProvider>
+      <Router>
+        <div>
+          <div className="sticky-top">
+            <NavigationBar />
+          </div>
+
+          <Container
+            className="my-5"
+            style={{ width: "100%", maxWidth: "90%" }}
+          >
+            <Switch>
+              <Route exact path="/ac" component={ACRanking} />
+              <Route exact path="/fast" component={FastestRanking} />
+              <Route exact path="/short" component={ShortRanking} />
+              <Route exact path="/first" component={FirstRanking} />
+              <Route exact path="/sum" component={SumRanking} />
+              <Route exact path="/streak" component={StreakRanking} />
+              <Route exact path="/lang" component={LanguageOwners} />
+              <Route
+                path="/user/:userIds([a-zA-Z0-9_]+)+"
+                render={({ match }): React.ReactElement => {
+                  const userIds: string | undefined = match.params.userIds;
+                  const userId: string = (userIds ?? "").split("/")[0];
+                  return <UserPage userId={userId} />;
+                }}
+              />
+              <Route
+                path="/table/:userIds([a-zA-Z0-9_]*)*"
+                render={({ match }): React.ReactElement => {
+                  const userIds: string | undefined = match.params.userIds;
+                  const userId = (userIds ?? "").split("/")[0];
+                  const rivals = (userIds ?? "/").split("/");
+                  const rivalList = List(rivals)
+                    .skip(1)
+                    .filter((x) => x.length > 0);
+                  return <TablePage userId={userId} rivals={rivalList} />;
+                }}
+              />
+              <Route
+                path="/list/:userIds([a-zA-Z0-9_]*)*"
+                render={({ match }): React.ReactElement => {
+                  const userIds: string | undefined = match.params.userIds;
+                  const userId = (userIds ?? "").split("/")[0];
+                  const rivals = (userIds ?? "/").split("/");
+                  const rivalList = List(rivals)
+                    .skip(1)
+                    .filter((x) => x.length > 0);
+                  return <ListPage userId={userId} rivals={rivalList} />;
+                }}
+              />
+
+              {/*Virtual Contests*/}
+              <Route
+                path="/contest/show/:contestId([a-zA-Z0-9_-]+)"
+                component={ShowContest}
+              />
+              <Route path="/contest/create" component={ContestCreatePage} />
+              <Route
+                path="/contest/update/:contestId([a-zA-Z0-9_-]+)"
+                component={ContestUpdatePage}
+              />
+              <Route path="/contest/recent" component={RecentContestList} />
+
+              {/*User Settings*/}
+              <Route path={ACCOUNT_INFO} component={MyAccountPage} />
+
+              {/*Problem List*/}
+              <Route
+                path="/problemlist/:listId([a-zA-Z0-9_-]+)"
+                component={SingleProblemList}
+              />
+              <Route path="/submissions/recent" component={RecentSubmissions} />
+
+              {/*Training*/}
+              <Route path="/training" component={TrainingPage} />
+
+              {/*Default Path*/}
+              <Redirect path="/" to="/table/" />
+            </Switch>
+          </Container>
         </div>
-
-        <Container className="my-5" style={{ width: "100%", maxWidth: "90%" }}>
-          <Switch>
-            <Route exact path="/ac" component={ACRanking} />
-            <Route exact path="/fast" component={FastestRanking} />
-            <Route exact path="/short" component={ShortRanking} />
-            <Route exact path="/first" component={FirstRanking} />
-            <Route exact path="/sum" component={SumRanking} />
-            <Route exact path="/streak" component={StreakRanking} />
-            <Route exact path="/lang" component={LanguageOwners} />
-            <Route
-              path="/user/:userIds([a-zA-Z0-9_]+)+"
-              render={({ match }): React.ReactElement => {
-                const userIds: string | undefined = match.params.userIds;
-                const userId: string = (userIds ?? "").split("/")[0];
-                return <UserPage userId={userId} />;
-              }}
-            />
-            <Route
-              path="/table/:userIds([a-zA-Z0-9_]*)*"
-              render={({ match }): React.ReactElement => {
-                const userIds: string | undefined = match.params.userIds;
-                const userId = (userIds ?? "").split("/")[0];
-                const rivals = (userIds ?? "/").split("/");
-                const rivalList = List(rivals)
-                  .skip(1)
-                  .filter((x) => x.length > 0);
-                return <TablePage userId={userId} rivals={rivalList} />;
-              }}
-            />
-            <Route
-              path="/list/:userIds([a-zA-Z0-9_]*)*"
-              render={({ match }): React.ReactElement => {
-                const userIds: string | undefined = match.params.userIds;
-                const userId = (userIds ?? "").split("/")[0];
-                const rivals = (userIds ?? "/").split("/");
-                const rivalList = List(rivals)
-                  .skip(1)
-                  .filter((x) => x.length > 0);
-                return <ListPage userId={userId} rivals={rivalList} />;
-              }}
-            />
-
-            {/*Virtual Contests*/}
-            <Route
-              path="/contest/show/:contestId([a-zA-Z0-9_-]+)"
-              component={ShowContest}
-            />
-            <Route path="/contest/create" component={ContestCreatePage} />
-            <Route
-              path="/contest/update/:contestId([a-zA-Z0-9_-]+)"
-              component={ContestUpdatePage}
-            />
-            <Route path="/contest/recent" component={RecentContestList} />
-
-            {/*User Settings*/}
-            <Route path={ACCOUNT_INFO} component={MyAccountPage} />
-
-            {/*Problem List*/}
-            <Route
-              path="/problemlist/:listId([a-zA-Z0-9_-]+)"
-              component={SingleProblemList}
-            />
-            <Route path="/submissions/recent" component={RecentSubmissions} />
-
-            {/*Training*/}
-            <Route path="/training" component={TrainingPage} />
-            <Redirect path="/" to="/table/" />
-          </Switch>
-        </Container>
-      </div>
-    </Router>
+      </Router>
+    </ThemeProvider>
   );
 };
 
