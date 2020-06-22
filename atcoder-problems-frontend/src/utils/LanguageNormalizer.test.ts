47e7d961828fb878bfd9c69d249b2b7659162774
kenkoooo <kenkou.n@gmail.com>
2020-06-23 00:34:37 +0900

    Added LanguageNormalizer

---
+++ atcoder-problems-frontend/src/utils/LanguageNormalizer.test.ts
@@ -0,0 +1,10 @@
+import { normalizeLanguage } from "./LanguageNormalizer";
+
+test("normalize language", () => {
+  expect(normalizeLanguage("Perl (v5.18.2)")).toBe("Perl");
+  expect(normalizeLanguage("Perl6 (rakudo-star 2016.01)")).toBe("Perl6");
+  expect(normalizeLanguage("Rust (1.42.0)")).toBe("Rust");
+  expect(normalizeLanguage("C++11 (Clang++ 3.4)")).toBe("C++");
+  expect(normalizeLanguage("Scala (2.11.7)")).toBe("Scala");
+  expect(normalizeLanguage("Fortran(GNU Fortran 9.2.1)")).toBe("Fortran");
+});
