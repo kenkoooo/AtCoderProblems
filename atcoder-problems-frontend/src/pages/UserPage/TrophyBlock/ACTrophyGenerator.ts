35dd37f7b8a8fd63c038384d2b9912cc61905e9a
25toma <2525toa@gmail.com>
2020-06-25 15:28:22 +0900

    fix: erase function - generateACTrophiesWithProblems

--- atcoder-problems-frontend/src/pages/UserPage/TrophyBlock/ACTrophyGenerator.ts
+++ atcoder-problems-frontend/src/pages/UserPage/TrophyBlock/ACTrophyGenerator.ts
@@ -15,26 +15,6 @@ const generateACTrophiesWithOneProblem = (
   });
 };
 
-const generateACTrophiesWithProblems = (
-  acProblemIds: Set<string>
-): Trophy[] => {
-  const milestones: [string, string, string[]][] = [
-    [
-      "World traveler2019",
-      "Accept all problems in WorldTourFinals2019",
-      ["wtf19_a", "wtf19_b", "wtf19_c1", "wtf19_c2", "wtf19_d", "wtf19_e"],
-    ],
-  ];
-  return milestones.map(([draftTitle, reason, problem_ids]) => {
-    const title = draftTitle;
-    const achieved = problem_ids.every((problem_id) =>
-      acProblemIds.has(problem_id)
-    );
-    const sortId = `accept-all-${draftTitle}`;
-    return { title, reason, achieved, sortId };
-  });
-};
-
 const uniqueACProblemIds = (submissions: TrophySubmission[]): Set<string> => {
   const acProblemIds = submissions
     .filter((submissions) => isAccepted(submissions.submission.result))
@@ -48,6 +28,5 @@ export const generateACTrophies = (
   const trophies = [] as Trophy[];
   const acProblemIds = uniqueACProblemIds(allSubmissions);
   trophies.push(...generateACTrophiesWithOneProblem(acProblemIds));
-  trophies.push(...generateACTrophiesWithProblems(acProblemIds));
   return trophies;
 };
