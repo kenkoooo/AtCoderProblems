10ccc8e24f750844c673d8a7577550a860d039b9
kenkoooo <kenkou.n@gmail.com>
2020-06-28 16:51:28 +0900

    Add API to use is_public

--- atcoder-problems-backend/tests/test_server_e2e_virtual_contest.rs
+++ atcoder-problems-backend/tests/test_server_e2e_virtual_contest.rs
@@ -45,7 +45,6 @@ fn setup() -> u16 {
 
 #[async_std::test]
 async fn test_virtual_contest() -> Result<()> {
-    simple_logger::init_with_level(log::Level::Info).unwrap();
     let port = setup();
     let server = task::spawn(async move {
         let pool = initialize_pool(utils::SQL_URL).unwrap();
@@ -235,6 +234,100 @@ async fn test_virtual_contest() -> Result<()> {
             }
         ])
     );
+
+    server.race(async_std::future::ready(())).await;
+    Ok(())
+}
+
+#[async_std::test]
+async fn test_virtual_contest_visibility() -> Result<()> {
+    let port = setup();
+    let server = task::spawn(async move {
+        let pool = initialize_pool(utils::SQL_URL).unwrap();
+        run_server(pool, MockAuth, port).await.unwrap();
+    });
+    task::sleep(std::time::Duration::from_millis(1000)).await;
+    surf::get(url(
+        &format!("/internal-api/authorize?code={}", VALID_CODE),
+        port,
+    ))
+    .await?;
+    let cookie_header = format!("token={}", VALID_TOKEN);
+
+    let mut response = surf::post(url("/internal-api/contest/create", port))
+        .set_header("Cookie", cookie_header.as_str())
+        .body_json(&json!({
+            "title":"visible",
+            "memo": "",
+            "start_epoch_second": 1,
+            "duration_second": 2
+        }))?
+        .await?;
+    assert!(response.status().is_success());
+    let body = response.body_json::<Value>().await?;
+    let contest_id = body["contest_id"].as_str().unwrap();
+
+    let response = surf::get(url("/internal-api/contest/recent", port))
+        .recv_json::<Value>()
+        .await?;
+    assert_eq!(response[0]["id"].as_str().unwrap(), contest_id);
+    assert_eq!(response.as_array().unwrap().len(), 1);
+
+    let response = surf::post(url("/internal-api/contest/update", port))
+        .set_header("Cookie", cookie_header.as_str())
+        .body_json(&json!({
+            "id": format!("{}", contest_id),
+            "title":"invisible",
+            "memo": "",
+            "start_epoch_second": 1,
+            "duration_second": 2,
+            "is_public": false
+        }))?
+        .await?;
+    assert!(response.status().is_success());
+
+    let response = surf::get(url("/internal-api/contest/recent", port))
+        .recv_json::<Value>()
+        .await?;
+    assert_eq!(response.as_array().unwrap().len(), 0);
+
+    let mut response = surf::post(url("/internal-api/contest/create", port))
+        .set_header("Cookie", cookie_header.as_str())
+        .body_json(&json!({
+            "title":"invisible",
+            "memo": "",
+            "start_epoch_second": 1,
+            "duration_second": 2,
+            "is_public": false
+        }))?
+        .await?;
+    assert!(response.status().is_success());
+    let body = response.body_json::<Value>().await?;
+    let contest_id = body["contest_id"].as_str().unwrap();
+
+    let response = surf::get(url("/internal-api/contest/recent", port))
+        .recv_json::<Value>()
+        .await?;
+    assert_eq!(response.as_array().unwrap().len(), 0);
+
+    let response = surf::post(url("/internal-api/contest/update", port))
+        .set_header("Cookie", cookie_header.as_str())
+        .body_json(&json!({
+            "id": contest_id,
+            "title":"visible",
+            "memo": "",
+            "start_epoch_second": 1,
+            "duration_second": 2,
+            "is_public": true
+        }))?
+        .await?;
+    assert!(response.status().is_success());
+
+    let response = surf::get(url("/internal-api/contest/recent", port))
+        .recv_json::<Value>()
+        .await?;
+    assert_eq!(response.as_array().unwrap().len(), 1);
+    assert_eq!(response[0]["id"].as_str().unwrap(), contest_id);
 
     server.race(async_std::future::ready(())).await;
     Ok(())
