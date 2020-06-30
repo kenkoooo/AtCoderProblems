ff44c774b466b9de4b90beb518d21a2e14dda114
MatsuTaku <tkm.matsumoto@gmail.com>
2020-07-01 03:18:12 +0900

    RelativeDifficultyIcon: change difficulty level section to 1/4

--- atcoder-problems-frontend/src/components/RelativeDifficultyIcon.tsx
+++ atcoder-problems-frontend/src/components/RelativeDifficultyIcon.tsx
@@ -11,50 +11,85 @@ interface Props {
   userInternalRating: number;
 }
 
+// const LEVEL_NAMES = ["Easy", "Moderate", "Difficult"];
+const LEVEL_SECTION = 1 / 4;
+
+const DifficultyLevel = {
+  Easy: 0,
+  Moderate: 1,
+  Difficult: 2,
+} as const;
+type DifficultyLevel = typeof DifficultyLevel[keyof typeof DifficultyLevel];
+
+const getDifficultyLevel = (relativeDifficultyRatio: number): DifficultyLevel => {
+  if (relativeDifficultyRatio <= LEVEL_SECTION) return DifficultyLevel.Easy;
+  else if (relativeDifficultyRatio <= 1 - LEVEL_SECTION)
+    return DifficultyLevel.Moderate;
+  else return DifficultyLevel.Difficult;
+};
+
+const getFillRatio = (relativeDifficultyRatio: number, level: DifficultyLevel) => {
+  switch (level) {
+    case DifficultyLevel.Easy:
+      return relativeDifficultyRatio / LEVEL_SECTION;
+    case DifficultyLevel.Moderate:
+      return (relativeDifficultyRatio - LEVEL_SECTION) / (LEVEL_SECTION * 2);
+    case DifficultyLevel.Difficult:
+      return (relativeDifficultyRatio - LEVEL_SECTION * 3) / LEVEL_SECTION;
+    default:
+      return 0;
+  }
+};
+
 const getRGB = (code: string) => {
-  const r: number = parseInt(code.slice(1, 3), 16);
-  const g: number = parseInt(code.slice(3, 5), 16);
-  const b: number = parseInt(code.slice(5, 7), 16);
+  const r = parseInt(code.slice(1, 3), 16);
+  const g = parseInt(code.slice(3, 5), 16);
+  const b = parseInt(code.slice(5, 7), 16);
   return [r, g, b];
 };
 
-const getLevelColors = (theme: Theme) => {
-  return [
-    theme.relativeDifficultyEasyColor,
-    theme.relativeDifficultyModerateColor,
-    theme.relativeDifficultyDifficultColor,
-  ];
+const getLevelColor = (level: DifficultyLevel, theme: Theme) => {
+  switch (level) {
+    case DifficultyLevel.Easy:
+      return theme.relativeDifficultyEasyColor;
+    case DifficultyLevel.Moderate:
+      return theme.relativeDifficultyModerateColor;
+    case DifficultyLevel.Difficult:
+      return theme.relativeDifficultyDifficultColor;
+    default:
+      return theme.relativeDifficultyBackgroundColor;
+  }
 };
 
-const LEVEL_NAMES = ["Easy", "Moderate", "Difficult"];
+const rgbStyle = (r: number, g: number, b: number, ratio: number) =>
+  `rgb(${r}, ${g}, ${b}) ${ratio * 100}%`;
 
 const RelativeDifficultyIcon: React.FC<Props> = (props) => {
   const { id, problemModel, userInternalRating } = props;
+
   const predictedSolveProbability = predictSolveProbability(
     problemModel,
     userInternalRating
   );
   const relativeDifficultyRatio = 1 - predictedSolveProbability;
-  const difficultyLevel = (relativeDifficultyRatio * 3) | 0;
-  const fillRatio = (relativeDifficultyRatio - 0.333 * difficultyLevel) / 0.333;
+  const difficultyLevel = getDifficultyLevel(relativeDifficultyRatio);
+  const fillRatio = getFillRatio(relativeDifficultyRatio, difficultyLevel);
+
   const theme = useTheme();
-  const color = getLevelColors(theme)[difficultyLevel];
+  const color = getLevelColor(difficultyLevel, theme);
   const [r, g, b] = getRGB(color);
   const [bg_r, bg_g, bg_b] = getRGB(theme.relativeDifficultyBackgroundColor);
-  const color_styles = [
-    `rgb(${r}, ${g}, ${b}) 0%`,
-    `rgb(${r}, ${g}, ${b}) ${fillRatio * 100}%`,
-    `rgb(${bg_r}, ${bg_g}, ${bg_b}) ${fillRatio * 100}%`,
-    `rgb(${bg_r}, ${bg_g}, ${bg_b}) 100%`,
-  ];
 
   const styleOptions = Object({
     borderColor: color,
-    background: `linear-gradient(to right, ${color_styles.join(", ")})`,
+    background: `linear-gradient(to right, ${[
+      rgbStyle(r, g, b, 0),
+      rgbStyle(r, g, b, fillRatio),
+      rgbStyle(bg_r, bg_g, bg_b, fillRatio),
+      rgbStyle(bg_r, bg_g, bg_b, 1),
+    ].join(", ")})`,
   });
-  const description = `Relative difficulty: ${
-    LEVEL_NAMES[difficultyLevel]
-  }\nPredicted solve probability: ${Math.round(
+  const description = `Predicted Solve Probability of User: ${Math.round(
     predictedSolveProbability * 100
   )}%`;
   const iconId = `RelativeDifficultyIcon-${id}`;
