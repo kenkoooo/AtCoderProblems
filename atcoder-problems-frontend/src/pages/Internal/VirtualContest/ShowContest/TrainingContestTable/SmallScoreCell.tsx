c88fb41bcdfab13ce4b4fdf5418a6f42c035a27a
Southball <jamie.choi.mail.2002@gmail.com>
2020-07-05 02:20:49 +0800

    frontend: refactoring

---
+++ atcoder-problems-frontend/src/pages/Internal/VirtualContest/ShowContest/TrainingContestTable/SmallScoreCell.tsx
@@ -0,0 +1,60 @@
+import { isNumber } from "util";
+import React, { useState } from "react";
+import { Popover } from "reactstrap";
+import ScoreCell from "../ScoreCell";
+import ProblemLink from "../../../../../components/ProblemLink";
+
+interface Props {
+  problem: {
+    title?: string;
+    id: string;
+    contestId?: string;
+    point: number | null;
+  };
+  maxPoint?: number;
+  trials?: number;
+  time?: number;
+  id: string;
+}
+
+const SmallScoreCell: React.FC<Props> = (props) => {
+  const { maxPoint, trials, problem, time, id } = props;
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const cellId = `small-score-cell-${id}`;
+  const classes =
+    "small-score-cell " +
+    (isNumber(maxPoint) && isNumber(trials) && isNumber(time)
+      ? (maxPoint === 0 && trials > 0 ? "small-score-cell-warning " : "") +
+        (maxPoint > 0 ? "small-score-cell-success" : "")
+      : "");
+
+  return (
+    <div
+      className={classes}
+      id={cellId}
+      onMouseOver={() => setShowTooltip(true)}
+      onMouseLeave={() => setShowTooltip(false)}
+    >
+      <Popover isOpen={showTooltip} placement="top" target={"#" + cellId}>
+        <div className="small-score-cell-tooltip">
+          {problem.contestId && problem.title ? (
+            <ProblemLink
+              problemId={problem.id}
+              contestId={problem.contestId}
+              problemTitle={problem.title}
+            />
+          ) : (
+            <b>{problem.title}</b>
+          )}
+          <br />
+          {isNumber(maxPoint) && isNumber(trials) && isNumber(time) ? (
+            <ScoreCell maxPoint={maxPoint} time={time} trials={trials} />
+          ) : null}
+        </div>
+      </Popover>
+    </div>
+  );
+};
+
+export default SmallScoreCell;
---
+++ atcoder-problems-frontend/src/pages/Internal/VirtualContest/ShowContest/TrainingContestTable/index.tsx
@@ -0,0 +1,166 @@
+import { Table } from "reactstrap";
+import React from "react";
+import { connect, PromiseState } from "react-refetch";
+import { List, Map as ImmutableMap } from "immutable";
+import { VirtualContestItem } from "../../../types";
+import { isAccepted } from "../../../../../utils";
+import { ProblemId } from "../../../../../interfaces/Status";
+import ProblemModel from "../../../../../interfaces/ProblemModel";
+import Submission from "../../../../../interfaces/Submission";
+import {
+  cachedProblemModels,
+  fetchVirtualContestSubmission,
+} from "../../../../../utils/CachedApiClient";
+import { extractBestSubmissions, BestSubmissionEntry } from "../util";
+import { compareProblem, calcTotalResult } from "../ContestTable";
+import SmallScoreCell from "./SmallScoreCell";
+
+const getSortedUserIds = (
+  users: string[],
+  problems: VirtualContestItem[],
+  bestSubmissions: BestSubmissionEntry[]
+): string[] => {
+  return users
+    .map((userId) => {
+      const result = calcTotalResult(userId, problems, bestSubmissions);
+      return { userId, ...result };
+    })
+    .sort((a, b) => {
+      if (a.solveCount === b.solveCount) {
+        return a.lastBestSubmissionTime - b.lastBestSubmissionTime;
+      }
+      return b.solveCount - a.solveCount;
+    })
+    .map((e) => e.userId);
+};
+
+interface OuterProps {
+  readonly showProblems: boolean;
+  readonly problems: {
+    item: VirtualContestItem;
+    title?: string;
+    contestId?: string;
+  }[];
+  readonly users: string[];
+  readonly start: number;
+  readonly end: number;
+  readonly enableAutoRefresh: boolean;
+}
+
+interface InnerProps extends OuterProps {
+  submissions: PromiseState<Submission[]>;
+  problemModels: PromiseState<ImmutableMap<ProblemId, ProblemModel>>;
+}
+
+const InnerContestTable: React.FC<InnerProps> = (props) => {
+  const { showProblems, problems, users, start } = props;
+  const submissionMap = props.submissions.fulfilled
+    ? props.submissions.value
+        .filter((s) => s.result !== "CE")
+        .reduce((map, s) => {
+          const list = map.get(s.problem_id) ?? ([] as Submission[]);
+          list.push(s);
+          map.set(s.problem_id, list);
+          return map;
+        }, new Map<ProblemId, Submission[]>())
+    : new Map<ProblemId, Submission[]>();
+  const problemIds = problems.map((p) => p.item.id);
+
+  const bestSubmissions = extractBestSubmissions(
+    submissionMap,
+    users,
+    problemIds
+  );
+  const sortedUserIds = getSortedUserIds(
+    users,
+    problems.map((p) => p.item),
+    bestSubmissions
+  );
+
+  const items = problems.map((p) => ({
+    contestId: p.contestId,
+    title: p.title,
+    ...p.item,
+  }));
+
+  return (
+    <Table striped>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Participant</th>
+          <th>Progress</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sortedUserIds.map((userId, i) => {
+          return (
+            <tr key={i}>
+              <th>{i + 1}</th>
+              <th>{userId}</th>
+              <td style={{ lineHeight: 0, minWidth: "15rem" }}>
+                {!showProblems
+                  ? null
+                  : items.sort(compareProblem).map((problem, index) => {
+                      const info = bestSubmissions.find(
+                        (e) => e.userId === userId && e.problemId === problem.id
+                      )?.bestSubmissionInfo;
+                      if (!info) {
+                        return (
+                          <SmallScoreCell
+                            key={problem.id}
+                            problem={problem}
+                            id={`${userId}-${index}`}
+                          />
+                        );
+                      }
+                      const best = info.bestSubmission;
+                      const trials =
+                        info.trialsBeforeBest +
+                        (isAccepted(info.bestSubmission.result) ? 0 : 1);
+                      const point =
+                        problem.point !== null
+                          ? isAccepted(best.result)
+                            ? problem.point
+                            : 0
+                          : best.point;
+                      return (
+                        <SmallScoreCell
+                          key={problem.id}
+                          problem={problem}
+                          trials={trials}
+                          maxPoint={point}
+                          time={best.epoch_second - start}
+                          id={`${userId}-${index}`}
+                        />
+                      );
+                    })}
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </Table>
+  );
+};
+
+export const TrainingContestTable = connect<OuterProps, InnerProps>(
+  (props) => ({
+    submissions: {
+      comparison: null,
+      value: (): Promise<List<Submission>> =>
+        fetchVirtualContestSubmission(
+          props.users,
+          props.problems.map((p) => p.item.id),
+          props.start,
+          props.end
+        ),
+      refreshInterval: props.enableAutoRefresh ? 60_000 : 1_000_000_000,
+      force: props.enableAutoRefresh,
+    },
+    problemModels: {
+      comparison: null,
+      value: () => cachedProblemModels(),
+    },
+  })
+)(InnerContestTable);
