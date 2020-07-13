d40b654dc98b7633e512a47ead75a2d8e04ebad7
Jamie Choi <jamie.choi.mail.2002@gmail.com>
2020-07-14 00:27:41 +0800

    feat(frontend): add explanation for different modes

---
+++ atcoder-problems-frontend/src/components/HelpBadgeModal.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Modal, ModalHeader, ModalBody, Badge, Tooltip } from "reactstrap";
+
+interface Props {
+  id: string;
+  children: React.ReactNode;
+  title?: React.ReactNode;
+  tooltipText?: React.ReactNode;
+}
+
+const HelpBadgeModal = (props: Props) => {
+  const { title, children, id, tooltipText } = props;
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [isTooltipOpen, setTooltipOpen] = useState(false);
+
+  const toggleModal = (): void => setModalOpen(!isModalOpen);
+  const badgeId = "HelpBadgeTooltipModal-" + id;
+
+  return (
+    <>
+      <Badge
+        color="secondary"
+        pill
+        id={badgeId}
+        style={{ cursor: "pointer" }}
+        onClick={toggleModal}
+      >
+        ?
+      </Badge>
+      <Tooltip
+        placement="top"
+        target={badgeId}
+        isOpen={isTooltipOpen}
+        toggle={(): void => setTooltipOpen(!isTooltipOpen)}
+      >
+        {tooltipText ?? "Click to see detailed explanation."}
+      </Tooltip>
+      <Modal isOpen={isModalOpen} toggle={toggleModal}>
+        <ModalHeader toggle={toggleModal}>{title ?? "Help"}</ModalHeader>
+        <ModalBody>{children}</ModalBody>
+      </Modal>
+    </>
+  );
+};
+
+export default HelpBadgeModal;
