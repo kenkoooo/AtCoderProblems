ab31e4a590c506059fd4609b2a27eda22cb58fcc
sirogamiemacs <sirogamiemacs@gmail.com>
2020-08-03 16:56:03 +0900

    add percentages in SinglePieChart

--- atcoder-problems-frontend/src/components/SinglePieChart.tsx
+++ atcoder-problems-frontend/src/components/SinglePieChart.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   PieChart,
   Pie,
   ResponsiveContainer,
   Cell,
-  Tooltip,
+  Sector,
   Legend,
 } from "recharts";
 
@@ -17,18 +17,102 @@ interface Props {
   data: Entry[];
   hideLegend?: boolean;
 }
+interface RenderProps {
+  cx: number;
+  cy: number;
+  innerRadius: number;
+  outerRadius: number;
+  startAngle: number;
+  endAngle: number;
+  fill: string;
+  payload: Entry;
+  value: number;
+  percent: number;
+}
+
+const renderActiveShape = (props: RenderProps) => {
+  const {
+    cx,
+    cy,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    value,
+    percent,
+  } = props;
+
+  const text: string = ((name: string) => {
+    if (name === "Accepted") return "AC";
+    else return name;
+  })(payload.name);
+
+  return (
+    <g>
+      <text
+        x={cx}
+        y={cy}
+        textAnchor="middle"
+        fill={"#111"}
+      >{`${text} : ${value}`}</text>
+      <text x={cx} y={cy} dy={18} textAnchor="middle" fill={"#aaa"}>{`(${(
+        percent * 100
+      ).toFixed(1)}%)`}</text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 3}
+        outerRadius={outerRadius + 6}
+        fill={fill}
+      />
+    </g>
+  );
+};
 
 export const SinglePieChart: React.FC<Props> = ({ data, hideLegend }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  // eslint-disable-next-line
+  const onPieEnter = ({}, index: number) => {
+    setActiveIndex(index);
+  };
+
+  const onPieLeave = () => {
+    setActiveIndex(0);
+  };
+
   return (
     <div>
       <ResponsiveContainer width="100%" height={300}>
         <PieChart>
-          <Pie dataKey="value" data={data} outerRadius="80%" fill="#ff0000">
+          <Pie
+            activeIndex={activeIndex}
+            activeShape={renderActiveShape}
+            dataKey="value"
+            data={data}
+            innerRadius="60%"
+            outerRadius="80%"
+            fill="#ff0000"
+            onMouseEnter={onPieEnter}
+            onMouseLeave={onPieLeave}
+          >
             {data.map((e) => (
               <Cell key={e.name} fill={e.color} />
             ))}
           </Pie>
-          <Tooltip />
           {!hideLegend && <Legend />}
         </PieChart>
       </ResponsiveContainer>
