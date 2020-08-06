c3e120991a9fb71b70537959dc2898114678b3b4
Yusuke Tanaka <yusuktan@maguro.dev>
2020-08-07 00:20:48 +0900

    refactor(sql-client): port `AcceptedCountClient` to sql-client

--- atcoder-problems-backend/sql-client/src/lib.rs
+++ atcoder-problems-backend/sql-client/src/lib.rs
@@ -1,9 +1,15 @@
 use anyhow::Result;
 use std::time::Duration;
 
+pub mod accepted_count;
 pub mod internal;
+pub mod models;
 
 pub type PgPool = sqlx::postgres::PgPool;
+
+const FIRST_AGC_EPOCH_SECOND: i64 = 1_468_670_400;
+const UNRATED_STATE: &str = "-";
+const MAX_INSERT_ROWS: usize = 10_000;
 
 pub async fn initialize_pool<S: AsRef<str>>(database_url: S) -> Result<PgPool> {
     let pool = PgPool::builder()
