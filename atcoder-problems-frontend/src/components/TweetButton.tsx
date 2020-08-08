23c50aff42391d7e0262cd6981d6ab1120eabf56
Makutamoto <34860673+makutamoto@users.noreply.github.com>
2020-08-08 21:45:51 +0900

    fixed TweetButton

--- atcoder-problems-frontend/src/components/TweetButton.tsx
+++ atcoder-problems-frontend/src/components/TweetButton.tsx
@@ -1,61 +1,26 @@
 import React from "react";
 import { Button } from "reactstrap";
 
-interface TweetButtonProps {
-  title: string;
+interface Props {
   id: string;
+  label: string;
+  color: string;
+  children: string;
 }
 
-interface ShareitButtonProps extends TweetButtonProps {
-  username: string;
-  rank: number;
-}
-
-const internalUrl = (id: string) =>
-  `https://kenkoooo.com/atcoder/#/contest/show/${id}`;
-const shareUrl = (internalUrl: string, text: string) =>
-  `https://twitter.com/intent/tweet?url=${encodeURIComponent(
+export const TweetButton: React.FC<Props> = (props) => {
+  const internalUrl = `https://kenkoooo.com/atcoder/#/contest/show/${props.id}`;
+  const shareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(
     internalUrl
-  )}&text=${encodeURIComponent(text)}&hashtags=AtCoderProblems`;
-
-function rankToString(rank: number) {
-  const rankString = rank.toString();
-  if (11 <= rank && rank <= 13) {
-    return rankString + "th";
-  } else {
-    const digit1 = rank % 10;
-    if (digit1 === 1) return rankString + "st";
-    else if (digit1 === 2) return rankString + "nd";
-    else if (digit1 === 3) return rankString + "rd";
-    else return rankString + "th";
-  }
-}
-
-export const TweetButton: React.FC<TweetButtonProps> = (props) => {
+  )}&text=${encodeURIComponent(props.children)}&hashtags=AtCoderProblems`;
   return (
     <Button
-      href={shareUrl(internalUrl(props.id), props.title)}
+      href={shareUrl}
       rel="noopener noreferrer"
       target="_blank"
-      color="primary"
+      color={props.color}
     >
-      Tweet
-    </Button>
-  );
-};
-
-export const ShareitButton: React.FC<ShareitButtonProps> = (props) => {
-  const message = `${props.username} took ${rankToString(
-    props.rank
-  )} place in ${props.title}!`;
-  return (
-    <Button
-      href={shareUrl(internalUrl(props.id), message)}
-      rel="noopener noreferrer"
-      target="_blank"
-      color="link"
-    >
-      Share it!
+      {props.label}
     </Button>
   );
 };
