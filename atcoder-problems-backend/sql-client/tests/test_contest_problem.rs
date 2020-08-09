032978ca4e07fdbeb3c18ede86393184ffc4258b
Yusuke Tanaka <yusuktan@maguro.dev>
2020-08-09 09:16:02 +0900

    fix(sql-client): make `insert_contest_problem` return unit

--- atcoder-problems-backend/sql-client/tests/test_contest_problem.rs
+++ atcoder-problems-backend/sql-client/tests/test_contest_problem.rs
@@ -15,20 +15,16 @@ async fn test_contest_problem() {
     let pool = utils::initialize_and_connect_to_test_sql().await;
     assert!(pool.load_contest_problem().await.unwrap().is_empty());
 
-    let inserted_rows = pool
-        .insert_contest_problem(&vec![create_problem(1), create_problem(2)])
+    pool.insert_contest_problem(&vec![create_problem(1), create_problem(2)])
         .await
         .unwrap();
-    assert_eq!(inserted_rows, 2);
     assert_eq!(
         pool.load_contest_problem().await.unwrap(),
         vec![create_problem(1), create_problem(2)]
     );
-    let inserted_rows = pool
-        .insert_contest_problem(&vec![create_problem(1)])
+    pool.insert_contest_problem(&vec![create_problem(1)])
         .await
         .unwrap();
-    assert_eq!(inserted_rows, 0);
     assert_eq!(
         pool.load_contest_problem().await.unwrap(),
         vec![create_problem(1), create_problem(2)]
