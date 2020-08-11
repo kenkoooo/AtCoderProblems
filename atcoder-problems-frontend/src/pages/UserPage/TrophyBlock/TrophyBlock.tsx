24d9dbcb1eddc5f68d4227de24aebbe0e1df78f7
Southball <jamie.choi.mail.2002@gmail.com>
2020-08-11 17:16:35 +0800

    feat(frontend): remove trophy group selector

--- atcoder-problems-frontend/src/pages/UserPage/TrophyBlock/TrophyBlock.tsx
+++ atcoder-problems-frontend/src/pages/UserPage/TrophyBlock/TrophyBlock.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Row, Col, ListGroup, ListGroupItem, Badge } from "reactstrap";
+import React from "react";
+import { Row, Col } from "reactstrap";
 import Contest from "../../../interfaces/Contest";
 import Problem from "../../../interfaces/Problem";
 import ProblemModel from "../../../interfaces/ProblemModel";
@@ -10,7 +10,7 @@ import { generateACCountTrophies } from "./ACCountTrophyGenerator";
 import { generateACProblemsTrophies } from "./ACProblemsTrophyGenerator";
 import { generateStreakTrophies } from "./StreakTrophyGenerator";
 import { generateCompleteContestTrophies } from "./CompleteContestTrophyGenerator";
-import { Trophy, TrophyGroup, TrophyGroups } from "./Trophy";
+import { Trophy, TrophyGroup } from "./Trophy";
 import { TrophySubgroup } from "./TrophySubgroup";
 
 interface Props {
@@ -39,61 +39,40 @@ export const TrophyBlock = (props: Props): JSX.Element => {
     )
   );
 
-  const [filterGroup, setFilterGroup] = useState<TrophyGroup | "All">("All");
-  const filteredTrophies = trophies
-    .sort((a, b) => a.sortId.localeCompare(b.sortId))
-    .filter((t) => filterGroup === "All" || t.group === filterGroup);
-  const subgroupedTrophies = groupBy(filteredTrophies, (t) => t.subgroup);
+  const sortedTrophies = trophies.sort((a, b) =>
+    a.sortId.localeCompare(b.sortId)
+  );
+  const groupedTrophies: [TrophyGroup, [string, Trophy[]][]][] = Array.from(
+    groupBy(sortedTrophies, (t) => t.group).entries()
+  ).map(([group, trophies]) => [
+    group,
+    Array.from(groupBy(trophies, (t) => t.subgroup).entries()),
+  ]);
 
   const achievedIds = new Set(
     trophies.filter((t) => t.achieved).map((t) => t.sortId)
   );
-
-  const groupChoices: [TrophyGroup | "All", number][] = [
-    ["All", trophies.filter((t) => t.achieved).length],
-    ...TrophyGroups.map(
-      (group) =>
-        [
-          group,
-          trophies.filter((t) => t.group === group && t.achieved).length,
-        ] as [TrophyGroup, number]
-    ),
-  ];
 
   return (
     <>
       <Row className="my-2">
         <h2>{achievedIds.size} Trophies</h2>
       </Row>
-      <Row>
-        <Col md="12" lg="3" className="mb-3">
-          <ListGroup>
-            {groupChoices.map(([group, count]) => (
-              <ListGroupItem
-                key={group}
-                tag="button"
-                action
-                active={filterGroup === group}
-                onClick={() => setFilterGroup(group)}
-              >
-                {group} <Badge pill>{count}</Badge>
-              </ListGroupItem>
-            ))}
-          </ListGroup>
-        </Col>
-        <Col md="12" lg="9">
-          {Array.from(subgroupedTrophies.entries()).map(
-            ([subgroup, trophies]) => (
+      {groupedTrophies.map(([group, subgroupedTrophies]) => (
+        <Row key={`trophy-group-${group.replace(/[ #]/g, "-")}`}>
+          <Col>
+            <h3>{group}</h3>
+            {subgroupedTrophies.map(([subgroup, trophies]) => (
               <TrophySubgroup
                 key={`trophy-subgroup-${subgroup.replace(/[ #]/g, "-")}`}
                 achievedIds={achievedIds}
                 title={subgroup}
                 trophies={trophies}
               />
-            )
-          )}
-        </Col>
-      </Row>
+            ))}
+          </Col>
+        </Row>
+      ))}
     </>
   );
 };
