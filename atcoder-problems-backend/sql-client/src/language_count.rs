57cd4556e7f04ccfad6dda2d0b823bb580ef3e19
Yusuke Tanaka <yusuktan@maguro.dev>
2020-08-19 23:42:40 +0900

    fix(sql-client): parse language correctly

--- atcoder-problems-backend/sql-client/src/language_count.rs
+++ atcoder-problems-backend/sql-client/src/language_count.rs
@@ -16,7 +16,6 @@ pub trait LanguageCountClient {
 #[async_trait]
 impl LanguageCountClient for PgPool {
     async fn update_language_count(&self, submissions: &[Submission]) -> Result<()> {
-        let re = Regex::new(r"\d* \(.*\)").unwrap();
         let language_count = submissions
             .iter()
             .map(|s| {
@@ -29,11 +28,7 @@ impl LanguageCountClient for PgPool {
             .fold(
                 BTreeMap::new(),
                 |mut map, (user_id, problem_id, language)| {
-                    let simplified_language = if language.len() >= 5 && &language[..5] == "Perl6" {
-                        "Perl6".to_string()
-                    } else {
-                        re.replace(&language, "").to_string()
-                    };
+                    let simplified_language = simplify_language(&language);
                     map.entry((user_id, simplified_language))
                         .or_insert_with(BTreeSet::new)
                         .insert(problem_id);
@@ -100,5 +95,32 @@ impl LanguageCountClient for PgPool {
         .fetch_all(self)
         .await?;
         Ok(count)
+    }
+}
+
+fn simplify_language(lang: &str) -> String {
+    let re = Regex::new(r"\d* \(.*\)").unwrap();
+    if lang.starts_with("Perl6") {
+        "Perl6".to_string()
+    } else if lang.starts_with("Fortran") {
+        "Fortran".to_string()
+    } else {
+        re.replace(lang, "").to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_simplify_language() {
+        assert_eq!(simplify_language("language1"), "language1");
+        assert_eq!(simplify_language("Perl (5)"), "Perl");
+        assert_eq!(simplify_language("Perl6"), "Perl6");
+        assert_eq!(simplify_language("Fortran(GNU Fortran 9.2.1)"), "Fortran");
+        assert_eq!(simplify_language("Ada2012 (GNAT 9.2.1)"), "Ada");
+        assert_eq!(simplify_language("PyPy2 (7.3.0)"), "PyPy");
+        assert_eq!(simplify_language("Haxe (4.0.3); js"), "Haxe; js");
     }
 }
