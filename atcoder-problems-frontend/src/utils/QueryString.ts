5c2e6e6af5c6490110e144d4052628775ae227eb
MatsuTaku <tkm.matsumoto@gmail.com>
2021-02-06 20:34:12 +0900

    Implement logic of creating virtual contest from selected problems in Recommendation table.

--- atcoder-problems-frontend/src/utils/QueryString.ts
+++ atcoder-problems-frontend/src/utils/QueryString.ts
@@ -1,4 +1,5 @@
 import { Location } from "history";
+import { ProblemId } from "../interfaces/Status";
 
 export const generatePathWithParams = (
   location: Location,
@@ -8,4 +9,16 @@ export const generatePathWithParams = (
   Object.keys(params).forEach((key) => searchParams.set(key, params[key]));
 
   return `${location.pathname}?${searchParams.toString()}`;
+};
+
+export const separateSymbol = "~";
+
+export const generateProblemIdsToString = (ids: ProblemId[] | undefined) => {
+  if (!ids) return "";
+  let qs = "";
+  for (const id of ids) {
+    if (qs.length > 0) qs += separateSymbol;
+    qs += id;
+  }
+  return qs;
 };
