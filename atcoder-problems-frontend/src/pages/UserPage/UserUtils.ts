479a53843d746835f8f0830e53ebbcf78fc4a72d
t-mangoe <m.takashi.xpair@gmail.com>
2021-04-30 09:39:08 +0900

    show Non-AC count in Difficulty Pies

--- atcoder-problems-frontend/src/pages/UserPage/UserUtils.ts
+++ atcoder-problems-frontend/src/pages/UserPage/UserUtils.ts
@@ -16,3 +16,17 @@ export const solvedProblemIdsFromArray = (submissions: Submission[]) => {
   const problemIds = new Set(accepted.map((s) => s.problem_id));
   return Array.from(problemIds);
 };
+
+export const rejectedProblemIdsFromArray = (submissions: Submission[]) => {
+  const accepted = submissions.filter((s) => isAccepted(s.result));
+  const acceptedProblemIds = new Set(accepted.map((s) => s.problem_id));
+
+  const rejected = submissions.filter((s) => !isAccepted(s.result));
+  const rejectedProblemIds = new Set(rejected.map((s) => s.problem_id));
+
+  const difference = new Set(
+    Array.from(rejectedProblemIds).filter((e) => !acceptedProblemIds.has(e))
+  );
+
+  return Array.from(difference);
+};
