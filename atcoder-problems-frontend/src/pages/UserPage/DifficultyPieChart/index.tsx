cfb38bf2a69fc787d8138a47dd5e2ff81c8b1345
t-mangoe <m.takashi.xpair@gmail.com>
2021-04-30 09:39:08 +0900

    show Non-AC count in Difficulty Pies

--- atcoder-problems-frontend/src/pages/UserPage/DifficultyPieChart/index.tsx
+++ atcoder-problems-frontend/src/pages/UserPage/DifficultyPieChart/index.tsx
@@ -8,11 +8,30 @@ import {
   RatingColors,
 } from "../../../utils";
 import { SinglePieChart } from "../../../components/SinglePieChart";
-import { solvedProblemIdsFromArray } from "../UserUtils";
+import {
+  rejectedProblemIdsFromArray,
+  solvedProblemIdsFromArray,
+} from "../UserUtils";
 
 interface Props {
   userId: string;
 }
+
+const getPieChartTitle = (ratingColor: RatingColor): string => {
+  if (!RatingColors.includes(ratingColor) || ratingColor === "Black") {
+    return "";
+  }
+  if (ratingColor === "Red") {
+    return "Difficulty 2800-";
+  }
+  const index = RatingColors.indexOf(ratingColor) - 1;
+  const str =
+    "Difficulty " +
+    (index * 400).toString() +
+    "-" +
+    ((index + 1) * 400 - 1).toString();
+  return str;
+};
 
 export const DifficultyPieChart: React.FC<Props> = (props) => {
   const submissions = useUserSubmission(props.userId) ?? [];
@@ -40,13 +59,28 @@ export const DifficultyPieChart: React.FC<Props> = (props) => {
     new Map<RatingColor, number>()
   );
 
+  // eslint-disable-next-line
+  const rejectedIds = rejectedProblemIdsFromArray(submissions);
+  const rejectedCount = rejectedIds.reduce((map, problemId) => {
+    const model = problemModels?.get(problemId);
+    if (model?.difficulty !== undefined) {
+      const color = getRatingColor(model.difficulty);
+      const curCount = map.get(color) ?? 0;
+      map.set(color, curCount + 1);
+      return map;
+    }
+    return map;
+  }, new Map<RatingColor, number>());
+
   const data = RatingColors.filter(
     (ratingColor) => ratingColor !== "Black"
   ).map((ratingColor) => {
     const totalCount = colorCount.get(ratingColor) ?? 0;
     const solved = solvedCount.get(ratingColor) ?? 0;
+    const rejected = rejectedCount.get(ratingColor) ?? 0;
     const color = getRatingColorCode(ratingColor);
-    return { color, totalCount, solved };
+    const title = getPieChartTitle(ratingColor);
+    return { color, totalCount, solved, rejected, title };
   });
 
   return (
@@ -59,14 +93,19 @@ export const DifficultyPieChart: React.FC<Props> = (props) => {
               <SinglePieChart
                 data={[
                   { name: "AC", color: e.color, value: e.solved },
+                  { name: "Non-AC", color: "#fd9", value: e.rejected },
                   {
                     name: "NoSub",
                     color: "#58616a",
-                    value: e.totalCount - e.solved,
+                    value: e.totalCount - e.solved - e.rejected,
                   },
                 ]}
               />
-              <h5 className="text-muted">{`${e.solved} / ${e.totalCount}`}</h5>
+              <h5 className="text-muted">
+                {`${e.solved} / ${e.rejected} /
+                ${e.totalCount - e.solved - e.rejected}`}
+              </h5>
+              <h5>{e.title}</h5>
             </Col>
           ))}
       </Row>
