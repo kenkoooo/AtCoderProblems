e209f51a676b88742840ec7356e040d52b657829
fukatani <nannyakannya@gmail.com>
2021-05-07 21:47:32 +0900

    backend: explain for command line option of crawl_whole_contest

--- atcoder-problems-backend/src/bin/crawl_whole_contest.rs
+++ atcoder-problems-backend/src/bin/crawl_whole_contest.rs
@@ -1,5 +1,5 @@
-use atcoder_client::AtCoderClient;
 use anyhow::Result;
+use atcoder_client::AtCoderClient;
 use atcoder_problems_backend::crawler::WholeContestCrawler;
 use atcoder_problems_backend::utils::init_log_config;
 use log::info;
@@ -10,8 +10,10 @@ use std::env;
 async fn main() -> Result<()> {
     init_log_config()?;
     info!("Started");
-    let url = env::var("SQL_URL").expect("SQL_URL is not set.");
-    let contest_id = env::args().nth(1).expect("contest_id is not set.");
+    let url = env::var("SQL_URL").expect("SQL_URL should be set as environmental variable.");
+    let contest_id = env::args()
+        .nth(1)
+        .expect("contest_id is not set.\nUsage: cargo run --bin crawl_whole_contest <contest_id>");
     let db = initialize_pool(&url).await?;
     let crawler = WholeContestCrawler::new(db, AtCoderClient::default(), contest_id);
     crawler.crawl().await?;
