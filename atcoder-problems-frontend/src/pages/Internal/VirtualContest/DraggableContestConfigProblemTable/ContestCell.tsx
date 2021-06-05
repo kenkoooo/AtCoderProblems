ca96ef16652a5bf9e5184ef0fca42f44a17733c5
monkukui <monkukui1953@gmail.com>
2021-06-05 20:23:52 +0900

    modified element when contest is undefined

--- atcoder-problems-frontend/src/pages/Internal/VirtualContest/DraggableContestConfigProblemTable/ContestCell.tsx
+++ atcoder-problems-frontend/src/pages/Internal/VirtualContest/DraggableContestConfigProblemTable/ContestCell.tsx
@@ -1,30 +1,12 @@
 import React from "react";
 import { ContestLink } from "../../../../components/ContestLink";
-import * as Url from "../../../../utils/Url";
 import { ProblemRowData } from "./index";
 
-type ContestCellProps = Pick<ProblemRowData, "problem" | "contest">;
-export const ContestCell: React.FC<ContestCellProps> = ({
-  problem,
-  contest,
-}) => {
+type ContestCellProps = Pick<ProblemRowData, "contest">;
+export const ContestCell: React.FC<ContestCellProps> = ({ contest }) => {
   return (
     <td style={{ width: "40%" }}>
-      {contest ? (
-        <ContestLink contest={contest} />
-      ) : (
-        <>
-          {problem ? (
-            <a
-              href={Url.formatContestUrl(problem.contest_id)}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              {problem.title}
-            </a>
-          ) : null}
-        </>
-      )}
+      {contest ? <ContestLink contest={contest} /> : null}
     </td>
   );
 };
