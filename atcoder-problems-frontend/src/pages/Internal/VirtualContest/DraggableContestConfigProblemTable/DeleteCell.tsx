e770ad678ebab42746ad5e8c78acf82816087599
monkukui <monkukui1953@gmail.com>
2021-06-05 22:25:03 +0900

    modified component designe

--- atcoder-problems-frontend/src/pages/Internal/VirtualContest/DraggableContestConfigProblemTable/DeleteCell.tsx
+++ atcoder-problems-frontend/src/pages/Internal/VirtualContest/DraggableContestConfigProblemTable/DeleteCell.tsx
@@ -1,23 +1,16 @@
 import React from "react";
 import { Button } from "reactstrap";
-import { VirtualContestItem } from "../../types";
 
 interface DeleteCellProps {
-  problemId: string;
-  problemSet: VirtualContestItem[];
-  setProblemSet: (newProblemSet: VirtualContestItem[]) => void;
+  onDelete: () => void;
 }
-export const DeleteCell: React.FC<DeleteCellProps> = ({
-  problemId,
-  problemSet,
-  setProblemSet,
-}) => {
+export const DeleteCell: React.FC<DeleteCellProps> = ({ onDelete }) => {
   return (
     <td style={{ width: "5px" }}>
       <Button
         close
-        onClick={(): void => {
-          setProblemSet(problemSet.filter((x) => x.id !== problemId));
+        onClick={() => {
+          onDelete();
         }}
       />
     </td>
