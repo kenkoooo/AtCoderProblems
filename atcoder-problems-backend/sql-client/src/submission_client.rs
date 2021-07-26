6e8d8b1f89668d9e5ba3d63038c8c00d97290a96
hotate29 <hotate_oc@yahoo.co.jp>
2021-07-26 21:38:32 +0900

    backend(sql): SubmissionClientからsubmission_count関連を削除

--- atcoder-problems-backend/sql-client/src/submission_client.rs
+++ atcoder-problems-backend/sql-client/src/submission_client.rs
@@ -2,9 +2,6 @@ use crate::models::Submission;
 use crate::PgPool;
 use anyhow::Result;
 use async_trait::async_trait;
-use sqlx::postgres::PgRow;
-use sqlx::Row;
-use std::collections::BTreeMap;
 
 const SUBMISSION_LIMIT: i64 = 10000;
 
@@ -48,12 +45,7 @@ pub enum SubmissionRequest<'a> {
 #[async_trait]
 pub trait SubmissionClient {
     async fn get_submissions<'a>(&self, request: SubmissionRequest<'a>) -> Result<Vec<Submission>>;
-    async fn get_user_submission_count(&self, user_id: &str) -> Result<i64>;
     async fn update_submissions(&self, values: &[Submission]) -> Result<usize>;
-    async fn update_submission_count(&self) -> Result<()>;
-    async fn update_user_submission_count(&self, user_id: &str) -> Result<()>;
-    async fn update_delta_submission_count(&self, values: &[Submission]) -> Result<()>;
-
     async fn count_stored_submissions(&self, ids: &[i64]) -> Result<usize> {
         let submissions = self
             .get_submissions(SubmissionRequest::ByIds { ids })
@@ -185,20 +177,6 @@ impl SubmissionClient for PgPool {
         Ok(submissions)
     }
 
-    async fn get_user_submission_count(&self, user_id: &str) -> Result<i64> {
-        let count = sqlx::query(
-            r"
-            SELECT count FROM submission_count
-            WHERE user_id = $1
-            ",
-        )
-        .bind(user_id)
-        .try_map(|row: PgRow| row.try_get::<i64, _>("count"))
-        .fetch_one(self)
-        .await?;
-        Ok(count)
-    }
-
     async fn update_submissions(&self, values: &[Submission]) -> Result<usize> {
         let (
             ids,
@@ -310,56 +288,5 @@ impl SubmissionClient for PgPool {
         .execute(self)
         .await?;
         Ok(count.rows_affected() as usize)
-    }
-
-    async fn update_submission_count(&self) -> Result<()> {
-        sqlx::query(
-            r"
-            INSERT INTO submission_count (user_id, count)
-            SELECT user_id, count(*) FROM submissions GROUP BY user_id
-            ON CONFLICT (user_id) DO UPDATE SET count=EXCLUDED.count",
-        )
-        .execute(self)
-        .await?;
-        Ok(())
-    }
-
-    async fn update_user_submission_count(&self, user_id: &str) -> Result<()> {
-        sqlx::query(
-            r"
-                INSERT INTO submission_count (user_id, count)
-                SELECT user_id, count(*) FROM submissions WHERE user_id = $1 GROUP BY user_id
-                ON CONFLICT (user_id) DO UPDATE SET count=EXCLUDED.count",
-        )
-        .bind(user_id)
-        .execute(self)
-        .await?;
-        Ok(())
-    }
-
-    async fn update_delta_submission_count(&self, values: &[Submission]) -> Result<()> {
-        let count_map = values.iter().fold(BTreeMap::new(), |mut map, submission| {
-            *map.entry(submission.user_id.as_str()).or_insert(0) += 1;
-            map
-        });
-        let (user_ids, counts): (Vec<&str>, Vec<i32>) = count_map.into_iter().unzip();
-
-        sqlx::query(
-            r"
-            INSERT INTO submission_count (user_id, count)
-            VALUE (
-                UNNEST($1::VARCHAR(255)[]),
-                UNNEST($2::BIGINT[])
-            )
-            ON CONFLICT (user_id)
-            DO UPDATE SET count = EXCLUDED.count
-            ",
-        )
-        .bind(user_ids)
-        .bind(counts)
-        .execute(self)
-        .await?;
-
-        Ok(())
     }
 }
