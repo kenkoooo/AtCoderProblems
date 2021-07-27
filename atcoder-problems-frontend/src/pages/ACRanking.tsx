4eb8ca9153e34757182097e36182b819cdc75939
fukatani <nannyakannya@gmail.com>
2021-07-27 13:03:59 +0900

    frontend: refactoring

--- atcoder-problems-frontend/src/pages/ACRanking.tsx
+++ atcoder-problems-frontend/src/pages/ACRanking.tsx
@@ -3,22 +3,12 @@ import { RemoteRanking } from "../components/Ranking";
 import { useACRanking, useUserACRank } from "../api/APIClient";
 
 export const ACRanking = () => {
-  const [page, setPage] = React.useState(1);
-  const [sizePerPage, setSizePerPage] = React.useState(20);
-  const data =
-    useACRanking((page - 1) * sizePerPage, page * sizePerPage).data ?? [];
-  const firstUser = data.length === 0 ? "" : data[0].user_id;
-  const firstRankOnPage = useUserACRank(firstUser).data?.rank ?? 0;
   return (
     <RemoteRanking
       title="AC Count Ranking"
       rankingSize={1000}
-      page={page}
-      sizePerPage={sizePerPage}
-      firstRankOnPage={firstRankOnPage}
-      data={data}
-      setPage={setPage}
-      setSizePerPage={setSizePerPage}
+      getRanking={useACRanking}
+      getUserRank={useUserACRank}
     />
   );
 };
--- atcoder-problems-frontend/src/pages/StreakRanking.tsx
+++ atcoder-problems-frontend/src/pages/StreakRanking.tsx
@@ -4,12 +4,6 @@ import { useStreakRanking, useUserStreakRank } from "../api/APIClient";
 import { RemoteRanking } from "../components/Ranking";
 
 export const StreakRanking = () => {
-  const [page, setPage] = React.useState(1);
-  const [sizePerPage, setSizePerPage] = React.useState(20);
-  const data =
-    useStreakRanking((page - 1) * sizePerPage, page * sizePerPage).data ?? [];
-  const firstUser = data.length === 0 ? "" : data[0].user_id;
-  const firstRankOnPage = useUserStreakRank(firstUser).data?.rank ?? 0;
   return (
     <RemoteRanking
       title={
@@ -25,12 +19,8 @@ export const StreakRanking = () => {
         </>
       }
       rankingSize={1000}
-      page={page}
-      sizePerPage={sizePerPage}
-      firstRankOnPage={firstRankOnPage}
-      data={data}
-      setPage={setPage}
-      setSizePerPage={setSizePerPage}
+      getRanking={useStreakRanking}
+      getUserRank={useUserStreakRank}
     />
   );
 };
