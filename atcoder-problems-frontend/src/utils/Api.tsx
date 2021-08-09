a2801f48e64ae2174bb8727193793c3f23ef013e
kenkoooo <kenkou.n@gmail.com>
2021-08-09 20:44:47 +0900

    Refresh IndexedDB if the local cache is not valid

--- atcoder-problems-frontend/src/utils/Api.tsx
+++ atcoder-problems-frontend/src/utils/Api.tsx
@@ -1,5 +1,6 @@
 import { UserId } from "../interfaces/Status";
 import Submission, { isSubmission } from "../interfaces/Submission";
+import { hasPropertyAsType, isNumber } from "./TypeUtils";
 
 const ATCODER_API_URL = process.env.REACT_APP_ATCODER_API_URL;
 
@@ -24,4 +25,19 @@ export const fetchPartialUserSubmissions = async (
     return [];
   }
   return json.filter(isSubmission);
+};
+
+export const fetchUserSubmissionCount = async (
+  userId: string,
+  fromSecond: number,
+  toSecond: number
+) => {
+  const url = `${ATCODER_API_URL}/v3/user/submission_count?user=${userId}&from_second=${fromSecond}&to_second=${toSecond}`;
+  const response = await fetch(url);
+  const json: unknown = await response.json();
+  if (hasPropertyAsType(json, "count", isNumber)) {
+    return json.count;
+  } else {
+    return 0;
+  }
 };
