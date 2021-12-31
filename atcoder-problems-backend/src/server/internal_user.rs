ab988b4d54ff7028e24e1498767e22429e1a9939
nebocco <nebocco@gmail.com>
2021-12-31 14:35:03 +0900

    change: users with actix-web

--- atcoder-problems-backend/src/server/internal_user.rs
+++ atcoder-problems-backend/src/server/internal_user.rs
@@ -1,29 +1,32 @@
-use crate::server::utils::RequestUnpack;
+use crate::server::utils::GetAuthId;
 use crate::server::{AppData, Authentication, CommonResponse};
 use serde::Deserialize;
 use sql_client::internal::user_manager::UserManager;
-use tide::{Request, Response, Result};
+use actix_web::{error, web, HttpRequest, HttpResponse, Result};
+
+#[derive(Deserialize)]
+struct Query {
+    atcoder_user_id: String,
+}
 
 pub(crate) async fn update<A: Authentication + Clone + Send + Sync + 'static>(
-    request: Request<AppData<A>>,
-) -> Result<Response> {
-    #[derive(Deserialize)]
-    struct Q {
-        atcoder_user_id: String,
-    }
-    let user_id = request.get_authorized_id().await?;
-    let conn = request.state().pg_pool.clone();
-    let body = request.parse_body::<Q>().await?;
+    request: HttpRequest,
+    data: web::Data<AppData<A>>,
+    body: web::Json<Query>
+) -> Result<HttpResponse> {
+    let user_id = data.get_authorized_id(request.cookie("token")).await?;
+    let conn = data.pg_pool.clone();
     conn.update_internal_user_info(&user_id, &body.atcoder_user_id)
-        .await?;
-    Ok(Response::empty_json())
+        .await.map_err(error::ErrorInternalServerError)?;
+    Ok(HttpResponse::empty_json())
 }
 
 pub(crate) async fn get<A: Authentication + Clone + Send + Sync + 'static>(
-    request: Request<AppData<A>>,
-) -> Result<Response> {
-    let user_id = request.get_authorized_id().await?;
-    let conn = request.state().pg_pool.clone();
-    let info = conn.get_internal_user_info(&user_id).await?;
-    Ok(Response::json(&info)?)
+    request: HttpRequest,
+    data: web::Data<AppData<A>>
+) -> Result<HttpResponse> {
+    let user_id = data.get_authorized_id(request.cookie("token")).await?;
+    let conn = data.pg_pool.clone();
+    let info = conn.get_internal_user_info(&user_id).await.map_err(error::ErrorInternalServerError)?;
+    Ok(HttpResponse::json(&info)?)
 }
