f4630d26be015d720d356dd5ee83d3d5d6e57949
nebocco <nebocco@gmail.com>
2021-12-31 15:09:36 +0900

    change: problem_list with actix-web

--- atcoder-problems-backend/src/server/problem_list.rs
+++ atcoder-problems-backend/src/server/problem_list.rs
@@ -1,135 +1,162 @@
-use crate::server::utils::RequestUnpack;
+use crate::server::utils::GetAuthId;
 use crate::server::{AppData, Authentication, CommonResponse};
+
+use actix_web::{error, web, HttpRequest, HttpResponse, Result};
 use serde::Deserialize;
 use sql_client::internal::problem_list_manager::ProblemListManager;
-use tide::{Request, Response, Result};
 
-pub(crate) async fn get_own_lists<A>(request: Request<AppData<A>>) -> Result<Response>
-where
-    A: Authentication + Clone + Send + Sync + 'static,
-{
-    let user_id = request.get_authorized_id().await?;
-    let conn = request.state().pg_pool.clone();
-    let list = conn.get_list(&user_id).await?;
-    let response = Response::json(&list)?;
+pub(crate) async fn get_own_lists<A: Authentication + Clone + Send + Sync + 'static>(
+    request: HttpRequest,
+    data: web::Data<AppData<A>>,
+) -> Result<HttpResponse> {
+    let user_id = data.get_authorized_id(request.cookie("token")).await?;
+    let conn = data.pg_pool.clone();
+    let list = conn
+        .get_list(&user_id)
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+    let response = HttpResponse::json(&list)?;
     Ok(response)
 }
 
-pub(crate) async fn get_single_list<A>(request: Request<AppData<A>>) -> Result<Response>
-where
-    A: Authentication + Clone + Send + Sync + 'static,
-{
-    let list_id = request.param("list_id")?;
-    let conn = request.state().pg_pool.clone();
-    let list = conn.get_single_list(list_id).await?;
-    let response = Response::json(&list)?;
+#[derive(Deserialize)]
+struct GetListQuery {
+    list_id: String,
+}
+
+pub(crate) async fn get_single_list<A: Authentication + Clone + Send + Sync + 'static>(
+    request: HttpRequest,
+    data: web::Data<AppData<A>>,
+    query: web::Query<GetListQuery>,
+) -> Result<HttpResponse> {
+    let user_id = data.get_authorized_id(request.cookie("token")).await?;
+    let conn = data.pg_pool.clone();
+    let list_id = &query.list_id;
+    let list = conn
+        .get_single_list(list_id)
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+    let response = HttpResponse::json(&list)?;
     Ok(response)
 }
 
-pub(crate) async fn create_list<A>(request: Request<AppData<A>>) -> Result<Response>
-where
-    A: Authentication + Clone + Send + Sync + 'static,
-{
-    #[derive(Deserialize)]
-    struct Query {
-        list_name: String,
-    }
-    let internal_user_id = request.get_authorized_id().await?;
-    let conn = request.state().pg_pool.clone();
-    let query = request.parse_body::<Query>().await?;
+#[derive(Deserialize)]
+struct CreateListQuery {
+    list_name: String,
+}
+
+pub(crate) async fn create_list<A: Authentication + Clone + Send + Sync + 'static>(
+    request: HttpRequest,
+    data: web::Data<AppData<A>>,
+    query: web::Json<CreateListQuery>,
+) -> Result<HttpResponse> {
+    let user_id = data.get_authorized_id(request.cookie("token")).await?;
+    let conn = data.pg_pool.clone();
     let internal_list_id = conn
-        .create_list(&internal_user_id, &query.list_name)
-        .await?;
+        .create_list(&user_id, &query.list_name)
+        .await
+        .map_err(error::ErrorInternalServerError)?;
     let body = serde_json::json!({ "internal_list_id": internal_list_id });
-    let response = Response::json(&body)?;
+    let response = HttpResponse::json(&body)?;
     Ok(response)
 }
 
-pub(crate) async fn delete_list<A>(request: Request<AppData<A>>) -> Result<Response>
-where
-    A: Authentication + Clone + Send + Sync + 'static,
-{
-    #[derive(Deserialize)]
-    struct Q {
-        internal_list_id: String,
-    }
-    request.get_authorized_id().await?;
-    let conn = request.state().pg_pool.clone();
-    let query = request.parse_body::<Q>().await?;
-    conn.delete_list(&query.internal_list_id).await?;
-    let response = Response::empty_json();
+#[derive(Deserialize)]
+struct DeleteListQuery {
+    internal_list_id: String,
+}
+
+pub(crate) async fn delete_list<A: Authentication + Clone + Send + Sync + 'static>(
+    request: HttpRequest,
+    data: web::Data<AppData<A>>,
+    query: web::Json<DeleteListQuery>,
+) -> Result<HttpResponse> {
+    let user_id = data.get_authorized_id(request.cookie("token")).await?;
+    let conn = data.pg_pool.clone();
+    conn.delete_list(&query.internal_list_id)
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+    let response = HttpResponse::empty_json();
     Ok(response)
 }
 
-pub(crate) async fn update_list<A>(request: Request<AppData<A>>) -> Result<Response>
-where
-    A: Authentication + Clone + Send + Sync + 'static,
-{
-    #[derive(Deserialize)]
-    struct Q {
-        internal_list_id: String,
-        name: String,
-    }
-    request.get_authorized_id().await?;
-    let conn = request.state().pg_pool.clone();
-    let query = request.parse_body::<Q>().await?;
+#[derive(Deserialize)]
+struct UpdateListQuery {
+    internal_list_id: String,
+    name: String,
+}
+
+pub(crate) async fn update_list<A: Authentication + Clone + Send + Sync + 'static>(
+    request: HttpRequest,
+    data: web::Data<AppData<A>>,
+    query: web::Json<UpdateListQuery>,
+) -> Result<HttpResponse> {
+    let user_id = data.get_authorized_id(request.cookie("token")).await?;
+    let conn = data.pg_pool.clone();
     conn.update_list(&query.internal_list_id, &query.name)
-        .await?;
-    let response = Response::empty_json();
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+    let response = HttpResponse::empty_json();
     Ok(response)
 }
 
-pub(crate) async fn add_item<A>(request: Request<AppData<A>>) -> Result<Response>
-where
-    A: Authentication + Clone + Send + Sync + 'static,
-{
-    #[derive(Deserialize)]
-    struct Q {
-        internal_list_id: String,
-        problem_id: String,
-    }
-    request.get_authorized_id().await?;
-    let conn = request.state().pg_pool.clone();
-    let query = request.parse_body::<Q>().await?;
+#[derive(Deserialize)]
+struct AddItemQuery {
+    internal_list_id: String,
+    problem_id: String,
+}
+
+pub(crate) async fn add_item<A: Authentication + Clone + Send + Sync + 'static>(
+    request: HttpRequest,
+    data: web::Data<AppData<A>>,
+    query: web::Json<AddItemQuery>,
+) -> Result<HttpResponse> {
+    let user_id = data.get_authorized_id(request.cookie("token")).await?;
+    let conn = data.pg_pool.clone();
     conn.add_item(&query.internal_list_id, &query.problem_id)
-        .await?;
-    let response = Response::empty_json();
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+    let response = HttpResponse::empty_json();
     Ok(response)
 }
 
-pub(crate) async fn update_item<A>(request: Request<AppData<A>>) -> Result<Response>
-where
-    A: Authentication + Clone + Send + Sync + 'static,
-{
-    #[derive(Deserialize)]
-    struct Q {
-        internal_list_id: String,
-        problem_id: String,
-        memo: String,
-    }
-    request.get_authorized_id().await?;
-    let conn = request.state().pg_pool.clone();
-    let query = request.parse_body::<Q>().await?;
+#[derive(Deserialize)]
+struct UpdateItemQuery {
+    internal_list_id: String,
+    problem_id: String,
+    memo: String,
+}
+
+pub(crate) async fn update_item<A: Authentication + Clone + Send + Sync + 'static>(
+    request: HttpRequest,
+    data: web::Data<AppData<A>>,
+    query: web::Json<UpdateItemQuery>,
+) -> Result<HttpResponse> {
+    let user_id = data.get_authorized_id(request.cookie("token")).await?;
+    let conn = data.pg_pool.clone();
     conn.update_item(&query.internal_list_id, &query.problem_id, &query.memo)
-        .await?;
-    let response = Response::empty_json();
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+    let response = HttpResponse::empty_json();
     Ok(response)
 }
 
-pub(crate) async fn delete_item<A>(request: Request<AppData<A>>) -> Result<Response>
-where
-    A: Authentication + Clone + Send + Sync + 'static,
-{
-    #[derive(Deserialize)]
-    struct Q {
-        internal_list_id: String,
-        problem_id: String,
-    }
-    request.get_authorized_id().await?;
-    let conn = request.state().pg_pool.clone();
-    let query = request.parse_body::<Q>().await?;
+#[derive(Deserialize)]
+struct DeleteItemQuery {
+    internal_list_id: String,
+    problem_id: String,
+}
+
+pub(crate) async fn delete_item<A: Authentication + Clone + Send + Sync + 'static>(
+    request: HttpRequest,
+    data: web::Data<AppData<A>>,
+    query: web::Json<DeleteItemQuery>,
+) -> Result<HttpResponse> {
+    let user_id = data.get_authorized_id(request.cookie("token")).await?;
+    let conn = data.pg_pool.clone();
     conn.delete_item(&query.internal_list_id, &query.problem_id)
-        .await?;
-    let response = Response::empty_json();
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+    let response = HttpResponse::empty_json();
     Ok(response)
 }
