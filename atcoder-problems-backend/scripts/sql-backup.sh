7ac3a07ef1d455bc388bd087797d948c5fff951d
kenkoooo <kenkou.n@gmail.com>
2021-12-31 19:14:33 +0900

    Put SQL backup script into the Docker image

--- atcoder-problems-backend/scripts/sql-backup.sh
+++ atcoder-problems-backend/scripts/sql-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-psql ${DATABASE_URL} -c "\copy submissions TO './submissions.csv' WITH (FORMAT CSV, HEADER)"
+psql ${SQL_URL} -c "\copy submissions TO './submissions.csv' WITH (FORMAT CSV, HEADER)"
 gzip submissions.csv
 aws s3 cp submissions.csv.gz s3://kenkoooo/submissions.csv.gz --acl public-read
