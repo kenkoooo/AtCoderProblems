6ee82e259ec9496b38bba3c871e244d06efec980
nebocco <nebocco@gmail.com>
2021-12-31 12:08:48 +0900

    change: time_submissions with actix-web

--- atcoder-problems-backend/src/server/time_submissions.rs
+++ atcoder-problems-backend/src/server/time_submissions.rs
@@ -1,17 +1,25 @@
 use crate::server::{AppData, CommonResponse};
 use sql_client::submission_client::{SubmissionClient, SubmissionRequest};
-use tide::{Request, Response, Result};
+use actix_web::{error, web, HttpRequest, HttpResponse, Result};
 
-pub(crate) async fn get_time_submissions<A>(request: Request<AppData<A>>) -> Result<Response> {
-    let from = request.param("from")?;
-    let from_epoch_second = from.parse::<i64>()?;
-    let conn = request.state().pg_pool.clone();
+struct Query {
+    from: i64
+}
+
+pub(crate) async fn get_time_submissions<A>(
+    request: HttpRequest,
+    data: web::Data<AppData<A>>,
+    query: web::Query<Query>
+) -> Result<HttpResponse> {
+    let from_epoch_second = query.from;
+    let conn = data.pg_pool.clone();
     let submissions: Vec<_> = conn
         .get_submissions(SubmissionRequest::FromTime {
             from_second: from_epoch_second,
             count: 1000,
         })
-        .await?;
-    let response = Response::json(&submissions)?.make_cors();
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+    let response = HttpResponse::json(&submissions)?.make_cors();
     Ok(response)
 }
