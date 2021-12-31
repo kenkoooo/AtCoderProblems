a85fdaf55f041bbbcf549b19954efa2e1076e792
nebocco <nebocco@gmail.com>
2021-12-31 11:18:32 +0900

    change: rps ranking with actix-web

--- atcoder-problems-backend/src/server/rated_point_sum_ranking.rs
+++ atcoder-problems-backend/src/server/rated_point_sum_ranking.rs
@@ -1,26 +1,31 @@
 use crate::server::{AppData, CommonResponse};
+use actix_web::{error, web, HttpRequest, HttpResponse, Result};
 use serde::Deserialize;
 use sql_client::rated_point_sum::RatedPointSumClient;
-use tide::{Request, Response, Result};
 
 const MAX_RANKING_RANGE_LENGTH: usize = 1000;
 
+#[derive(Debug, Deserialize)]
+struct Query {
+    from: usize,
+    to: usize,
+}
+
 pub(crate) async fn get_rated_point_sum_ranking<A>(
-    request: Request<AppData<A>>,
-) -> Result<Response> {
-    #[derive(Debug, Deserialize)]
-    struct Query {
-        from: usize,
-        to: usize,
-    }
-    let conn = request.state().pg_pool.clone();
-    let query = request.query::<Query>()?;
+    request: HttpRequest,
+    data: web::Data<AppData<A>>,
+    query: web::Query<Query>,
+) -> Result<HttpResponse> {
+    let conn = data.pg_pool.clone();
     let query = (query.from)..(query.to);
     if query.len() > MAX_RANKING_RANGE_LENGTH {
-        return Ok(Response::new(400));
+        return Ok(HttpResponse::Ok().finish());
     }
 
-    let ranking = conn.load_rated_point_sum_in_range(query).await?;
-    let response = Response::json(&ranking)?;
+    let ranking = conn
+        .load_rated_point_sum_in_range(query)
+        .await
+        .map_err(error::ErrorInternalServerError)?;
+    let response = HttpResponse::json(&ranking)?;
     Ok(response)
 }
