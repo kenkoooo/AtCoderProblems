b0e23694d6b91735b877e4592e31544ab6ca1729
hotate29 <hotate_oc@yahoo.co.jp>
2022-02-27 01:33:26 +0900

    test_list_item()をactix風に

--- atcoder-problems-backend/tests/test_server_e2e_problem_list.rs
+++ atcoder-problems-backend/tests/test_server_e2e_problem_list.rs
@@ -237,72 +237,61 @@ async fn test_invalid_token() {
 
 #[actix_web::test]
 async fn test_list_item() {
-    let port = setup().await;
     let mock_server = utils::start_mock_github_server(VALID_TOKEN);
     let mock_server_base_url = mock_server.base_url();
     let mock_api_server = utils::start_mock_github_api_server(VALID_TOKEN, GithubToken { id: 0 });
     let mock_api_server_base_url = mock_api_server.base_url();
-    let server = actix_web::rt::spawn(async move {
-        let pg_pool = sql_client::initialize_pool(utils::get_sql_url_from_env())
-            .await
-            .unwrap();
-        let github =
-            GithubClient::new("", "", &mock_server_base_url, &mock_api_server_base_url).unwrap();
-        actix_web::HttpServer::new(move || {
-            actix_web::App::new()
-                .wrap(GithubAuthentication::new(github.clone()))
-                .app_data(actix_web::web::Data::new(github.clone()))
-                .app_data(actix_web::web::Data::new(pg_pool.clone()))
-                .configure(atcoder_problems_backend::server::config_services)
-        })
-        .bind(("0.0.0.0", port))
-        .unwrap()
-        .run()
+
+    let pg_pool = sql_client::initialize_pool(utils::get_sql_url_from_env())
         .await
         .unwrap();
-    });
-    actix_web::rt::time::sleep(std::time::Duration::from_millis(1000)).await;
+    let github =
+        GithubClient::new("", "", &mock_server_base_url, &mock_api_server_base_url).unwrap();
 
-    reqwest::get(url(
-        &format!("/internal-api/authorize?code={}", VALID_CODE),
-        port,
-    ))
-    .await
-    .unwrap();
+    let app = test::init_service(
+        actix_web::App::new()
+            .wrap(GithubAuthentication::new(github.clone()))
+            .app_data(actix_web::web::Data::new(github))
+            .app_data(actix_web::web::Data::new(pg_pool))
+            .configure(atcoder_problems_backend::server::config_services),
+    )
+    .await;
+
+    let request = test::TestRequest::get()
+        .uri(&format!("/internal-api/authorize?code={}", VALID_CODE))
+        .to_request();
+    test::call_service(&app, request).await;
+
     let cookie_header = format!("token={}", VALID_TOKEN);
 
-    let response = reqwest::Client::new()
-        .post(url("/internal-api/list/create", port))
-        .header("Cookie", cookie_header.as_str())
-        .json(&json!({"list_name":"a"}))
-        .send()
-        .await
-        .unwrap();
-    assert!(response.status().is_success(), "{:?}", response);
-    let value = response.json::<Value>().await.unwrap();
-    let internal_list_id = value.get("internal_list_id").unwrap().as_str().unwrap();
+    let request = test::TestRequest::post()
+        .uri("/internal-api/list/create")
+        .insert_header(("Cookie", cookie_header.as_str()))
+        .set_json(json!({"list_name":"a"}))
+        .to_request();
+    let response: Value = test::call_and_read_body_json(&app, request).await;
 
-    let response = reqwest::Client::new()
-        .post(url("/internal-api/list/item/add", port))
-        .header("Cookie", cookie_header.as_str())
-        .json(&json!({
+    let internal_list_id = response["internal_list_id"].as_str().unwrap();
+
+    let request = test::TestRequest::post()
+        .uri("/internal-api/list/item/add")
+        .insert_header(("Cookie", cookie_header.as_str()))
+        .set_json(json!({
             "internal_list_id": internal_list_id,
             "internal_user_id": "0",
             "problem_id": "problem_1"
         }))
-        .send()
-        .await
-        .unwrap();
+        .to_request();
+    let response = test::call_service(&app, request).await;
+
     assert!(response.status().is_success(), "{:?}", response);
-    let list: Value = reqwest::Client::new()
-        .get(url("/internal-api/list/my", port))
-        .header("Cookie", cookie_header.as_str())
-        .send()
-        .await
-        .unwrap()
-        .json::<Value>()
-        .await
-        .unwrap();
+
+    let request = test::TestRequest::get()
+        .uri("/internal-api/list/my")
+        .insert_header(("Cookie", cookie_header.as_str()))
+        .to_request();
+    let list: Value = test::call_and_read_body_json(&app, request).await;
+
     assert_eq!(
         list,
         json!([
@@ -314,29 +303,26 @@ async fn test_list_item() {
             }
         ])
     );
-
-    let response = reqwest::Client::new()
-        .post(url("/internal-api/list/item/update", port))
-        .header("Cookie", cookie_header.as_str())
-        .json(&json!({
+    let request = test::TestRequest::post()
+        .uri("/internal-api/list/item/update")
+        .insert_header(("Cookie", cookie_header.as_str()))
+        .set_json(json!({
             "internal_list_id": internal_list_id,
             "problem_id": "problem_1",
             "internal_user_id": "0",
             "memo": "memo_1"
         }))
-        .send()
-        .await
-        .unwrap();
+        .to_request();
+    let response = test::call_service(&app, request).await;
+
     assert!(response.status().is_success(), "{:?}", response);
-    let list = reqwest::Client::new()
-        .get(url("/internal-api/list/my", port))
-        .header("Cookie", cookie_header.as_str())
-        .send()
-        .await
-        .unwrap()
-        .json::<Value>()
-        .await
-        .unwrap();
+
+    let request = test::TestRequest::get()
+        .uri("/internal-api/list/my")
+        .insert_header(("Cookie", cookie_header.as_str()))
+        .to_request();
+    let list: Value = test::call_and_read_body_json(&app, request).await;
+
     assert_eq!(
         list,
         json!([
@@ -349,26 +335,24 @@ async fn test_list_item() {
         ])
     );
 
-    let response = reqwest::Client::new()
-        .post(url("/internal-api/list/item/delete", port))
-        .header("Cookie", cookie_header.as_str())
-        .json(&json!({
+    let request = test::TestRequest::post()
+        .uri("/internal-api/list/item/delete")
+        .insert_header(("Cookie", cookie_header.as_str()))
+        .set_json(json!({
             "internal_list_id": internal_list_id,
             "problem_id": "problem_1"
         }))
-        .send()
-        .await
-        .unwrap();
+        .to_request();
+    let response = test::call_service(&app, request).await;
+
     assert!(response.status().is_success(), "{:?}", response);
-    let list = reqwest::Client::new()
-        .get(url("/internal-api/list/my", port))
-        .header("Cookie", cookie_header.as_str())
-        .send()
-        .await
-        .unwrap()
-        .json::<Value>()
-        .await
-        .unwrap();
+
+    let request = test::TestRequest::get()
+        .uri("/internal-api/list/my")
+        .insert_header(("Cookie", cookie_header.as_str()))
+        .to_request();
+    let list: Value = test::call_and_read_body_json(&app, request).await;
+
     assert_eq!(
         list,
         json!([
@@ -380,9 +364,6 @@ async fn test_list_item() {
             }
         ])
     );
-
-    server.abort();
-    server.await.unwrap_err();
 }
 
 #[actix_web::test]
