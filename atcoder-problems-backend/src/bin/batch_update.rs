722c6eb94a55bf1ec8ae40c19fe0fff00b1677e8
hotate29 <hotate_oc@yahoo.co.jp>
2022-12-25 03:55:43 +0900

    `submissions`テーブルに存在しないユーザーの記録を削除するようにした

--- atcoder-problems-backend/src/bin/batch_update.rs
+++ atcoder-problems-backend/src/bin/batch_update.rs
@@ -1,7 +1,6 @@
 use atcoder_problems_backend::utils::{init_log_config, EXCLUDED_USERS};
 use log::info;
 use sql_client::accepted_count::AcceptedCountClient;
-use sql_client::initialize_pool;
 use sql_client::language_count::LanguageCountClient;
 use sql_client::models::Submission;
 use sql_client::problem_info::ProblemInfoUpdater;
@@ -9,8 +8,32 @@ use sql_client::problems_submissions::ProblemsSubmissionUpdater;
 use sql_client::rated_point_sum::RatedPointSumClient;
 use sql_client::streak::StreakClient;
 use sql_client::submission_client::{SubmissionClient, SubmissionRequest};
+use sql_client::{initialize_pool, PgPool};
 use std::env;
 use std::error::Error;
+
+async fn clean_up_ranking(conn: &PgPool) -> anyhow::Result<()> {
+    // AtCoderアカウントの名前を変更すると、`submissions`テーブルに存在する古い名前が新しい名前に変更される。
+    // `accepted_count`などを更新する処理は提出データの更新を想定しておらず、古い名前のデータが残ってしまうので、ここで削除する。
+    for table_name in [
+        "accepted_count",
+        "rated_point_sum",
+        "language_count",
+        "max_streaks",
+    ] {
+        sql_client::query(&format!(
+            "
+            DELETE FROM {table_name} WHERE NOT EXISTS (
+                SELECT user_id FROM submissions WHERE {table_name}.user_id = submissions.user_id
+            )
+            "
+        ))
+        .execute(conn)
+        .await?;
+    }
+
+    Ok(())
+}
 
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -63,6 +86,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     info!("Executing update_streak_count...");
     conn.update_streak_count(&all_accepted_submissions).await?;
+
+    info!("Executing clean_up_disappeared_users...");
+    clean_up_ranking(&conn).await?;
 
     info!("Finished");
     Ok(())
